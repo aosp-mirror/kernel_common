89d0bbe023300fef146e4703d1729d1b6686c1a6
Mike Lockwood <lockwood@android.com>
2010-02-06 21:53:51 -0500

    USB: composite: Add class driver for enabling and disabling USB functions.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/linux/usb/composite.h
+++ include/linux/usb/composite.h
@@ -129,6 +129,7 @@ struct usb_function {
 	/* internals */
 	struct list_head		list;
 	DECLARE_BITMAP(endpoints, 32);
+	struct device			*dev;
 };
 
 int usb_add_function(struct usb_configuration *, struct usb_function *);
@@ -274,6 +275,9 @@ struct usb_composite_driver {
 	struct usb_gadget_strings		**strings;
 	unsigned		needs_serial:1;
 
+	struct class		*class;
+	atomic_t		function_count;
+
 	int			(*unbind)(struct usb_composite_dev *);
 
 	void			(*disconnect)(struct usb_composite_dev *);
@@ -281,6 +285,8 @@ struct usb_composite_driver {
 	/* global suspend hooks */
 	void			(*suspend)(struct usb_composite_dev *);
 	void			(*resume)(struct usb_composite_dev *);
+
+	void			(*enable_function)(struct usb_function *f, int enable);
 };
 
 extern int usb_composite_probe(struct usb_composite_driver *driver,
