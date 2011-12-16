ee1f8e84625f6a0e668362d588496d07dfe59505
John Stultz <john.stultz@linaro.org>
2011-12-15 18:51:04 -0800

    ANDROID: ashmem: Add shmem_set_file to mm/shmem.c
    NOT FOR STAGING
    This patch re-adds the original shmem_set_file to mm/shmem.c
    and converts ashmem.c back to using it.
    
    Change-Id: Ie604c9f8f4d0ee6bc2aae1a96d261c8373a1a2dc
    CC: Brian Swetland <swetland@google.com>
    CC: Colin Cross <ccross@android.com>
    CC: Arve Hjønnevåg <arve@android.com>
    CC: Dima Zavin <dima@android.com>
    CC: Robert Love <rlove@google.com>
    CC: Greg KH <greg@kroah.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -4261,6 +4261,14 @@ struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags
 }
 EXPORT_SYMBOL_GPL(shmem_file_setup);
 
+void shmem_set_file(struct vm_area_struct *vma, struct file *file)
+{
+	if (vma->vm_file)
+		fput(vma->vm_file);
+	vma->vm_file = file;
+	vma->vm_ops = &shmem_vm_ops;
+}
+
 /**
  * shmem_file_setup_with_mnt - get an unlinked file living in tmpfs
  * @mnt: the tmpfs mount where the file will be created
@@ -4294,10 +4302,7 @@ int shmem_zero_setup(struct vm_area_struct *vma)
 	if (IS_ERR(file))
 		return PTR_ERR(file);
 
-	if (vma->vm_file)
-		fput(vma->vm_file);
-	vma->vm_file = file;
-	vma->vm_ops = &shmem_vm_ops;
+	shmem_set_file(vma, file);
 
 	if (IS_ENABLED(CONFIG_TRANSPARENT_HUGE_PAGECACHE) &&
 			((vma->vm_start + ~HPAGE_PMD_MASK) & HPAGE_PMD_MASK) <
