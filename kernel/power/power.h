c81843338fb936ce5c582acbd0f56606a6fdd5c8
Arve Hjønnevåg <arve@android.com>
2012-03-16 17:39:38 -0700

    Revert "PM: Add user-space wake lock api."
    This reverts commit 92e7a6e6a59c3cf0742621309a1d534eaecab5f5.
    
    Change-Id: I1d44ce687b0053c6a8e29f49e02c508d1741c081

--- kernel/power/power.h
+++ kernel/power/power.h
@@ -276,14 +276,3 @@ extern struct workqueue_struct *suspend_work_queue;
 extern struct wake_lock main_wake_lock;
 extern suspend_state_t requested_suspend_state;
 #endif
-
-#ifdef CONFIG_USER_WAKELOCK
-ssize_t wake_lock_show(struct kobject *kobj, struct kobj_attribute *attr,
-			char *buf);
-ssize_t wake_lock_store(struct kobject *kobj, struct kobj_attribute *attr,
-			const char *buf, size_t n);
-ssize_t wake_unlock_show(struct kobject *kobj, struct kobj_attribute *attr,
-			char *buf);
-ssize_t  wake_unlock_store(struct kobject *kobj, struct kobj_attribute *attr,
-			const char *buf, size_t n);
-#endif
