ef215d0afa906e14c31b29bce2bac80d241102c5
Duan Jiong <duanj.fnst@cn.fujitsu.com>
2013-11-08 09:56:53 +0800

    ipv6: use rt6_get_dflt_router to get default router in rt6_route_rcv
    As the rfc 4191 said, the Router Preference and Lifetime values in a
    ::/0 Route Information Option should override the preference and lifetime
    values in the Router Advertisement header. But when the kernel deals with
    a ::/0 Route Information Option, the rt6_get_route_info() always return
    NULL, that means that overriding will not happen, because those default
    routers were added without flag RTF_ROUTEINFO in rt6_add_dflt_router().
    
    In order to deal with that condition, we should call rt6_get_dflt_router
    when the prefix length is 0.
    
    (cherry picked from commit f104a567e673f382b09542a8dc3500aa689957b4)
    
    Bug: 33333670
    Test: net_test passes
    Change-Id: I47c19d592fd810b649220f878731ed49edd3cb03
    Signed-off-by: Joel Scherpelz <jscherpelz@google.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -683,7 +683,10 @@ int rt6_route_rcv(struct net_device *dev, u8 *opt, int len,
 		prefix = &prefix_buf;
 	}
 
-	rt = rt6_get_route_info(dev, prefix, rinfo->prefix_len, gwaddr);
+	if (rinfo->prefix_len == 0)
+		rt = rt6_get_dflt_router(gwaddr, dev);
+	else
+		rt = rt6_get_route_info(dev, prefix, rinfo->prefix_len, gwaddr);
 
 	if (rt && !lifetime) {
 		ip6_del_rt(rt);
