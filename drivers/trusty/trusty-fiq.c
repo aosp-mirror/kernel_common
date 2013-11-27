23fccb369a21c0781d26be1fab6125b3e3b43c71
Arve Hjønnevåg <arve@android.com>
2013-11-26 20:18:35 -0800

    trusty: Add fiq support
    Register a custom fiq-return handler with the fiq-glue so the
    monitor mode can atomically re-enable the fiq and return to the
    last mode.
    
    Change-Id: I7f2720f6c66f171995ab1927602e781f7dbea52d
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

---
+++ drivers/trusty/trusty-fiq.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <asm/fiq_glue.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
+#include <linux/trusty/smcall.h>
+#include <linux/trusty/trusty.h>
+
+#define _STRINGIFY(x) #x
+#define STRINGIFY(x) _STRINGIFY(x)
+
+static void __naked trusty_fiq_return(void)
+{
+	asm volatile(
+		".arch_extension sec\n"
+		"mov	r12, r0\n"
+		"ldr	r0, =" STRINGIFY(SMC_FC_FIQ_EXIT) "\n"
+		"smc	#0");
+}
+
+static int trusty_fiq_remove_child(struct device *dev, void *data)
+{
+	platform_device_unregister(to_platform_device(dev));
+	return 0;
+}
+
+static int trusty_fiq_probe(struct platform_device *pdev)
+{
+	int ret;
+
+	ret = fiq_glue_set_return_handler(trusty_fiq_return);
+	if (ret)
+		goto err_set_fiq_return;
+
+	ret = of_platform_populate(pdev->dev.of_node, NULL, NULL, &pdev->dev);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "Failed to add children: %d\n", ret);
+		goto err_add_children;
+	}
+
+	return 0;
+
+err_add_children:
+	device_for_each_child(&pdev->dev, NULL, trusty_fiq_remove_child);
+err_set_fiq_return:
+	return ret;
+}
+
+static int trusty_fiq_remove(struct platform_device *pdev)
+{
+	device_for_each_child(&pdev->dev, NULL, trusty_fiq_remove_child);
+	fiq_glue_clear_return_handler(trusty_fiq_return);
+	return 0;
+}
+
+static const struct of_device_id trusty_fiq_of_match[] = {
+	{ .compatible = "android,trusty-fiq-v1", },
+	{},
+};
+
+static struct platform_driver trusty_fiq_driver = {
+	.probe = trusty_fiq_probe,
+	.remove = trusty_fiq_remove,
+	.driver	= {
+		.name = "trusty-fiq",
+		.owner = THIS_MODULE,
+		.of_match_table = trusty_fiq_of_match,
+	},
+};
+
+module_platform_driver(trusty_fiq_driver);
