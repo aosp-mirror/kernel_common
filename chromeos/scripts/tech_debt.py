a7167e74b9f1906a290c893fc3bd545680cd6f77
Mike Frysinger <vapier@chromium.org>
2014-09-09 04:01:16 -0400

    CHROMIUM: add PRESUBMIT.cfg to disable various checks
    This makes it so that we don't need to run 'repo upload --no-verify'.
    
    BUG=chromium:411452
    TEST=ran upload on some CLs
    
    Change-Id: I094dd90548a4722115a6d4d21ba8b4ef86b00c54
    Signed-off-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/217026
    
    [rebase66(tzungbi):
     Revised commit message.
     Squashed:
       Part of "CHROMIUM: scripts: add initial skeleton"
       CHROMIUM: PRESUBMIT: Fix quoting, relative paths
         (https://crrev.com/c/4182621)
       CHROMIUM: PRESUBMIT.cfg: Disable presubmit checks which might fire on UPSTREAM patches
         (https://crrev.com/c/4186326)
       CHROMIUM: PRESUBMIT: `cros lint --include ...`
         (https://crrev.com/c/4224960)
       CHROMIUM: PRESUBMIT: delete unused json_check
         (https://crrev.com/c/4241960)
       CHROMIUM: PRESUBMIT: delete clang_format_check
         (https://crrev.com/c/4921998)
       CHROMIUM: PRESUBMIT: delete check_rustfmt
         (https://crrev.com/c/5003932)
       CHROMIUM: PRESUBMIT: tech_debt: run cros format
         (https://crrev.com/c/5013343)
       CHROMIUM: PRESUBMIT: tech_debt: Allow BUG=none tags
         (https://crrev.com/c/5013344)
    ]
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- chromeos/scripts/tech_debt.py
+++ chromeos/scripts/tech_debt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
 """Does presubmit checks for kernel technical debt.
 
 This is intended to be used by `repo`. For more information, please see
@@ -14,19 +16,28 @@ import subprocess
 import sys
 
 
-TECH_DEBT_MSG = 'This patch is not fully upstream. Please open a tracking bug here: go/cros-kernel-technical-debt-bug and add a label UPSTREAM-TASK=b:XXXX referencing it. A member of the review committee will review the CL. Thank you.'
-TECH_DEBT_MISSING_BUG = 'Proper BUG=b:XXXX entry is missing in the commit message'
-TECH_DEBT_DUP_BUG_MSG = 'UPSTREAM-TASK bugs cannot be the same as those present in the BUG tag.'
+TECH_DEBT_MSG = (
+    "This patch is not fully upstream. Please open a tracking bug here: "
+    "go/cros-kernel-technical-debt-bug and add a label UPSTREAM-TASK=b:XXXX "
+    "referencing it. A member of the review committee will review the CL. "
+    "Thank you."
+)
+TECH_DEBT_DUP_BUG_MSG = (
+    "UPSTREAM-TASK bugs cannot be the same as those present in the BUG tag."
+)
+
 
 def is_chromium_only(files):
     """Check if the CL modifies files that only belong to ChromiumOS"""
     repo_path = subprocess.check_output(
-        ['git', 'rev-parse', '--show-toplevel'], encoding='utf-8').strip()
+        ["git", "rev-parse", "--show-toplevel"], encoding="utf-8"
+    ).strip()
 
     for file in files:
         file = os.path.relpath(file, repo_path)
-        if not file.startswith(('chromeos/', 'OWNERS', 'PRESUBMIT.cfg',
-                                'unblocked_terms.txt')):
+        if not file.startswith(
+            ("chromeos/", "OWNERS", "PRESUBMIT.cfg", "unblocked_terms.txt")
+        ):
             return False
 
     return True
@@ -35,26 +46,30 @@ def is_chromium_only(files):
 def check_tech_debt(commit):
     """Inspect commit messages to validate tech_debt tags."""
     commit_message = subprocess.check_output(
-        ['git', 'log', '-n', '1', '--format=%B', commit],
-        encoding='utf-8',
+        ["git", "log", "-n", "1", "--format=%B", commit],
+        encoding="utf-8",
     )
     if commit_message.startswith(
-        ('UPSTREAM:', 'FROMGIT:', 'WIP:', 'TEST:', 'TEST-ONLY:', 'BACKPORT:',
-         'Revert', 'Reland')) and not commit_message.startswith(
-        'BACKPORT: FROMLIST'):
+        (
+            "UPSTREAM:",
+            "FROMGIT:",
+            "WIP:",
+            "TEST:",
+            "TEST-ONLY:",
+            "BACKPORT:",
+            "Revert",
+            "Reland",
+        )
+    ) and not commit_message.startswith("BACKPORT: FROMLIST"):
         return True
 
-    bug_id = r'b:[0-9]{7,}'
-    bugs_ids= rf'({bug_id}(?:[, ]+{bug_id})*)'
+    bug_id = r"b:[0-9]{7,}"
+    bugs_ids = rf"({bug_id}(?:[, ]+{bug_id})*)"
 
-    bug_tag=rf'\nBUG={bugs_ids}+'
-    upstream_task_tag=rf'\nUPSTREAM-TASK={bugs_ids}+'
+    bug_tag = rf"\nBUG={bugs_ids}+"
+    upstream_task_tag = rf"\nUPSTREAM-TASK={bugs_ids}+"
 
     bug_line = re.findall(bug_tag, commit_message)
-    if bug_line == []:
-        print(TECH_DEBT_MISSING_BUG, file=sys.stderr)
-        return False
-
     upstream_task_line = re.findall(upstream_task_tag, commit_message)
     if upstream_task_line == []:
         print(TECH_DEBT_MSG, file=sys.stderr)
@@ -74,16 +89,17 @@ def check_tech_debt(commit):
 
     return True
 
+
 def main():
     """Main function."""
 
-    presubmit_files = os.environ.get('PRESUBMIT_FILES')
+    presubmit_files = os.environ.get("PRESUBMIT_FILES")
     if presubmit_files is None:
-        sys.exit('Need a value for PRESUBMIT_FILES')
+        sys.exit("Need a value for PRESUBMIT_FILES")
 
-    presubmit_commit = os.environ.get('PRESUBMIT_COMMIT')
+    presubmit_commit = os.environ.get("PRESUBMIT_COMMIT")
     if presubmit_commit is None:
-        sys.exit('Need a value for PRESUBMIT_COMMIT')
+        sys.exit("Need a value for PRESUBMIT_COMMIT")
 
     if is_chromium_only(presubmit_files.splitlines()):
         sys.exit(0)
@@ -92,5 +108,5 @@ def main():
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
