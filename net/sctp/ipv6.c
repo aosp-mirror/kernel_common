678e10fd60e6b751e1152056aed0e2b851f5e175
Martin KaFai Lau <kafai@fb.com>
2015-05-22 20:56:01 -0700

    ipv6: Add rt6_get_cookie() function
    [ Upstream commit b197df4f0f3782782e9ea8996e91b65ae33e8dd9 ]
    
    Instead of doing the rt6->rt6i_node check whenever we need
    to get the route's cookie.  Refactor it into rt6_get_cookie().
    It is a prep work to handle FLOWI_FLAG_KNOWN_NH and also
    percpu rt6_info later.
    
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Cc: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Cc: Steffen Klassert <steffen.klassert@secunet.com>
    Cc: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [Lee: Back-ported to fix a build issue]
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I13fe96b05e449edfb77b954379222b459636275d

--- net/sctp/ipv6.c
+++ net/sctp/ipv6.c
@@ -361,7 +361,7 @@ out:
 		struct rt6_info *rt;
 
 		rt = (struct rt6_info *)dst;
-		t->dst_cookie = rt->rt6i_node ? rt->rt6i_node->fn_sernum : 0;
+		t->dst_cookie = rt6_get_cookie(rt);
 		pr_debug("rt6_dst:%pI6/%d rt6_src:%pI6\n",
 			 &rt->rt6i_dst.addr, rt->rt6i_dst.plen,
 			 &fl->u.ip6.saddr);
