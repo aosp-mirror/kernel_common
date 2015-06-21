e8360a736d184d95328e48b985992eacb3052802
Marek Lindner <mareklindner@neomailbox.ch>
2015-06-22 00:30:22 +0800

    batman-adv: convert orig_node->vlan_list to hlist
    [ Upstream commit d0fa4f3f5b0419a016ff27eea2fa615e4c0d1713 ]
    
    Since the list's tail is never accessed using a double linked list head
    wastes memory.
    
    [Lee: Backported to Android-3.18 to fix the following build issue]
    
     In file included from ../include/linux/mutex.h:14,
                      from ../net/batman-adv/main.h:167,
                      from ../net/batman-adv/originator.c:18:
     ../net/batman-adv/originator.c: In function ‘batadv_orig_node_release’:
     ../include/linux/list.h:753:36: error: ‘struct list_head’ has no member named ‘first’
       for (pos = hlist_entry_safe((head)->first, typeof(*pos), member);\
                                        ^~
    
    Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>
    Signed-off-by: Antonio Quartulli <antonio@meshcoding.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I2023de81078ff39cc887a7610a473754368397ee

--- net/batman-adv/originator.c
+++ net/batman-adv/originator.c
@@ -15,6 +15,8 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <linux/rculist.h>
+
 #include "main.h"
 #include "distributed-arp-table.h"
 #include "originator.h"
@@ -58,7 +60,7 @@ batadv_orig_node_vlan_get(struct batadv_orig_node *orig_node,
 	struct batadv_orig_node_vlan *vlan = NULL, *tmp;
 
 	rcu_read_lock();
-	list_for_each_entry_rcu(tmp, &orig_node->vlan_list, list) {
+	hlist_for_each_entry_rcu(tmp, &orig_node->vlan_list, list) {
 		if (tmp->vid != vid)
 			continue;
 
@@ -106,7 +108,7 @@ batadv_orig_node_vlan_new(struct batadv_orig_node *orig_node,
 	atomic_set(&vlan->refcount, 2);
 	vlan->vid = vid;
 
-	list_add_rcu(&vlan->list, &orig_node->vlan_list);
+	hlist_add_head_rcu(&vlan->list, &orig_node->vlan_list);
 
 out:
 	spin_unlock_bh(&orig_node->vlan_list_lock);
@@ -629,7 +631,7 @@ struct batadv_orig_node *batadv_orig_node_new(struct batadv_priv *bat_priv,
 		return NULL;
 
 	INIT_HLIST_HEAD(&orig_node->neigh_list);
-	INIT_LIST_HEAD(&orig_node->vlan_list);
+	INIT_HLIST_HEAD(&orig_node->vlan_list);
 	INIT_HLIST_HEAD(&orig_node->ifinfo_list);
 	spin_lock_init(&orig_node->bcast_seqno_lock);
 	spin_lock_init(&orig_node->neigh_list_lock);
