df507d4a772170f99e4f366aaf346607e98958ff
Jagadish Krishnamoorthy <jagadish.krishnamoorthy@intel.com>
2015-10-20 12:29:06 -0700

    CHROMIUM: HACK: xhci: Increase the reset handshake timeout
    There are few occasions where the xhci controller
    is found to require more than 10s to complete
    the reset operation.
    
    BRANCH=None
    BUG=chrome-os-partner:46559,chrome-os-partner:46294
    TEST=Verified CherryView / Braswell platforms go through over
    firmware_ConsecutiveBoot test
    
    Change-Id: I5f2c018d5c5e3fa914eb6988debb0d8079b1917a
    Signed-off-by: Jagadish Krishnamoorthy <jagadish.krishnamoorthy@intel.com>
    Signed-off-by: Freddy Paul <freddy.paul@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/307394
    Commit-Ready: Bernie Thompson <bhthompson@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    [rebase44(groeck): Resolved conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -201,7 +201,8 @@ int xhci_reset(struct xhci_hcd *xhci, u64 timeout_us)
 	if (xhci->quirks & XHCI_INTEL_HOST)
 		udelay(1000);
 
-	ret = xhci_handshake(&xhci->op_regs->command, CMD_RESET, 0, timeout_us);
+	ret = xhci_handshake(&xhci->op_regs->command,
+			CMD_RESET, 0, 20 * 1000 * 1000);
 	if (ret)
 		return ret;
 
