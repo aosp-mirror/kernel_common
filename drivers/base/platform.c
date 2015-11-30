1adf32c2ca3fdeed10fca0ad2f55c9a54ce53b93
Martin Wilck <Martin.Wilck@ts.fujitsu.com>
2015-11-30 12:50:05 +0100

    base/platform: Fix platform drivers with no probe callback
    commit 25cad69f21f5532d99e2ee73c8ab6512bcab614c upstream.
    
    Since b8b2c7d845d5, platform_drv_probe() is called for all platform
    devices. If drv->probe is NULL, and dev_pm_domain_attach() fails,
    platform_drv_probe() will return the error code from dev_pm_domain_attach().
    
    This causes real_probe() to enter the "probe_failed" path and set
    dev->driver to NULL. Before b8b2c7d845d5, real_probe() would assume
    success if both dev->bus->probe and drv->probe were missing. As a result,
    a device and driver could be "bound" together just by matching their names;
    this doesn't work any more after b8b2c7d845d5.
    
    This may cause problems later for certain usage of platform_driver_register()
    and platform_device_register_simple(). I observed a panic while loading
    the tpm_tis driver with parameter "force=1" (i.e. registering tpm_tis as
    a platform driver), because tpm_tis_init's assumption that the device
    returned by platform_device_register_simple() was bound didn't hold any more
    (tpmm_chip_alloc() dereferences chip->pdev->driver, causing panic).
    
    This patch restores the previous (4.3.0 and earlier) behavior of
    platform_drv_probe() in the case when the associated platform driver has
    no "probe" function.
    
    Fixes: b8b2c7d845d5 ("base/platform: assert that dev_pm_domain callbacks are called unconditionally")
    Signed-off-by: Martin Wilck <Martin.Wilck@ts.fujitsu.com>
    Cc: Martin Fuzzey <mfuzzey@parkeon.com>
    Acked-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Icbbdee571ce119cd8228e6720a74a7cf91d8df03

--- drivers/base/platform.c
+++ drivers/base/platform.c
@@ -514,9 +514,14 @@ static int platform_drv_probe(struct device *_dev)
 
 	ret = dev_pm_domain_attach(_dev, true);
 	if (ret != -EPROBE_DEFER) {
-		ret = drv->probe(dev);
-		if (ret)
-			dev_pm_domain_detach(_dev, true);
+		if (drv->probe) {
+			ret = drv->probe(dev);
+			if (ret)
+				dev_pm_domain_detach(_dev, true);
+		} else {
+			/* don't fail if just dev_pm_domain_attach failed */
+			ret = 0;
+		}
 	}
 
 	if (drv->prevent_deferred_probe && ret == -EPROBE_DEFER) {
