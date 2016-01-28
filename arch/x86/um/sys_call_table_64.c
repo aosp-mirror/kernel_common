0cacc3742e6c7e8737da650b6c559ea6619cb3a0
Andy Lutomirski <luto@kernel.org>
2016-01-28 15:11:22 -0800

    BACKPORT: x86/syscalls: Remove __SYSCALL_COMMON and __SYSCALL_X32
    The common/64/x32 distinction has no effect other than
    determining which kernels actually support the syscall.  Move
    the logic into syscalltbl.sh.
    
    Signed-off-by: Andy Lutomirski <luto@kernel.org>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/58d4a95f40e43b894f93288b4a3633963d0ee22e.1454022279.git.luto@kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 32324ce15ea8cb4c8acc28acb2fd36fabf73e9db)
    
    Bug: 119769499
    Change-Id: I8bf383ac2886f2bc3f9b027d9fb683a25d1bcbcf
    [ghackmann@google.com:
     - apply entry/syscall_64.c changes to kernel/syscall_64.c
     - adjust context]
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- arch/x86/um/sys_call_table_64.c
+++ arch/x86/um/sys_call_table_64.c
@@ -33,9 +33,6 @@
 #define stub_execve sys_execve
 #define stub_rt_sigreturn sys_rt_sigreturn
 
-#define __SYSCALL_COMMON(nr, sym, compat) __SYSCALL_64(nr, sym, compat)
-#define __SYSCALL_X32(nr, sym, compat) /* Not supported */
-
 #define __SYSCALL_64(nr, sym, compat) extern asmlinkage void sym(void) ;
 #include <asm/syscalls_64.h>
 
