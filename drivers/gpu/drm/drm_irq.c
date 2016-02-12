fb138e525295ad64c17b6219e67eee862086dbaf
Mario Kleiner <mario.kleiner.de@gmail.com>
2016-02-12 20:30:30 +0100

    drm: Fix treatment of drm_vblank_offdelay in drm_vblank_on() (v2)
    commit bb74fc1bf3072bd3ab4ed5f43afd287a63baf2d7 upstream.
    
    drm_vblank_offdelay can have three different types of values:
    
    < 0 is to be always treated the same as dev->vblank_disable_immediate
    = 0 is to be treated as "never disable vblanks"
    > 0 is to be treated as disable immediate if kms driver wants it
        that way via dev->vblank_disable_immediate. Otherwise it is
        a disable timeout in msecs.
    
    This got broken in Linux 3.18+ for the implementation of
    drm_vblank_on. If the user specified a value of zero which should
    always reenable vblank irqs in this function, a kms driver could
    override the users choice by setting vblank_disable_immediate
    to true. This patch fixes the regression and keeps the user in
    control.
    
    v2: Only reenable vblank if there are clients left or the user
        requested to "never disable vblanks" via offdelay 0. Enabling
        vblanks even in the "delayed disable" case (offdelay > 0) was
        specifically added by Ville in commit cd19e52aee922
        ("drm: Kick start vblank interrupts at drm_vblank_on()"),
        but after discussion it turns out that this was done by accident.
    
        Citing Ville: "I think it just ended up as a mess due to changing
        some of the semantics of offdelay<0 vs. offdelay==0 vs.
        disable_immediate during the review of the series. So yeah, given
        how drm_vblank_put() works now, I'd just make this check for
        offdelay==0."
    
    Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: michel@daenzer.net
    Cc: vbabka@suse.cz
    Cc: ville.syrjala@linux.intel.com
    Cc: daniel.vetter@ffwll.ch
    Cc: dri-devel@lists.freedesktop.org
    Cc: alexander.deucher@amd.com
    Cc: christian.koenig@amd.com
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I3c443e27d55bfbbbf08ea7f65494a39a9fee738f

--- drivers/gpu/drm/drm_irq.c
+++ drivers/gpu/drm/drm_irq.c
@@ -1224,8 +1224,7 @@ void drm_vblank_on(struct drm_device *dev, int crtc)
 	 * re-enable interrupts if there are users left, or the
 	 * user wishes vblank interrupts to be enabled all the time.
 	 */
-	if (atomic_read(&vblank->refcount) != 0 ||
-	    (!dev->vblank_disable_immediate && drm_vblank_offdelay == 0))
+	if (atomic_read(&vblank->refcount) != 0 || drm_vblank_offdelay == 0)
 		WARN_ON(drm_vblank_enable(dev, crtc));
 	spin_unlock_irqrestore(&dev->vbl_lock, irqflags);
 }
