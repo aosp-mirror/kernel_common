85c1b272e3905ebea8b3a2a69e8300c0336cc591
Gabor Juhos <juhosg@openwrt.org>
2016-02-17 12:58:20 +0100

    pata-rb532-cf: get rid of the irq_to_gpio() call
    commit 018361767a21fb2d5ebd3ac182c04baf8a8b4e08 upstream.
    
    The RB532 platform specific irq_to_gpio() implementation has been
    removed with commit 832f5dacfa0b ("MIPS: Remove all the uses of
    custom gpio.h"). Now the platform uses the generic stub which causes
    the following error:
    
      pata-rb532-cf pata-rb532-cf: no GPIO found for irq149
      pata-rb532-cf: probe of pata-rb532-cf failed with error -2
    
    Drop the irq_to_gpio() call and get the GPIO number from platform
    data instead. After this change, the driver works again:
    
      scsi host0: pata-rb532-cf
      ata1: PATA max PIO4 irq 149
      ata1.00: CFA: CF 1GB, 20080820, max MWDMA4
      ata1.00: 1989792 sectors, multi 0: LBA
      ata1.00: configured for PIO4
      scsi 0:0:0:0: Direct-Access     ATA      CF 1GB           0820 PQ: 0\
      ANSI: 5
      sd 0:0:0:0: [sda] 1989792 512-byte logical blocks: (1.01 GB/971 MiB)
      sd 0:0:0:0: [sda] Write Protect is off
      sd 0:0:0:0: [sda] Write cache: disabled, read cache: enabled, doesn't\
      support DPO or FUA
       sda: sda1 sda2
      sd 0:0:0:0: [sda] Attached SCSI disk
    
    Fixes: 832f5dacfa0b ("MIPS: Remove all the uses of custom gpio.h")
    Cc: Alban Bedel <albeu@free.fr>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I08fdc82c406e2417ebff51fbe8ebd26e997b485e

--- drivers/ata/pata_rb532_cf.c
+++ drivers/ata/pata_rb532_cf.c
@@ -33,6 +33,8 @@
 
 #include <asm/gpio.h>
 
+#include <asm/mach-rc32434/rb.h>
+
 #define DRV_NAME	"pata-rb532-cf"
 #define DRV_VERSION	"0.1.0"
 #define DRV_DESC	"PATA driver for RouterBOARD 532 Compact Flash"
@@ -108,6 +110,7 @@ static int rb532_pata_driver_probe(struct platform_device *pdev)
 	int gpio;
 	struct resource *res;
 	struct ata_host *ah;
+	struct cf_device *pdata;
 	struct rb532_cf_info *info;
 	int ret;
 
@@ -123,7 +126,13 @@ static int rb532_pata_driver_probe(struct platform_device *pdev)
 		return -ENOENT;
 	}
 
-	gpio = irq_to_gpio(irq);
+	pdata = dev_get_platdata(&pdev->dev);
+	if (!pdata) {
+		dev_err(&pdev->dev, "no platform data specified\n");
+		return -EINVAL;
+	}
+
+	gpio = pdata->gpio_pin;
 	if (gpio < 0) {
 		dev_err(&pdev->dev, "no GPIO found for irq%d\n", irq);
 		return -ENOENT;
