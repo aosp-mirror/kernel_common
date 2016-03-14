f2f8a36d2837a993d1cce732e7d6cdb7790886ba
Arnd Bergmann <arnd@arndb.de>
2016-03-14 15:18:37 +0100

    ath9k: fix misleading indentation
    commit 362210e0dff4eb7bb36a9b34dbef3b39d779d95e upstream.
    
    A cleanup patch in linux-3.18 moved around some code in the ath9k
    driver and left some code to be indented in a misleading way,
    made worse by the addition of some new code for p2p mode, as
    discovered by a new gcc-6 warning:
    
    drivers/net/wireless/ath/ath9k/init.c: In function 'ath9k_set_hw_capab':
    drivers/net/wireless/ath/ath9k/init.c:851:4: warning: statement is indented as if it were guarded by... [-Wmisleading-indentation]
        hw->wiphy->iface_combinations = if_comb;
        ^~
    drivers/net/wireless/ath/ath9k/init.c:847:3: note: ...this 'if' clause, but it is not
       if (ath9k_is_chanctx_enabled())
       ^~
    
    The code is in fact correct, but the indentation is not, so I'm
    reformatting it as it should have been after the original cleanup.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: 499afaccf6f3 ("ath9k: Isolate ath9k_use_chanctx module parameter")
    Fixes: eb61f9f623f7 ("ath9k: advertise p2p dev support when chanctx")
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I41ef92155bbe36694b5505d5a5863ee3671c17b0

--- drivers/net/wireless/ath/ath9k/init.c
+++ drivers/net/wireless/ath/ath9k/init.c
@@ -802,8 +802,8 @@ static void ath9k_set_hw_capab(struct ath_softc *sc, struct ieee80211_hw *hw)
 			BIT(NL80211_IFTYPE_MESH_POINT) |
 			BIT(NL80211_IFTYPE_WDS);
 
-			hw->wiphy->iface_combinations = if_comb;
-			hw->wiphy->n_iface_combinations = ARRAY_SIZE(if_comb);
+		hw->wiphy->iface_combinations = if_comb;
+		hw->wiphy->n_iface_combinations = ARRAY_SIZE(if_comb);
 	}
 
 	hw->wiphy->flags &= ~WIPHY_FLAG_PS_ON_BY_DEFAULT;
