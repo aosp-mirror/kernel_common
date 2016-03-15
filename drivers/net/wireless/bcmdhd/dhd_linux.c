15bd2913ac072e61bab827303d72495fbe882aee
Ashwin <ashwin.bhat@broadcom.com>
2016-03-15 16:44:42 -0700

    net: wireless: bcmdhd: Remove ePNO support bit
    (cherry pick drom 8fc74ed25d30f1e698494f3655745b42d9697601)
    
    Remove the ePNO support bit in capabilities since Nexus 9 will
    not support the new ePNO in Android N
    
    Signed-off-by: Ashwin <ashwin.bhat@broadcom.com>
    Bug: 28257814
    Change-Id: I92d395bdace78f791dbd58cbb59a5a4df283da0c

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -7819,7 +7819,6 @@ int dhd_dev_get_feature_set(struct net_device *dev)
 		feature_set |= WIFI_FEATURE_BATCH_SCAN;
 #ifdef GSCAN_SUPPORT
 		feature_set |= WIFI_FEATURE_GSCAN;
-		feature_set |= WIFI_FEATURE_HAL_EPNO;
 #endif /* GSCAN_SUPPORT */
 	}
 	if (FW_SUPPORTED(dhd, rssi_mon)) {
