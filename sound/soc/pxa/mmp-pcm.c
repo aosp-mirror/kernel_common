a1b143c589b72ad8f2c1976037eed0873ddf8f9a
Andrea Adami <andrea.adami@gmail.com>
2016-05-06 17:27:34 +0200

    ASoC: pxa: Fix module autoload for platform drivers
    commit e5b7d71aa5b32180adec49a17c752e577c68f740 upstream.
    
    These platform drivers are lacking MODULE_ALIAS so module autoloading
    doesn't work. Tested on corgi and poodle with kernel 4.4.
    
    Signed-off-by: Andrea Adami <andrea.adami@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I2bee5bfc0477e27614230eff939b47b10862e695

--- sound/soc/pxa/mmp-pcm.c
+++ sound/soc/pxa/mmp-pcm.c
@@ -256,3 +256,4 @@ module_platform_driver(mmp_pcm_driver);
 MODULE_AUTHOR("Leo Yan <leoy@marvell.com>");
 MODULE_DESCRIPTION("MMP Soc Audio DMA module");
 MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:mmp-pcm-audio");
