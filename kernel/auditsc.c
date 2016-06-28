7ed07928b0f6d2eb13388b5620a586ae007c012e
Richard Guy Briggs <rgb@redhat.com>
2016-06-28 12:06:58 -0400

    audit: move calcs after alloc and check when logging set loginuid
    commit 76a658c20efd541a62838d9ff68ce94170d7a549 upstream.
    
    Move the calculations of values after the allocation in case the
    allocation fails.  This avoids wasting effort in the rare case that it
    fails, but more importantly saves us extra logic to release the tty
    ref.
    
    Signed-off-by: Richard Guy Briggs <rgb@redhat.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Cc: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Ide1d1a5bca3624681a5889f3cb5cebb914a48426

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -2075,13 +2075,14 @@ static void audit_log_set_loginuid(kuid_t koldloginuid, kuid_t kloginuid,
 	if (!audit_enabled)
 		return;
 
+	ab = audit_log_start(NULL, GFP_KERNEL, AUDIT_LOGIN);
+	if (!ab)
+		return;
+
 	uid = from_kuid(&init_user_ns, task_uid(current));
 	oldloginuid = from_kuid(&init_user_ns, koldloginuid);
 	loginuid = from_kuid(&init_user_ns, kloginuid),
 
-	ab = audit_log_start(NULL, GFP_KERNEL, AUDIT_LOGIN);
-	if (!ab)
-		return;
 	audit_log_format(ab, "pid=%d uid=%u", task_tgid_nr(current), uid);
 	audit_log_task_context(ab);
 	audit_log_format(ab, " old-auid=%u auid=%u old-ses=%u ses=%u res=%d",
