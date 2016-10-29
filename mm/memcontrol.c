2b7a22faddd5205f939a192d0f4803bc9163b6ec
Amit Pundir <amit.pundir@linaro.org>
2016-10-29 11:59:25 +0800

    Revert "memcg: add permission check"
    This reverts commit 12f6e0702f2aff6c109584e3015f8bd2b7ea445a.
    
    Part of Chromium commit series:
    
    CHROMIUM: remove Android's cgroup generic permissions checks
    
    The implementation is utterly broken, resulting in all processes being
    allows to move tasks between sets (as long as they have access to the
    "tasks" attribute), and upstream is heading towards checking only
    capability anyway, so let's get rid of this code.
    
    BUG=b:31790445,chromium:647994
    TEST=Boot android container, examine logcat
    
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/394967
    Reviewed-by: Ricky Zhou <rickyz@chromium.org>
    Reviewed-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5044,12 +5044,6 @@ static int mem_cgroup_can_attach(struct cgroup_subsys_state *css,
 	return ret;
 }
 
-static int mem_cgroup_allow_attach(struct cgroup_subsys_state *css,
-				   struct cgroup_taskset *tset)
-{
-	return subsys_cgroup_allow_attach(css->cgroup, tset);
-}
-
 static void mem_cgroup_cancel_attach(struct cgroup_subsys_state *css,
 				     struct cgroup_taskset *tset)
 {
@@ -5210,11 +5204,6 @@ static void mem_cgroup_move_task(struct cgroup_subsys_state *css,
 #else	/* !CONFIG_MMU */
 static int mem_cgroup_can_attach(struct cgroup_subsys_state *css,
 				 struct cgroup_taskset *tset)
-{
-	return 0;
-}
-static int mem_cgroup_allow_attach(struct cgroup_subsys_state *css,
-				   struct cgroup_taskset *tset)
 {
 	return 0;
 }
@@ -5396,7 +5385,6 @@ struct cgroup_subsys memory_cgrp_subsys = {
 	.can_attach = mem_cgroup_can_attach,
 	.cancel_attach = mem_cgroup_cancel_attach,
 	.attach = mem_cgroup_move_task,
-	.allow_attach = mem_cgroup_allow_attach,
 	.bind = mem_cgroup_bind,
 	.dfl_cftypes = memory_files,
 	.legacy_cftypes = mem_cgroup_legacy_files,
