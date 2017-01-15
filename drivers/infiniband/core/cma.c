456b6bca3c080e46db9463d6f68b3e594ecbf4f1
Jack Morgenstein <jackm@dev.mellanox.co.il>
2017-01-15 20:15:00 +0200

    RDMA/cma: Fix unknown symbol when CONFIG_IPV6 is not enabled
    commit b4cfe3971f6eab542dd7ecc398bfa1aeec889934 upstream.
    
    If IPV6 has not been enabled in the underlying kernel, we must avoid
    calling IPV6 procedures in rdma_cm.ko.
    
    This requires using "IS_ENABLED(CONFIG_IPV6)" in "if" statements
    surrounding any code which calls external IPV6 procedures.
    
    In the instance fixed here, procedure cma_bind_addr() called
    ipv6_addr_type() -- which resulted in calling external procedure
    __ipv6_addr_type().
    
    Fixes: 6c26a77124ff ("RDMA/cma: fix IPv6 address resolution")
    Cc: Spencer Baugh <sbaugh@catern.com>
    Signed-off-by: Jack Morgenstein <jackm@dev.mellanox.co.il>
    Reviewed-by: Moni Shoua <monis@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I27ffa1ea44575c085a54f3e5952eb57c202db3ed

--- drivers/infiniband/core/cma.c
+++ drivers/infiniband/core/cma.c
@@ -2188,7 +2188,8 @@ static int cma_bind_addr(struct rdma_cm_id *id, struct sockaddr *src_addr,
 	if (!src_addr || !src_addr->sa_family) {
 		src_addr = (struct sockaddr *) &id->route.addr.src_addr;
 		src_addr->sa_family = dst_addr->sa_family;
-		if (dst_addr->sa_family == AF_INET6) {
+		if (IS_ENABLED(CONFIG_IPV6) &&
+		    dst_addr->sa_family == AF_INET6) {
 			((struct sockaddr_in6 *) src_addr)->sin6_scope_id =
 				((struct sockaddr_in6 *) dst_addr)->sin6_scope_id;
 		} else if (dst_addr->sa_family == AF_IB) {
