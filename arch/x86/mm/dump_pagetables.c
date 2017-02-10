0222f4b00a05253f538e6ef9ebc7bc59ad10c186
Andrey Ryabinin <aryabinin@virtuozzo.com>
2017-02-10 12:54:05 +0300

    x86/mm/ptdump: Fix soft lockup in page table walker
    commit 146fbb766934dc003fcbf755b519acef683576bf upstream.
    
    CONFIG_KASAN=y needs a lot of virtual memory mapped for its shadow.
    In that case ptdump_walk_pgd_level_core() takes a lot of time to
    walk across all page tables and doing this without
    a rescheduling causes soft lockups:
    
     NMI watchdog: BUG: soft lockup - CPU#3 stuck for 23s! [swapper/0:1]
     ...
     Call Trace:
      ptdump_walk_pgd_level_core+0x40c/0x550
      ptdump_walk_pgd_level_checkwx+0x17/0x20
      mark_rodata_ro+0x13b/0x150
      kernel_init+0x2f/0x120
      ret_from_fork+0x2c/0x40
    
    I guess that this issue might arise even without KASAN on huge machines
    with several terabytes of RAM.
    
    Stick cond_resched() in pgd loop to fix this.
    
    Reported-by: Tobias Regnery <tobias.regnery@gmail.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: kasan-dev@googlegroups.com
    Cc: Alexander Potapenko <glider@google.com>
    Cc: "Paul E . McKenney" <paulmck@linux.vnet.ibm.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: stable@vger.kernel.org
    Link: http://lkml.kernel.org/r/20170210095405.31802-1-aryabinin@virtuozzo.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    [bwh: Backported to 4.4: adjust context]
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I76372d64ae2f2235445d6ba09c5bf876a896d4cf

--- arch/x86/mm/dump_pagetables.c
+++ arch/x86/mm/dump_pagetables.c
@@ -15,6 +15,7 @@
 #include <linux/debugfs.h>
 #include <linux/mm.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 #include <linux/seq_file.h>
 
 #include <asm/pgtable.h>
@@ -371,6 +372,7 @@ void ptdump_walk_pgd_level(struct seq_file *m, pgd_t *pgd)
 		} else
 			note_page(m, &st, __pgprot(0), 1);
 
+		cond_resched();
 		start++;
 	}
 
