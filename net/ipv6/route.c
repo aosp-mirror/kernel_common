94759703abaac043d19f7eb10b73c5abd1ab4d14
Joel Scherpelz <jscherpelz@google.com>
2017-04-06 18:21:32 +0900

    Revert "ipv6: use rt6_get_dflt_router to get default router in rt6_route_rcv"
    This reverts commit 7944a7e69fec06efa427f07f550163e438b9927c.
    
    Change-Id: I911dad45640933f602c9c6f2571d4fe66eb5e09f
    Signed-off-by: Joel Scherpelz <jscherpelz@google.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -683,11 +683,7 @@ int rt6_route_rcv(struct net_device *dev, u8 *opt, int len,
 		prefix = &prefix_buf;
 	}
 
-	if (rinfo->prefix_len == 0)
-		rt = rt6_get_dflt_router(gwaddr, dev);
-	else
-		rt = rt6_get_route_info(net, prefix, rinfo->prefix_len,
-					gwaddr, dev->ifindex);
+	rt = rt6_get_route_info(dev, prefix, rinfo->prefix_len, gwaddr);
 
 	if (rt && !lifetime) {
 		ip6_del_rt(rt);
