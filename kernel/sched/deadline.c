efe13f06b04c351e739bac6a5d2e1438d37a506e
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2017-11-08 19:56:30 +0000

    ANDROID: sched: Introduce Window Assisted Load Tracking (WALT)
    This patch is a combination of many many patches which have
    been previously applied to Android/EAS kernels. Similarly to
    other EAS components, we are squashing these to present a more
    orderly view of component history and relationships.
    
    The original description of WALT was:
    
    Use a window based view of time in order to track task demand
    and CPU utilization in the scheduler.
    
    WALT accounts two major statistics; CPU load and cumulative tasks
    demand.
    
    The CPU load which is account of accumulated each CPU's absolute
    execution time is for CPU frequency guidance.  Whereas cumulative
    tasks demand which is each CPU's instantaneous load to reflect
    CPU's load at given time is for task placement decision.
    
    Use cumulative tasks demand for cpu_util() for task placement and
    introduce cpu_util_freq() for frequency guidance.
    
    This version includes the "cumulative window demand" statistic
    which was originally described as:
    
    Energy cost estimation has been a long lasting challenge for WALT
    because WALT guides CPU frequency based on the CPU utilization of
    previous window.  Consequently it's not possible to know newly
    waking-up task's energy cost until WALT's end of the current window.
    
    The WALT already tracks 'Previous Runnable Sum' (prev_runnable_sum)
    and 'Cumulative Runnable Average' (cr_avg).  They are designed for
    CPU frequency guidance and task placement but unfortunately both
    are not suitable for the energy cost estimation.
    
    It's because using prev_runnable_sum for energy cost calculation would
    make us to account CPU and task's energy solely based on activity in the
    previous window so for example, any task didn't have an activity in the
    previous window will be accounted as a 'zero energy cost' task.
    Energy estimation with cr_avg is what energy_diff() relies on at present.
    However cr_avg can only represent instantaneous picture of energy cost
    thus for example, if a CPU was fully occupied for an entire WALT window
    and became idle just before window boundary, and if there is a wake-up,
    energy_diff() accounts that CPU is a 'zero energy cost' CPU.
    
    As a result, introduce a new accounting unit 'Cumulative Window Demand'.
    The cumulative window demand tracks all the tasks' demands have seen in
    current window which is neither instantaneous nor actual execution time.
    Because task demand represents estimated scaled execution time when the
    task runs a full window, accumulation of all the demands represents
    predicted CPU load at the end of window.
    
    Thus we can estimate CPU's frequency at the end of current WALT window
    with the cumulative window demand.
    
    This version is extracted wholesale from the version currently
    available in android-4.4 and android-4.9.
    
    Window Assisted Load Tracking (WALT) implementation credits:
     Srivatsa Vaddagiri, Steve Muckle, Syed Rameez Mustafa,
     Joonwoo Park, Pavan Kumar Kondeti, Olav Haugan,
     Srinath Sridharan, Vikram Mulukutla, Todd Kjos, Juri Lelli,
     John Stultz, Andres Oportus
    
    Change-Id: If92dd9db843374073be59d2cb83febfef993b562
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -20,6 +20,8 @@
 #include <linux/slab.h>
 #include <uapi/linux/sched/types.h>
 
+#include "walt.h"
+
 struct dl_bandwidth def_dl_bandwidth;
 
 static inline struct task_struct *dl_task_of(struct sched_dl_entity *dl_se)
@@ -1353,6 +1355,7 @@ void inc_dl_tasks(struct sched_dl_entity *dl_se, struct dl_rq *dl_rq)
 	WARN_ON(!dl_prio(prio));
 	dl_rq->dl_nr_running++;
 	add_nr_running(rq_of_dl_rq(dl_rq), 1);
+	walt_inc_cumulative_runnable_avg(rq_of_dl_rq(dl_rq), dl_task_of(dl_se));
 
 	inc_dl_deadline(dl_rq, deadline);
 	inc_dl_migration(dl_se, dl_rq);
@@ -1367,6 +1370,7 @@ void dec_dl_tasks(struct sched_dl_entity *dl_se, struct dl_rq *dl_rq)
 	WARN_ON(!dl_rq->dl_nr_running);
 	dl_rq->dl_nr_running--;
 	sub_nr_running(rq_of_dl_rq(dl_rq), 1);
+	walt_dec_cumulative_runnable_avg(rq_of_dl_rq(dl_rq), dl_task_of(dl_se));
 
 	dec_dl_deadline(dl_rq, dl_se->deadline);
 	dec_dl_migration(dl_se, dl_rq);
@@ -2085,7 +2089,9 @@ retry:
 	deactivate_task(rq, next_task, 0);
 	sub_running_bw(&next_task->dl, &rq->dl);
 	sub_rq_bw(&next_task->dl, &rq->dl);
+	next_task->on_rq = TASK_ON_RQ_MIGRATING;
 	set_task_cpu(next_task, later_rq->cpu);
+	next_task->on_rq = TASK_ON_RQ_QUEUED;
 	add_rq_bw(&next_task->dl, &later_rq->dl);
 	add_running_bw(&next_task->dl, &later_rq->dl);
 	activate_task(later_rq, next_task, 0);
@@ -2177,7 +2183,9 @@ static void pull_dl_task(struct rq *this_rq)
 			deactivate_task(src_rq, p, 0);
 			sub_running_bw(&p->dl, &src_rq->dl);
 			sub_rq_bw(&p->dl, &src_rq->dl);
+			p->on_rq = TASK_ON_RQ_MIGRATING;
 			set_task_cpu(p, this_cpu);
+			p->on_rq = TASK_ON_RQ_QUEUED;
 			add_rq_bw(&p->dl, &this_rq->dl);
 			add_running_bw(&p->dl, &this_rq->dl);
 			activate_task(this_rq, p, 0);
