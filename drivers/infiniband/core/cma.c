c9c797146ebb58310f214c216c2c1b9bd874a858
Moni Shoua <monis@mellanox.com>
2017-11-26 20:23:54 +0200

    RDMA/cma: Make sure that PSN is not over max allowed
    [ Upstream commit 23a9cd2ad90543e9da3786878d2b2729c095439d ]
    
    This patch limits the initial value for PSN to 24 bits as
    spec requires.
    
    Signed-off-by: Moni Shoua <monis@mellanox.com>
    Signed-off-by: Mukesh Kacker <mukesh.kacker@oracle.com>
    Signed-off-by: Daniel Jurgens <danielj@mellanox.com>
    Reviewed-by: Parav Pandit <parav@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I40a477421d4a085d2ccfbaf93a7497c63592bd30

--- drivers/infiniband/core/cma.c
+++ drivers/infiniband/core/cma.c
@@ -514,6 +514,7 @@ struct rdma_cm_id *rdma_create_id(rdma_cm_event_handler event_handler,
 	INIT_LIST_HEAD(&id_priv->listen_list);
 	INIT_LIST_HEAD(&id_priv->mc_list);
 	get_random_bytes(&id_priv->seq_num, sizeof id_priv->seq_num);
+	id_priv->seq_num &= 0x00ffffff;
 
 	return &id_priv->id;
 }
