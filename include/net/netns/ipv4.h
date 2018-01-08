a8a6566f723265fd353ebdc4a522aba3d325e2f7
Amit Pundir <amit.pundir@linaro.org>
2018-01-08 22:51:52 +0530

    RFC: ANDROID: net: ipv4: tcp: Namespace-ify sysctl_tcp_default_init_rwnd
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -155,6 +155,7 @@ struct netns_ipv4 {
 	int sysctl_tcp_invalid_ratelimit;
 	int sysctl_tcp_pacing_ss_ratio;
 	int sysctl_tcp_pacing_ca_ratio;
+	int sysctl_tcp_default_init_rwnd;
 	int sysctl_tcp_wmem[3];
 	int sysctl_tcp_rmem[3];
 	struct inet_timewait_death_row tcp_death_row;
