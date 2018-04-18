d1cb6e4a93ca1aadd52f071a2ca6997acc961d91
Greg Kroah-Hartman <gregkh@google.com>
2018-04-18 10:10:09 +0200

    arm64: fix up merge problem with VA_START
    I messed up the upstream merge...
    
    Change-Id: Id2a374d0333c4a81b273caa7bb21066f4923927f
    Reported-by: Miles Chen <miles.chen@mediatek.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -52,8 +52,6 @@
 	(UL(1) << VA_BITS) + 1)
 #define PAGE_OFFSET		(UL(0xffffffffffffffff) - \
 	(UL(1) << (VA_BITS - 1)) + 1)
-#define VA_START		(UL(0xffffffffffffffff) << VA_BITS)
-#define PAGE_OFFSET		(UL(0xffffffffffffffff) << (VA_BITS - 1))
 #define KIMAGE_VADDR		(MODULES_END)
 #define MODULES_END		(MODULES_VADDR + MODULES_VSIZE)
 #define MODULES_VADDR		(VA_START + KASAN_SHADOW_SIZE)
