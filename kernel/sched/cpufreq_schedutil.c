396d254c129e1e7a61f509f99bcdbcb7e8577e2a
Vincent Guittot <vincent.guittot@linaro.org>
2018-05-25 15:12:24 +0200

    FROMLIST: cpufreq/schedutil: add rt utilization tracking
    Add both cfs and rt utilization when selecting an OPP for cfs tasks as rt
    can preempt and steal cfs's running time.
    
    Change-Id: I8656dcfbf242116e059233d36760ff052a2c9ba4
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -57,6 +57,7 @@ struct sugov_cpu {
 	/* The fields below are only needed when sharing a policy: */
 	unsigned long		util_cfs;
 	unsigned long		util_dl;
+	unsigned long		util_rt;
 	unsigned long		max;
 
 	/* The field below is for single-CPU policies only: */
@@ -179,14 +180,21 @@ static void sugov_get_util(struct sugov_cpu *sg_cpu)
 	sg_cpu->max = arch_scale_cpu_capacity(NULL, sg_cpu->cpu);
 	sg_cpu->util_cfs = cpu_util_cfs(rq);
 	sg_cpu->util_dl  = cpu_util_dl(rq);
+	sg_cpu->util_rt  = cpu_util_rt(rq);
 }
 
 static unsigned long sugov_aggregate_util(struct sugov_cpu *sg_cpu)
 {
 	struct rq *rq = cpu_rq(sg_cpu->cpu);
+	unsigned long util;
 
-	if (rq->rt.rt_nr_running)
-		return sg_cpu->max;
+	if (rq->rt.rt_nr_running) {
+		util = sg_cpu->max;
+	} else {
+		util = sg_cpu->util_dl;
+		util += sg_cpu->util_cfs;
+		util += sg_cpu->util_rt;
+	}
 
 	/*
 	 * Utilization required by DEADLINE must always be granted while, for
@@ -198,7 +206,7 @@ static unsigned long sugov_aggregate_util(struct sugov_cpu *sg_cpu)
 	 * util_cfs + util_dl as requested freq. However, cpufreq is not yet
 	 * ready for such an interface. So, we only do the latter for now.
 	 */
-	return min(sg_cpu->max, (sg_cpu->util_dl + sg_cpu->util_cfs));
+	return min(sg_cpu->max, util);
 }
 
 static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time, unsigned int flags)
