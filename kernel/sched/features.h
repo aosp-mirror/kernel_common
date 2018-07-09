921514b7ca53bb3692604a4c3cdd961aab94abe5
Chris Redpath <chris.redpath@arm.com>
2018-07-09 15:44:00 +0100

    ANDROID: Add hold functionality to schedtune CPU boost
    When tasks come and go from a runqueue quickly, this can lead to boost
    being applied and removed quickly which sometimes means we cannot raise
    the CPU frequency again when we need to (due to the rate limit on
    frequency updates). This has proved to be a particular issue for RT tasks
    and alternative methods have been used in the past to work around it.
    
    This is an attempt to solve the issue for all task classes and cpufreq
    governors by introducing a generic mechanism in schedtune to retain
    the max boost level from task enqueue for a minimum period - defined
    here as 50ms. This timeout was determined experimentally and is not
    configurable.
    
    A sched_feat guards the application of this to tasks - in the default
    configuration, task boosting only applied to tasks which have RT
    policy. Change SCHEDTUNE_BOOST_HOLD_ALL to true to apply it to all
    tasks regardless of class.
    
    It works like so:
    
    Every task enqueue (in an allowed class) stores a cpu-local timestamp.
    If the task is not a member of an allowed class (all or RT depending
    upon feature selection), the timestamp is not updated.
    The boost group will stay active regardless of tasks present until
    50ms beyond the last timestamp stored. We also store the timestamp
    of the active boost group to avoid unneccesarily revisiting the boost
    groups when checking CPU boost level.
    
    If the timestamp is more than 50ms in the past when we check boost then
    we re-evaluate the boost groups for that CPU, taking into account the
    timestamps associated with each group.
    
    Idea based on rt-boost-retention patches from Joel.
    
    Change-Id: I52cc2d2e82d1c5aa03550378c8836764f41630c1
    Suggested-by: Joel Fernandes <joelaf@google.com>
    Reviewed-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    [forward ported from android-4.9-eas-dev proposal]
    (cherry picked from commit a485e8b7bf8e95759e600396feeb7bfb400b6e46)
    [ - Trivial cherry-pick conflicts in include/trace/events/sched.h ]
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -90,3 +90,14 @@ SCHED_FEAT(WA_BIAS, true)
  * UtilEstimation. Use estimated CPU utilization.
  */
 SCHED_FEAT(UTIL_EST, true)
+
+/*
+ * Apply schedtune boost hold to tasks of all sched classes.
+ * If enabled, schedtune will hold the boost applied to a CPU
+ * for 50ms regardless of task activation - if the task is
+ * still running 50ms later, the boost hold expires and schedtune
+ * boost will expire immediately the task stops.
+ * If disabled, this behaviour will only apply to tasks of the
+ * RT class.
+ */
+SCHED_FEAT(SCHEDTUNE_BOOST_HOLD_ALL, false)
