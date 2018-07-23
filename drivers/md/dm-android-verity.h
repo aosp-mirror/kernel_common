7c20442f5a6394c352d65dd7fc99dc1ee01bd985
Sandeep Patil <sspatil@google.com>
2018-07-23 16:31:32 -0700

    ANDROID: android-verity: Make it work with newer kernels
     Fixed bio API calls as they changed from 4.4 to 4.9.
     Fixed the driver to use the new verify_signature_one() API.
     Remove the dead code resulted from the rebase.
    
    Bug: 72722987
    Test: Build and boot hikey with system partition mounted as root using
          android-verity
    Signed-off-by: Sandeep Patil <sspatil@google.com>
    
    Change-Id: I1e29111d57b62f0451404c08d49145039dd00737
    
    [AmitP: Rebased and build fixes for v4.18]
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- drivers/md/dm-android-verity.h
+++ drivers/md/dm-android-verity.h
@@ -119,8 +119,14 @@ extern int dm_linear_prepare_ioctl(struct dm_target *ti, struct block_device **b
 extern int dm_linear_iterate_devices(struct dm_target *ti,
 			iterate_devices_callout_fn fn, void *data);
 extern int dm_linear_ctr(struct dm_target *ti, unsigned int argc, char **argv);
-extern long dm_linear_dax_direct_access(struct dm_target *ti, sector_t sector,
-                                 void **kaddr, pfn_t *pfn, long size);
+#if IS_ENABLED(CONFIG_DAX_DRIVER)
+extern long dm_linear_dax_direct_access(struct dm_target *ti, pgoff_t pgoff,
+					long nr_pages, void **kaddr,
+					pfn_t *pfn);
 extern size_t dm_linear_dax_copy_from_iter(struct dm_target *ti, pgoff_t pgoff,
 		void *addr, size_t bytes, struct iov_iter *i);
+#else
+#define dm_linear_dax_direct_access NULL
+#define dm_linear_dax_copy_from_iter NULL
+#endif
 #endif /* DM_ANDROID_VERITY_H */
