c3ad4c81ab45c05cb052deae243f85ac9d4796f2
Bart Van Assche <bart.vanassche@wdc.com>
2018-08-06 15:34:21 -0700

    UPSTREAM: lib/vsprintf: Do not handle %pO[^F] as %px
    This patch avoids that gcc reports the following when building with W=1:
    
    lib/vsprintf.c:1941:3: warning: this statement may fall through [-Wimplicit-fallthrough=]
       switch (fmt[1]) {
       ^~~~~~
    
    Fixes: 7b1924a1d930eb2 ("vsprintf: add printk specifier %px")
    Link: http://lkml.kernel.org/r/20180806223421.11995-1-bart.vanassche@wdc.com
    Cc: linux-kernel@vger.kernel.org
    Cc: Bart Van Assche <bart.vanassche@wdc.com>
    Cc: Pantelis Antoniou <pantelis.antoniou@konsulko.com>
    Cc: Joe Perches <joe@perches.com>
    Cc: Rob Herring <robh@kernel.org>
    Cc: v4.15+ <stable@vger.kernel.org>
    Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    (cherry picked from commit 554ec508653688c21d9b8024af73a1ffaa0164b9)
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Id9e304b8287d760bd0dd42b18e2bb92062cf4747

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -1979,6 +1979,7 @@ char *pointer(const char *fmt, char *buf, char *end, void *ptr,
 		case 'F':
 			return device_node_string(buf, end, ptr, spec, fmt + 1);
 		}
+		break;
 	case 'x':
 		return pointer_string(buf, end, ptr, spec);
 	}
