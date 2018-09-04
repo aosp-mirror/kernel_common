b56b8774a4959c4a3344bc3d99790f775ad9040b
Loic Poulain <loic.poulain@linaro.org>
2018-09-04 17:18:58 +0200

    usb: chipidea: Fix otg event handler
    [ Upstream commit 59739131e0ca06db7560f9073fff2fb83f6bc2a5 ]
    
    At OTG work running time, it's possible that several events need to be
    addressed (e.g. ID and VBUS events). The current implementation handles
    only one event at a time which leads to ignoring the other one. Fix it.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I25b7b426cf05284cd6ae9857137949c78c2c50d8

--- drivers/usb/chipidea/otg.c
+++ drivers/usb/chipidea/otg.c
@@ -99,11 +99,12 @@ static void ci_otg_work(struct work_struct *work)
 	if (ci->id_event) {
 		ci->id_event = false;
 		ci_handle_id_switch(ci);
-	} else if (ci->b_sess_valid_event) {
+	}
+
+	if (ci->b_sess_valid_event) {
 		ci->b_sess_valid_event = false;
 		ci_handle_vbus_change(ci);
-	} else
-		dev_err(ci->dev, "unexpected event occurs at %s\n", __func__);
+	}
 
 	enable_irq(ci->irq);
 }
