857260113eeb7c145dd939728eca0477394248f5
Mario Limonciello <mario.limonciello@dell.com>
2018-09-10 13:01:53 -0500

    platform/x86: alienware-wmi: Correct a memory leak
    commit ff0e9f26288d2daee4950f42b37a3d3d30d36ec1 upstream.
    
    An ACPI buffer that was allocated was not being freed after use.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@dell.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I6100406003af9c92606cf89bf111bee1342310f2

--- drivers/platform/x86/alienware-wmi.c
+++ drivers/platform/x86/alienware-wmi.c
@@ -465,6 +465,7 @@ static acpi_status alienware_hdmi_command(struct hdmi_args *in_args,
 		status = wmi_evaluate_method(WMAX_CONTROL_GUID, 1,
 					     command, &input, NULL);
 	}
+	kfree(output.pointer);
 	return status;
 }
 
