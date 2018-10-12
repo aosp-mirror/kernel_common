ebad3303e5e987948b0a33278b8c3d96ec057b0c
Hou Tao <houtao1@huawei.com>
2018-10-12 14:01:26 +0800

    jffs2: return -ERANGE when xattr buffer is too small
    When a file have multiple xattrs and the passed buffer is
    smaller than the required size, jffs2_listxattr() should
    return -ERANGE instead of continue, else Oops may occur
    due to memory corruption.
    
    Also remove the unnecessary check ("rc < 0"), because
    xhandle->list(...) will not return an error number.
    
    Spotted by generic/377 in xfstests-dev.
    
    NB: The problem had been fixed by commit 764a5c6b1fa4 ("xattr
    handlers: Simplify list operation") in v4.5-rc1, but the
    modification in that commit may be too much because it modifies
    all file-systems which implement xattr, so I create a single
    patch for jffs2 to fix the problem.
    
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I3458a2dfae080fbaa11739ce22774f9c8866ce4b

--- fs/jffs2/xattr.c
+++ fs/jffs2/xattr.c
@@ -1003,12 +1003,14 @@ ssize_t jffs2_listxattr(struct dentry *dentry, char *buffer, size_t size)
 		if (buffer) {
 			rc = xhandle->list(dentry, buffer+len, size-len,
 					   xd->xname, xd->name_len, xd->flags);
+			if (rc > size - len) {
+				rc = -ERANGE;
+				goto out;
+			}
 		} else {
 			rc = xhandle->list(dentry, NULL, 0, xd->xname,
 					   xd->name_len, xd->flags);
 		}
-		if (rc < 0)
-			goto out;
 		len += rc;
 	}
 	rc = len;
