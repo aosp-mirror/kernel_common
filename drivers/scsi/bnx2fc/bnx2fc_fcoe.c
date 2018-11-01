99ad99673a7aec9d5297506279cd40656ef5b1f7
Dan Carpenter <dan.carpenter@oracle.com>
2018-11-01 08:25:30 +0300

    scsi: bnx2fc: Fix NULL dereference in error handling
    [ Upstream commit 9ae4f8420ed7be4b13c96600e3568c144d101a23 ]
    
    If "interface" is NULL then we can't release it and trying to will only
    lead to an Oops.
    
    Fixes: aea71a024914 ("[SCSI] bnx2fc: Introduce interface structure for each vlan interface")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I68e0a1f8cf5fbd9ecb39d4d0ee312dafe2010a9f

--- drivers/scsi/bnx2fc/bnx2fc_fcoe.c
+++ drivers/scsi/bnx2fc/bnx2fc_fcoe.c
@@ -2208,7 +2208,8 @@ static int _bnx2fc_create(struct net_device *netdev,
 	interface = bnx2fc_interface_create(hba, netdev, fip_mode);
 	if (!interface) {
 		printk(KERN_ERR PFX "bnx2fc_interface_create failed\n");
-		goto ifput_err;
+		rc = -ENOMEM;
+		goto netdev_err;
 	}
 
 	if (netdev->priv_flags & IFF_802_1Q_VLAN) {
