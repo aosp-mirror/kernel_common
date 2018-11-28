ca90e2fe0aedaf2b458776931f2f1de7af413a13
Pan Bian <bianpan2016@163.com>
2018-11-28 10:57:33 +0800

    ubi: Put MTD device after it is not used
    [ Upstream commit b95f83ab762dd6211351b9140f99f43644076ca8 ]
    
    The MTD device reference is dropped via put_mtd_device, however its
    field ->index is read and passed to ubi_msg. To fix this, the patch
    moves the reference dropping after calling ubi_msg.
    
    Signed-off-by: Pan Bian <bianpan2016@163.com>
    Reviewed-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Ib3ea8750f9ecb8b87eb3efca91796211a5366b91

--- drivers/mtd/ubi/build.c
+++ drivers/mtd/ubi/build.c
@@ -1147,10 +1147,10 @@ int ubi_detach_mtd_dev(int ubi_num, int anyway)
 	ubi_wl_close(ubi);
 	ubi_free_internal_volumes(ubi);
 	vfree(ubi->vtbl);
-	put_mtd_device(ubi->mtd);
 	vfree(ubi->peb_buf);
 	vfree(ubi->fm_buf);
 	ubi_msg("mtd%d is detached from ubi%d", ubi->mtd->index, ubi->ubi_num);
+	put_mtd_device(ubi->mtd);
 	put_device(&ubi->dev);
 	return 0;
 }
