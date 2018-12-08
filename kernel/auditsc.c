36ed72ee527e568a5fda22f67a0c91b3c37570a5
mifl <mifl.pc@gmail.com>
2018-12-08 11:19:54 +0100

    audit: remove duplicated uaccess header
    Preserved the structure of the linux kernel to avoid future conflicts
    
    Change-Id: Ie1567ee47a82435cb2d97f09601cac7074e130a4
    Signed-off-by: mifl <mifl.pc@gmail.com>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -72,7 +72,6 @@
 #include <linux/fs_struct.h>
 #include <linux/compat.h>
 #include <linux/ctype.h>
-#include <linux/uaccess.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
 #include <uapi/linux/limits.h>
