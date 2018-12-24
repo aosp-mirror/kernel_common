6a1aab6950f7c38937be7129c1040c7c7a4cd448
Aditya Pakki <pakki001@umn.edu>
2018-12-23 19:42:38 -0600

    net/net_namespace: Check the return value of register_pernet_subsys()
    [ Upstream commit 0eb987c874dc93f9c9d85a6465dbde20fdd3884c ]
    
    In net_ns_init(), register_pernet_subsys() could fail while registering
    network namespace subsystems. The fix checks the return value and
    sends a panic() on failure.
    
    Signed-off-by: Aditya Pakki <pakki001@umn.edu>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Ieca2cb70bf66d6088e363a376997c15e2e1ad8ae

--- net/core/net_namespace.c
+++ net/core/net_namespace.c
@@ -450,7 +450,8 @@ static int __init net_ns_init(void)
 
 	mutex_unlock(&net_mutex);
 
-	register_pernet_subsys(&net_ns_ops);
+	if (register_pernet_subsys(&net_ns_ops))
+		panic("Could not register network namespace subsystems");
 
 	return 0;
 }
