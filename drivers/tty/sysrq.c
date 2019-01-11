56cba53e6f5cb4f9ddde5e6227ad92813b0a4ca6
Petr Mladek <pmladek@suse.com>
2019-01-11 17:20:37 +0100

    sysrq: Remove duplicated sysrq message
    commit c3fee60908db4a8594f2e4a2131998384b8fa006 upstream.
    
    The commit 97f5f0cd8cd0a0544 ("Input: implement SysRq as a separate input
    handler") added pr_fmt() definition. It caused a duplicated message
    prefix in the sysrq header messages, for example:
    
    [  177.053931] sysrq: SysRq : Show backtrace of all active CPUs
    [  742.864776] sysrq: SysRq : HELP : loglevel(0-9) reboot(b) crash(c)
    
    Fixes: 97f5f0cd8cd0a05 ("Input: implement SysRq as a separate input handler")
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Tommi Rantala  <tommi.t.rantala@nokia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Ia578acee571a71cd4eaa3766de5b9e58e6ed71f5

--- drivers/tty/sysrq.c
+++ drivers/tty/sysrq.c
@@ -524,7 +524,6 @@ void __handle_sysrq(int key, bool check_mask)
 	 */
 	orig_log_level = console_loglevel;
 	console_loglevel = CONSOLE_LOGLEVEL_DEFAULT;
-	printk(KERN_INFO "SysRq : ");
 
         op_p = __sysrq_get_key_op(key);
         if (op_p) {
@@ -533,15 +532,15 @@ void __handle_sysrq(int key, bool check_mask)
 		 * should not) and is the invoked operation enabled?
 		 */
 		if (!check_mask || sysrq_on_mask(op_p->enable_mask)) {
-			printk("%s\n", op_p->action_msg);
+			pr_info("%s\n", op_p->action_msg);
 			console_loglevel = orig_log_level;
 			op_p->handler(key);
 		} else {
-			printk("This sysrq operation is disabled.\n");
+			pr_info("This sysrq operation is disabled.\n");
 			console_loglevel = orig_log_level;
 		}
 	} else {
-		printk("HELP : ");
+		pr_info("HELP : ");
 		/* Only print the help msg once per handler */
 		for (i = 0; i < ARRAY_SIZE(sysrq_key_table); i++) {
 			if (sysrq_key_table[i]) {
