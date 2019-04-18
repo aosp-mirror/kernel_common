ea8f44f4f2478e3c0516922f4ceb5c05d3830ff0
Pan Bian <bianpan2016@163.com>
2019-04-18 10:27:18 +0800

    EDAC/sysfs: Fix memory leak when creating a csrow object
    [ Upstream commit 585fb3d93d32dbe89e718b85009f9c322cc554cd ]
    
    In edac_create_csrow_object(), the reference to the object is not
    released when adding the device to the device hierarchy fails
    (device_add()). This may result in a memory leak.
    
    Signed-off-by: Pan Bian <bianpan2016@163.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: James Morse <james.morse@arm.com>
    Cc: Mauro Carvalho Chehab <mchehab@kernel.org>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Link: https://lkml.kernel.org/r/1555554438-103953-1-git-send-email-bianpan2016@163.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Ice69726fe2456e9f711d2695a2b81a1366ca8e24

--- drivers/edac/edac_mc_sysfs.c
+++ drivers/edac/edac_mc_sysfs.c
@@ -387,8 +387,10 @@ static int edac_create_csrow_object(struct mem_ctl_info *mci,
 		 dev_name(&csrow->dev));
 
 	err = device_add(&csrow->dev);
-	if (err < 0)
+	if (err < 0) {
+		put_device(&csrow->dev);
 		return err;
+	}
 
 	for (chan = 0; chan < csrow->nr_channels; chan++) {
 		/* Only expose populated DIMMs */
