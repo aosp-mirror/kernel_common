2140ac9dbed8777d58fa60aa51851ec8feb5840e
Kyle Tso <kyletso@google.com>
2019-06-18 16:25:00 +0800

    ANDROID: usb: typec: tcpm: Fix incorrect SVDM version in the responses
    The value of SVDM version in the reponses to the DISC_IDENT is not set
    to the correct value. Update it.
    
    Bug: 142545898
    Change-Id: I784f20ddab81604b67146a177d601cc865a5c962
    Signed-off-by: Kyle Tso <kyletso@google.com>
    (cherry picked from commit a792329510a589c416975468661ffb499ca049ae)
    (cherry picked from commit b3dd0c424d2b3c4d0b2345a52deab535382f6a87)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -1482,6 +1482,8 @@ static int tcpm_pd_svdm(struct tcpm_port *port, struct typec_altmode *adev,
 			response[0] = p[0] | VDO_CMDT(CMDT_RSP_BUSY);
 			rlen = 1;
 		}
+		response[0] = (response[0] & ~VDO_SVDM_VERS_MASK) |
+			      VDO_SVDM_VERS(port->svdm_version);
 		break;
 	case CMDT_RSP_ACK:
 		/* silently drop message if we are not connected */
@@ -1546,6 +1548,8 @@ static int tcpm_pd_svdm(struct tcpm_port *port, struct typec_altmode *adev,
 			/* Unrecognized SVDM */
 			response[0] = p[0] | VDO_CMDT(CMDT_RSP_NAK);
 			rlen = 1;
+			response[0] = (response[0] & ~VDO_SVDM_VERS_MASK) |
+				      VDO_SVDM_VERS(port->svdm_version);
 			break;
 		}
 		tcpm_ams_finish(port);
@@ -1564,6 +1568,8 @@ static int tcpm_pd_svdm(struct tcpm_port *port, struct typec_altmode *adev,
 			/* Unrecognized SVDM */
 			response[0] = p[0] | VDO_CMDT(CMDT_RSP_NAK);
 			rlen = 1;
+			response[0] = (response[0] & ~VDO_SVDM_VERS_MASK) |
+				      VDO_SVDM_VERS(port->svdm_version);
 			break;
 		}
 		port->vdm_sm_running = false;
@@ -1572,6 +1578,8 @@ static int tcpm_pd_svdm(struct tcpm_port *port, struct typec_altmode *adev,
 	default:
 		response[0] = p[0] | VDO_CMDT(CMDT_RSP_NAK);
 		rlen = 1;
+		response[0] = (response[0] & ~VDO_SVDM_VERS_MASK) |
+			      VDO_SVDM_VERS(port->svdm_version);
 		port->vdm_sm_running = false;
 		break;
 	}
