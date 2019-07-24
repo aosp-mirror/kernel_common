a5bfd2c6297fca47f7235ef517cbd84d19f45a6e
Steve French <stfrench@microsoft.com>
2019-07-23 22:14:29 -0500

    cifs: fix rmmod regression in cifs.ko caused by force_sig changes
    [ Upstream commit 247bc9470b1eeefc7b58cdf2c39f2866ba651509 ]
    
    Fixes: 72abe3bcf091 ("signal/cifs: Fix cifs_put_tcp_session to call send_sig instead of force_sig")
    
    The global change from force_sig caused module unloading of cifs.ko
    to fail (since the cifsd process could not be killed, "rmmod cifs"
    now would always fail)
    
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Reviewed-by: Ronnie Sahlberg <lsahlber@redhat.com>
    CC: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I5fc49bec8d80c7739b36e4cf53adf0d706cc742f

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -876,6 +876,7 @@ cifs_demultiplex_thread(void *p)
 				GFP_KERNEL);
 
 	set_freezable();
+	allow_signal(SIGKILL);
 	while (server->tcpStatus != CifsExiting) {
 		if (try_to_freeze())
 			continue;
