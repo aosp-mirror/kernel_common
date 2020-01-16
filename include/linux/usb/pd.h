b6c76028c8130f2039b45475fb1ae2635f60577b
Badhri Jagan Sridharan <badhri@google.com>
2020-01-15 22:19:31 -0800

    ANDROID: usb: pd: timer adjustments for compliance tests
    Bug: 139192601
    Change-Id: I1a6ce945abe53078d50b25b368341b6095969911
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    (cherry picked from commit 9c50a917b7043ac042c63416483e2038e2634c05)
    (cherry picked from commit 0cf7bea5370cc1a46bc9e76709d82db410dc9952)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- include/linux/usb/pd.h
+++ include/linux/usb/pd.h
@@ -455,15 +455,15 @@ static inline unsigned int rdo_max_power(u32 rdo)
 #define PD_T_NO_RESPONSE	5000	/* 4.5 - 5.5 seconds */
 #define PD_T_DB_DETECT		10000	/* 10 - 15 seconds */
 #define PD_T_SEND_SOURCE_CAP	150	/* 100 - 200 ms */
-#define PD_T_SENDER_RESPONSE	60	/* 24 - 30 ms, relaxed */
+#define PD_T_SENDER_RESPONSE	26	/* 24 - 30 ms, relaxed */
 #define PD_T_SOURCE_ACTIVITY	45
 #define PD_T_SINK_ACTIVITY	135
-#define PD_T_SINK_WAIT_CAP	240
+#define PD_T_SINK_WAIT_CAP	450 /* 450 for compliance */
 #define PD_T_PS_TRANSITION	500
 #define PD_T_SRC_TRANSITION	35
 #define PD_T_DRP_SNK		40
 #define PD_T_DRP_SRC		30
-#define PD_T_PS_SOURCE_OFF	920
+#define PD_T_PS_SOURCE_OFF	870
 #define PD_T_PS_SOURCE_ON	480
 #define PD_T_PS_SOURCE_ON_PRS	450	/* 390 - 480ms */
 #define PD_T_PS_HARD_RESET	30
@@ -482,7 +482,7 @@ static inline unsigned int rdo_max_power(u32 rdo)
 #define PD_T_DRP_TRY		100	/* 75 - 150 ms */
 #define PD_T_DRP_TRYWAIT	600	/* 400 - 800 ms */
 
-#define PD_T_CC_DEBOUNCE	200	/* 100 - 200 ms */
+#define PD_T_CC_DEBOUNCE	170	/* 100 - 200 ms */
 #define PD_T_PD_DEBOUNCE	20	/* 10 - 20 ms */
 
 #define PD_N_CAPS_COUNT		(PD_T_NO_RESPONSE / PD_T_SEND_SOURCE_CAP)
