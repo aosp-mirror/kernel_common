fe1425c169538fb60e9da57a1a096ed8508c8328
Alistair Delva <adelva@google.com>
2020-03-02 12:50:07 -0800

    NOUPSTREAM: ANDROID: Unconditionally create bridge tracepoints
    Even if the bridge module is not enabled, we may need the tracepoints
    downstream in products that enable bridge.ko, so avoid defining the
    export of these symbols based on a config option.
    
    [CPNOTE: 20/07/21] Lee: Androidness
    
    Bug: 150625937
    Change-Id: Ib961fd6e353fe3bdfde11a38488568f42f1dbe7a
    Signed-off-by: Alistair Delva <adelva@google.com>
    Signed-off-by: Lee Jones <joneslee@google.com>

--- net/core/net-traces.c
+++ net/core/net-traces.c
@@ -35,14 +35,12 @@
 #include <trace/events/tcp.h>
 #include <trace/events/fib.h>
 #include <trace/events/qdisc.h>
-#if IS_ENABLED(CONFIG_BRIDGE)
 #include <trace/events/bridge.h>
 EXPORT_TRACEPOINT_SYMBOL_GPL(br_fdb_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(br_fdb_external_learn_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(fdb_delete);
 EXPORT_TRACEPOINT_SYMBOL_GPL(br_fdb_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(br_mdb_full);
-#endif
 
 #if IS_ENABLED(CONFIG_PAGE_POOL)
 #include <trace/events/page_pool.h>
