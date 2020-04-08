6722a51bc7e89cfe6d2944d13467a8768577d46c
Bodo Stroesser <bstroesser@ts.fujitsu.com>
2020-04-08 15:26:10 +0200

    scsi: target: fix PR IN / READ FULL STATUS for FC
    [ Upstream commit 8fed04eb79a74cbf471dfaa755900a51b37273ab ]
    
    Creation of the response to READ FULL STATUS fails for FC based
    reservations. Reason is the too high loop limit (< 24) in
    fc_get_pr_transport_id(). The string representation of FC WWPN is 23 chars
    long only ("11:22:33:44:55:66:77:88"). So when i is 23, the loop body is
    executed a last time for the ending '\0' of the string and thus hex2bin()
    reports an error.
    
    Link: https://lore.kernel.org/r/20200408132610.14623-3-bstroesser@ts.fujitsu.com
    Signed-off-by: Bodo Stroesser <bstroesser@ts.fujitsu.com>
    Reviewed-by: Mike Christie <mchristi@redhat.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I44405aa8d8e5b8abae2466db275b233f62aa80a2

--- drivers/target/target_core_fabric_lib.c
+++ drivers/target/target_core_fabric_lib.c
@@ -170,7 +170,7 @@ u32 fc_get_pr_transport_id(
 	 */
 	ptr = &se_nacl->initiatorname[0];
 
-	for (i = 0; i < 24; ) {
+	for (i = 0; i < 23; ) {
 		if (!strncmp(&ptr[i], ":", 1)) {
 			i++;
 			continue;
