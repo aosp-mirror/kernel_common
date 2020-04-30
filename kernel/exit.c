49ae84a00312d474a3e916ae58aaf588d3454eba
Edgar Arriaga García <edgararriaga@google.com>
2020-04-30 00:34:09 +0000

    BACKPORT: pid: move pidfd_get_pid() to pid.c
    process_madvise syscall needs pidfd_get_pid function to translate pidfd to
    pid so this patch move the function to kernel/pid.c.
    
    Link: http://lkml.kernel.org/r/20200302193630.68771-5-minchan@kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Suggested-by: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Reviewed-by: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Jann Horn <jannh@google.com>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Daniel Colascione <dancol@google.com>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: John Dias <joaodias@google.com>
    Cc: Kirill Tkhai <ktkhai@virtuozzo.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oleksandr Natalenko <oleksandr@redhat.com>
    Cc: Sandeep Patil <sspatil@google.com>
    Cc: SeongJae Park <sj38.park@gmail.com>
    Cc: SeongJae Park <sjpark@amazon.de>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Sonny Rao <sonnyrao@google.com>
    Cc: Tim Murray <timmurray@google.com>
    Cc: <linux-man@vger.kernel.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit 1aa92cd31c1c032ddfed27e79d646bbb429e9b52)
    
    Conflicts:
    	kernel/pid.c
    	kernel/exit.c
    1. included linux/file.h library which contains definition for
    struct fd.
    
    2. Added flags argument required for updated pidfd_get_pid
    function in exit.c
    
    Bug: 153444106
    Test: Built kernel
    
    Signed-off-by: Edgar Arriaga García <edgararriaga@google.com>
    Change-Id: I9383bebe450adb2475a990808e7cafa8cd631afa

--- kernel/exit.c
+++ kernel/exit.c
@@ -1481,23 +1481,6 @@ end:
 	return retval;
 }
 
-static struct pid *pidfd_get_pid(unsigned int fd)
-{
-	struct fd f;
-	struct pid *pid;
-
-	f = fdget(fd);
-	if (!f.file)
-		return ERR_PTR(-EBADF);
-
-	pid = pidfd_pid(f.file);
-	if (!IS_ERR(pid))
-		get_pid(pid);
-
-	fdput(f);
-	return pid;
-}
-
 static long kernel_waitid(int which, pid_t upid, struct waitid_info *infop,
 			  int options, struct rusage *ru)
 {
@@ -1505,6 +1488,7 @@ static long kernel_waitid(int which, pid_t upid, struct waitid_info *infop,
 	struct pid *pid = NULL;
 	enum pid_type type;
 	long ret;
+	unsigned int f_flags;
 
 	if (options & ~(WNOHANG|WNOWAIT|WEXITED|WSTOPPED|WCONTINUED|
 			__WNOTHREAD|__WCLONE|__WALL))
@@ -1538,7 +1522,7 @@ static long kernel_waitid(int which, pid_t upid, struct waitid_info *infop,
 		if (upid < 0)
 			return -EINVAL;
 
-		pid = pidfd_get_pid(upid);
+		pid = pidfd_get_pid(upid, &f_flags);
 		if (IS_ERR(pid))
 			return PTR_ERR(pid);
 		break;
--- kernel/pid.c
+++ kernel/pid.c
@@ -41,6 +41,7 @@
 #include <linux/anon_inodes.h>
 #include <linux/sched/signal.h>
 #include <linux/sched/task.h>
+#include <linux/file.h>
 #include <linux/idr.h>
 
 struct pid init_struct_pid = {
@@ -453,6 +454,25 @@ EXPORT_SYMBOL_GPL(task_active_pid_ns);
 struct pid *find_ge_pid(int nr, struct pid_namespace *ns)
 {
 	return idr_get_next(&ns->idr, &nr);
+}
+
+struct pid *pidfd_get_pid(unsigned int fd, unsigned int *flags)
+{
+	struct fd f;
+	struct pid *pid;
+
+	f = fdget(fd);
+	if (!f.file)
+		return ERR_PTR(-EBADF);
+
+	pid = pidfd_pid(f.file);
+	if (!IS_ERR(pid)) {
+		get_pid(pid);
+		*flags = f.file->f_flags;
+	}
+
+	fdput(f);
+	return pid;
 }
 
 /**
