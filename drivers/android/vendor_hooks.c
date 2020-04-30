4082ebee733c9c311fa543eaa0c15d2011020af1
Todd Kjos <tkjos@google.com>
2020-04-30 16:14:25 -0700

    [DO NOT SUBMIT] Example vendor hook
    This is not intended for merging. This patch
    contains an example of adding a vendor hook
    to the kernel.
    
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: I90ff9469ffe2c95f50caa72571d8e31efd088f81

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -8,9 +8,11 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/hooks/vendor_hooks.h>
+#include <trace/hooks/sched.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
  */
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sched_exit);
 
