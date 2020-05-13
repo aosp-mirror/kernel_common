77a5e7c3a7499e383260cf0eb106dfc0f8595a12
Martijn Coenen <maco@android.com>
2020-05-13 15:38:43 +0200

    BACKPORT: loop: Rework lo_ioctl() __user argument casting
    In preparation for a new ioctl that needs to copy_from_user(); makes the
    code easier to read as well.
    
    Signed-off-by: Martijn Coenen <maco@android.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Bug: 148607611
    (cherry picked from commit 571fae6e290d64a3e8132c455e7786c99c467ed1)
    Change-Id: I61cb8a6771f517f19ea87a145ab02827f46ab850

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1430,6 +1430,7 @@ static int lo_ioctl(struct block_device *bdev, fmode_t mode,
 	unsigned int cmd, unsigned long arg)
 {
 	struct loop_device *lo = bdev->bd_disk->private_data;
+	void __user *argp = (void __user *) arg;
 	int err;
 
 	mutex_lock_nested(&lo->lo_ctl_mutex, 1);
@@ -1449,20 +1450,18 @@ static int lo_ioctl(struct block_device *bdev, fmode_t mode,
 	case LOOP_SET_STATUS:
 		err = -EPERM;
 		if ((mode & FMODE_WRITE) || capable(CAP_SYS_ADMIN))
-			err = loop_set_status_old(lo,
-					(struct loop_info __user *)arg);
+			err = loop_set_status_old(lo, argp);
 		break;
 	case LOOP_GET_STATUS:
-		err = loop_get_status_old(lo, (struct loop_info __user *) arg);
+		err = loop_get_status_old(lo, argp);
 		break;
 	case LOOP_SET_STATUS64:
 		err = -EPERM;
 		if ((mode & FMODE_WRITE) || capable(CAP_SYS_ADMIN))
-			err = loop_set_status64(lo,
-					(struct loop_info64 __user *) arg);
+			err = loop_set_status64(lo, argp);
 		break;
 	case LOOP_GET_STATUS64:
-		err = loop_get_status64(lo, (struct loop_info64 __user *) arg);
+		err = loop_get_status64(lo, argp);
 		break;
 	case LOOP_SET_CAPACITY:
 		err = -EPERM;
