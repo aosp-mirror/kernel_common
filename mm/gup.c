af177e58654c354f49791d21173daa669889e61d
Peter Xu <peterx@redhat.com>
2020-05-13 17:50:41 -0700

    UPSTREAM: mm/gup: fix fixup_user_fault() on multiple retries
    This part was overlooked when reworking the gup code on multiple
    retries.
    
    When we get the 2nd+ retry, we'll be with TRIED flag set.  Current code
    will bail out on the 2nd retry because the !TRIED check will fail so the
    retry logic will be skipped.  What's worse is that, it will also return
    zero which errornously hints the caller that the page is faulted in
    while it's not.
    
    The !TRIED flag check seems to not be needed even before the mutliple
    retries change because if we get a VM_FAULT_RETRY, it must be the 1st
    retry, and we should not have TRIED set for that.
    
    Fix it by removing the !TRIED check, at the meantime check against fatal
    signals properly before the page fault so we can still properly respond
    to the user killing the process during retries.
    
    Fixes: 4426e945df58 ("mm/gup: allow VM_FAULT_RETRY for multiple times")
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Brian Geffon <bgeffon@google.com>
    Link: http://lkml.kernel.org/r/20200502003523.8204-1-peterx@redhat.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 475f4dfc021c5fde69f3b7d3287bde0a50477b05)
    
    Bug: 176847924
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Change-Id: I51dbc07fc845b346691f9bcf4846a1d9355cbe71

--- mm/gup.c
+++ mm/gup.c
@@ -976,6 +976,10 @@ retry:
 	if (!vma_permits_fault(vma, fault_flags))
 		return -EFAULT;
 
+	if ((fault_flags & FAULT_FLAG_KILLABLE) &&
+	    fatal_signal_pending(current))
+		return -EINTR;
+
 	ret = handle_mm_fault(vma, address, fault_flags);
 	major |= ret & VM_FAULT_MAJOR;
 	if (ret & VM_FAULT_ERROR) {
@@ -988,11 +992,9 @@ retry:
 
 	if (ret & VM_FAULT_RETRY) {
 		down_read(&mm->mmap_sem);
-		if (!(fault_flags & FAULT_FLAG_TRIED)) {
-			*unlocked = true;
-			fault_flags |= FAULT_FLAG_TRIED;
-			goto retry;
-		}
+		*unlocked = true;
+		fault_flags |= FAULT_FLAG_TRIED;
+		goto retry;
 	}
 
 	if (tsk) {
