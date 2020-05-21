2735339966ed1b8c4fa175baefce6024283763a3
Arve Hjønnevåg <arve@android.com>
2020-05-21 13:02:28 -0700

    arm64: smp: Fix trace crash with custom ipi interrupt handlers
    Add NR_IPI guard around trace_ipi_raise. If a custom ipi is enabled, it
    would trigger this trace event with an out of bounds array index. Other
    trace_ipi_ calls are already guarded.
    
    Bug: 156673764
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Change-Id: Ic493ef3fcfb2a38916910b6a12a7cebd05e74362

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -795,7 +795,8 @@ static const char *ipi_types[NR_IPI] __tracepoint_string = {
 
 static void smp_cross_call(const struct cpumask *target, unsigned int ipinr)
 {
-	trace_ipi_raise(target, ipi_types[ipinr]);
+	if ((unsigned)ipinr < NR_IPI)
+		trace_ipi_raise(target, ipi_types[ipinr]);
 	__smp_cross_call(target, ipinr);
 }
 
