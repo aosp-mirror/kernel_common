98d978169fa2330fc7308e752fb03cf49f946829
Christian Brauner <christian.brauner@ubuntu.com>
2020-06-01 11:50:07 -0700

    UPSTREAM: seccomp: Lift wait_queue into struct seccomp_filter
    Lift the wait_queue from struct notification into struct seccomp_filter.
    This is cleaner overall and lets us avoid having to take the notifier
    mutex in the future for EPOLLHUP notifications since we need to neither
    read nor modify the notifier specific aspects of the seccomp filter. In
    the exit path I'd very much like to avoid having to take the notifier mutex
    for each filter in the task's filter hierarchy.
    
    Cc: Tycho Andersen <tycho@tycho.ws>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Matt Denton <mpdenton@google.com>
    Cc: Sargun Dhillon <sargun@sargun.me>
    Cc: Jann Horn <jannh@google.com>
    Cc: Chris Palmer <palmer@google.com>
    Cc: Aleksa Sarai <cyphar@cyphar.com>
    Cc: Robert Sesek <rsesek@google.com>
    Cc: Jeffrey Vander Stoep <jeffv@google.com>
    Cc: Linux Containers <containers@lists.linux-foundation.org>
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    (cherry picked from commit 76194c4e830d570d9e369d637bb907591d2b3111)
    Signed-off-by: Jeff Vander Stoep <jeffv@google.com>
    Bug: 176068146
    Change-Id: Id87b359f1155d26068cd62d5295dc39849d46495

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -102,13 +102,11 @@ struct seccomp_knotif {
  *           filter->notify_lock.
  * @next_id: The id of the next request.
  * @notifications: A list of struct seccomp_knotif elements.
- * @wqh: A wait queue for poll.
  */
 struct notification {
 	struct semaphore request;
 	u64 next_id;
 	struct list_head notifications;
-	wait_queue_head_t wqh;
 };
 
 /**
@@ -124,6 +122,7 @@ struct notification {
  * @prog: the BPF program to evaluate
  * @notif: the struct that holds all notification related information
  * @notify_lock: A lock for all notification-related accesses.
+ * @wqh: A wait queue for poll if a notifier is in use.
  *
  * seccomp_filter objects are organized in a tree linked via the @prev
  * pointer.  For any task, it appears to be a singly-linked list starting
@@ -142,6 +141,7 @@ struct seccomp_filter {
 	struct bpf_prog *prog;
 	struct notification *notif;
 	struct mutex notify_lock;
+	wait_queue_head_t wqh;
 };
 
 /* Limit any path through the tree to 256KB worth of instructions. */
@@ -510,6 +510,7 @@ static struct seccomp_filter *seccomp_prepare_filter(struct sock_fprog *fprog)
 	}
 
 	refcount_set(&sfilter->refs, 1);
+	init_waitqueue_head(&sfilter->wqh);
 
 	return sfilter;
 }
@@ -777,7 +778,7 @@ static void seccomp_do_user_notification(int this_syscall,
 	list_add(&n.list, &match->notif->notifications);
 
 	up(&match->notif->request);
-	wake_up_poll(&match->notif->wqh, EPOLLIN | EPOLLRDNORM);
+	wake_up_poll(&match->wqh, EPOLLIN | EPOLLRDNORM);
 	mutex_unlock(&match->notify_lock);
 
 	/*
@@ -1092,7 +1093,7 @@ static long seccomp_notify_recv(struct seccomp_filter *filter,
 	unotif.data = *(knotif->data);
 
 	knotif->state = SECCOMP_NOTIFY_SENT;
-	wake_up_poll(&filter->notif->wqh, EPOLLOUT | EPOLLWRNORM);
+	wake_up_poll(&filter->wqh, EPOLLOUT | EPOLLWRNORM);
 	ret = 0;
 out:
 	mutex_unlock(&filter->notify_lock);
@@ -1207,7 +1208,7 @@ static __poll_t seccomp_notify_poll(struct file *file,
 	__poll_t ret = 0;
 	struct seccomp_knotif *cur;
 
-	poll_wait(file, &filter->notif->wqh, poll_tab);
+	poll_wait(file, &filter->wqh, poll_tab);
 
 	if (mutex_lock_interruptible(&filter->notify_lock) < 0)
 		return EPOLLERR;
@@ -1245,7 +1246,6 @@ static struct file *init_listener(struct seccomp_filter *filter)
 	sema_init(&filter->notif->request, 0);
 	filter->notif->next_id = get_random_u64();
 	INIT_LIST_HEAD(&filter->notif->notifications);
-	init_waitqueue_head(&filter->notif->wqh);
 
 	ret = anon_inode_getfile("seccomp notify", &seccomp_notify_ops,
 				 filter, O_RDWR);
