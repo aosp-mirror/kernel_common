4ffebe49ed8a9b6f89713fbaf11469d74b0bd86f
Alex Williamson <alex.williamson@redhat.com>
2020-06-16 15:26:36 -0600

    vfio/pci: Clear error and request eventfd ctx after releasing
    [ Upstream commit 5c5866c593bbd444d0339ede6a8fb5f14ff66d72 ]
    
    The next use of the device will generate an underflow from the
    stale reference.
    
    Cc: Qian Cai <cai@lca.pw>
    Fixes: 1518ac272e78 ("vfio/pci: fix memory leaks of eventfd ctx")
    Reported-by: Daniel Wagner <dwagner@suse.de>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Tested-by: Daniel Wagner <dwagner@suse.de>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [Lee: Remove references to non-existent 'req_trigger']
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I9228a67dfdc5492547736dcac69995ad1883ab78

--- drivers/vfio/pci/vfio_pci.c
+++ drivers/vfio/pci/vfio_pci.c
@@ -181,8 +181,10 @@ static void vfio_pci_release(void *device_data)
 	if (!(--vdev->refcnt)) {
 		vfio_spapr_pci_eeh_release(vdev->pdev);
 		vfio_pci_disable(vdev);
-		if (vdev->err_trigger)
+		if (vdev->err_trigger) {
 			eventfd_ctx_put(vdev->err_trigger);
+			vdev->err_trigger = NULL;
+		}
 	}
 
 	mutex_unlock(&driver_lock);
