cbe59e17494c6f55a837b10877d88e9c246ef7f5
Greg Kroah-Hartman <gregkh@google.com>
2020-06-25 10:48:36 +0200

    ANDROID: GKI: USB: Gadget: add Android ABI padding to struct usb_gadget
    Over time, this structure is being changed a bunch to fix issues found
    in the gadget code as more and more people stress it.  So add padding to
    handle any needed future changes that might occur.
    
    Of course, now that the padding is there, there never will be any need
    to change this structure again...
    
    Bug: 151154716
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I496797469268cc5d3cfafdb19c141b6b6196fe72

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -25,6 +25,7 @@
 #include <linux/types.h>
 #include <linux/workqueue.h>
 #include <linux/usb/ch9.h>
+#include <linux/android_kabi.h>
 
 #define UDC_TRACE_STR_MAX	512
 
@@ -427,6 +428,11 @@ struct usb_gadget {
 	unsigned			deactivated:1;
 	unsigned			connected:1;
 	unsigned			lpm_capable:1;
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
 };
 #define work_to_gadget(w)	(container_of((w), struct usb_gadget, work))
 
