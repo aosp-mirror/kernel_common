7ecc1cf324ef7950972b243153ac02829a05c317
Qais Yousef <qais.yousef@arm.com>
2020-07-16 12:03:45 +0100

    BACKPORT: sched/uclamp: Add a new sysctl to control RT default boost value
    RT tasks by default run at the highest capacity/performance level. When
    uclamp is selected this default behavior is retained by enforcing the
    requested uclamp.min (p->uclamp_req[UCLAMP_MIN]) of the RT tasks to be
    uclamp_none(UCLAMP_MAX), which is SCHED_CAPACITY_SCALE; the maximum
    value.
    
    This is also referred to as 'the default boost value of RT tasks'.
    
    See commit 1a00d999971c ("sched/uclamp: Set default clamps for RT tasks").
    
    On battery powered devices, it is desired to control this default
    (currently hardcoded) behavior at runtime to reduce energy consumed by
    RT tasks.
    
    For example, a mobile device manufacturer where big.LITTLE architecture
    is dominant, the performance of the little cores varies across SoCs, and
    on high end ones the big cores could be too power hungry.
    
    Given the diversity of SoCs, the new knob allows manufactures to tune
    the best performance/power for RT tasks for the particular hardware they
    run on.
    
    They could opt to further tune the value when the user selects
    a different power saving mode or when the device is actively charging.
    
    The runtime aspect of it further helps in creating a single kernel image
    that can be run on multiple devices that require different tuning.
    
    Keep in mind that a lot of RT tasks in the system are created by the
    kernel. On Android for instance I can see over 50 RT tasks, only
    a handful of which created by the Android framework.
    
    To control the default behavior globally by system admins and device
    integrator, introduce the new sysctl_sched_uclamp_util_min_rt_default
    to change the default boost value of the RT tasks.
    
    I anticipate this to be mostly in the form of modifying the init script
    of a particular device.
    
    To avoid polluting the fast path with unnecessary code, the approach
    taken is to synchronously do the update by traversing all the existing
    tasks in the system. This could race with a concurrent fork(), which is
    dealt with by introducing sched_post_fork() function which will ensure
    the racy fork will get the right update applied.
    
    Tested on Juno-r2 in combination with the RT capacity awareness [1].
    By default an RT task will go to the highest capacity CPU and run at the
    maximum frequency, which is particularly energy inefficient on high end
    mobile devices because the biggest core[s] are 'huge' and power hungry.
    
    With this patch the RT task can be controlled to run anywhere by
    default, and doesn't cause the frequency to be maximum all the time.
    Yet any task that really needs to be boosted can easily escape this
    default behavior by modifying its requested uclamp.min value
    (p->uclamp_req[UCLAMP_MIN]) via sched_setattr() syscall.
    
    [1] 804d402fb6f6: ("sched/rt: Make RT capacity-aware")
    
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200716110347.19553-2-qais.yousef@arm.com
    (cherry picked from commit 13685c4a08fca9dd76bf53bfcbadc044ab2a08cb)
    [ minor conflicts in fork.c and sysctl.c due to code movements/additions ]
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: Ib7728be195708be5d621de3a92583017cbfa9861

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -59,6 +59,7 @@ extern int sysctl_sched_rt_runtime;
 #ifdef CONFIG_UCLAMP_TASK
 extern unsigned int sysctl_sched_uclamp_util_min;
 extern unsigned int sysctl_sched_uclamp_util_max;
+extern unsigned int sysctl_sched_uclamp_util_min_rt_default;
 #endif
 
 #ifdef CONFIG_CFS_BANDWIDTH
--- kernel/fork.c
+++ kernel/fork.c
@@ -2227,6 +2227,7 @@ static __latent_entropy struct task_struct *copy_process(
 	write_unlock_irq(&tasklist_lock);
 
 	proc_fork_connector(p);
+	sched_post_fork(p);
 	cgroup_post_fork(p);
 	cgroup_threadgroup_change_end(current);
 	perf_event_fork(p);
