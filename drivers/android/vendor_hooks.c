f0ff9d0ac58cf16ce9c24586e88cf893313dfad1
Sangmoon Kim <sangmoon.kim@samsung.com>
2020-08-05 22:13:43 +0900

    ANDROID: vendor_hooks: add waiting information for blocked tasks
     - Add the hook to print messages for sched_show_task.
    
    Bug: 162776704
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>
    Change-Id: Ib436fbd8d0ad509c3b5a73ea8f5170e0761a13fd
    (cherry picked from commit 9ad8ff902e39f37baf15904c6627ca03e8f60c27)
    (cherry picked from commit b519ac423787d38f467ca479d2126b7204d6f498)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -35,6 +35,7 @@
 #include <trace/hooks/remoteproc.h>
 #include <trace/hooks/timer.h>
 #include <trace/hooks/fpsimd.h>
+#include <trace/hooks/dtask.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -95,3 +96,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sched_show_task);
