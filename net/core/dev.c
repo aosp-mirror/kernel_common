84b60b44dc52f56a640d4305f303f0a2cc6dee4c
Bae Soukjin <soukjin.bae@samsung.com>
2020-08-19 08:30:51 +0900

    NOUPSTREAM: ANDROID: net: NET related vendor hooks
      android_vh_ptype_head:
        To add a debugging chain to ptype list
    
      android_vh_kfree_skb
        To sniff the dropped packet at kernel network
    
    [CPNOTE: 04/08/21] Lee: Vendor hooks
    
    Bug: 163716381
    
    Signed-off-by: Bae Soukjin <soukjin.bae@samsung.com>
    Change-Id: Ide80bf0a129da31a1824d4a33026ac42be327361
    (cherry picked from commit d88b2969cfa78608ad4563e5f4fa84497cc0ffa6)
    (cherry picked from commit a8021ba684c584b8a4361d1680f6e466748ab012)
    Signed-off-by: Lee Jones <joneslee@google.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -158,6 +158,7 @@
 #include <net/page_pool/types.h>
 #include <net/page_pool/helpers.h>
 #include <net/rps.h>
+#include <trace/hooks/net.h>
 
 #include "dev.h"
 #include "net-sysfs.h"
@@ -566,6 +567,12 @@ static inline void netdev_set_addr_lockdep_class(struct net_device *dev)
 
 static inline struct list_head *ptype_head(const struct packet_type *pt)
 {
+	struct list_head vendor_pt = { .next  = NULL, };
+
+	trace_android_vh_ptype_head(pt, &vendor_pt);
+	if (vendor_pt.next)
+		return vendor_pt.next;
+
 	if (pt->type == htons(ETH_P_ALL))
 		return pt->dev ? &pt->dev->ptype_all : &net_hotdata.ptype_all;
 	else
