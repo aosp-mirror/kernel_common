664b74271565af7a3c885bba2fdbabd1a2f3e2ea
Paul Lawrence <paullawrence@google.com>
2020-08-20 12:45:41 -0700

    ANDROID: Incremental fs: Separate pseudo-file code
    Also fixed two bugs in the process:
    
    is_pseudo_filename was not previously checking for .log, so an attempt
    to create a .log would succeed.
    
    All ioctls could be called on all files. ioctls now set on the correct
    files.
    
    Bug: 162856396
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I3f1e87d018836f51a97897880dd70181db4f7169

--- fs/incfs/data_mgmt.c
+++ fs/incfs/data_mgmt.c
@@ -927,9 +927,7 @@ static int wait_for_data_block(struct data_file *df, int block_index,
 	mi = df->df_mount_info;
 	segment = get_file_segment(df, block_index);
 
-	error = down_read_killable(&segment->rwsem);
-	if (error)
-		return error;
+	down_read(&segment->rwsem);
 
 	/* Look up the given block */
 	error = get_data_file_block(df, block_index, &block);
@@ -977,9 +975,7 @@ static int wait_for_data_block(struct data_file *df, int block_index,
 		return wait_res;
 	}
 
-	error = down_read_killable(&segment->rwsem);
-	if (error)
-		return error;
+	down_read(&segment->rwsem);
 
 	/*
 	 * Re-read block's info now, it has just arrived and
@@ -1094,9 +1090,7 @@ int incfs_process_new_data_block(struct data_file *df,
 	if (block->compression == COMPRESSION_LZ4)
 		flags |= INCFS_BLOCK_COMPRESSED_LZ4;
 
-	error = down_read_killable(&segment->rwsem);
-	if (error)
-		return error;
+	down_read(&segment->rwsem);
 
 	error = get_data_file_block(df, block->block_index, &existing_block);
 
