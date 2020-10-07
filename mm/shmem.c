34d04e7388784da5623c505a4a4370ee52061d25
Will McVicker <willmcvicker@google.com>
2020-10-07 15:36:26 -0700

    [REVERT ME] ANDROID: GKI: mm: export shmem_(getpage|mapping)
    Needed by vendor.
    
    Bug: 140053385
    Bug: 170424292
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: I6ec0cf1b4995789aa188db82cf1fbb6122dab8a3
    Signed-off-by: J. Avila <elavila@google.com>
    (cherry picked from commit b9330923300304e8dea653d21189aa3abce2d94d)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -155,6 +155,7 @@ int shmem_getpage(struct inode *inode, pgoff_t index,
 	return shmem_getpage_gfp(inode, index, pagep, sgp,
 		mapping_gfp_mask(inode->i_mapping), NULL, NULL, NULL);
 }
+EXPORT_SYMBOL_GPL(shmem_getpage);
 
 static inline struct shmem_sb_info *SHMEM_SB(struct super_block *sb)
 {
@@ -2346,6 +2347,7 @@ bool shmem_mapping(struct address_space *mapping)
 {
 	return mapping->a_ops == &shmem_aops;
 }
+EXPORT_SYMBOL_GPL(shmem_mapping);
 
 static int shmem_mfill_atomic_pte(struct mm_struct *dst_mm,
 				  pmd_t *dst_pmd,
