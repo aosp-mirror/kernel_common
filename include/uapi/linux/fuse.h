0a97e75fe010830e1afdbb8ffd45d79c54b1d343
Alessio Balsini <balsini@google.com>
2020-11-12 10:32:12 +0000

    FROMLIST: fuse: Fix colliding FUSE_PASSTHROUGH flag
    The FUSE_PASSTHROUGH flag has a conflict with linux-next, in which this id
    has been reserved for other purposes.
    Update FUSE_PASSTHROUGH bit id to one that has not been reserved upstream
    yet.
    
    Bug: 168023149
    Fixes: 2b39717c57b5 ("FROMLIST: fuse: Definitions and ioctl() for passthrough")
    Link: https://lore.kernel.org/lkml/20201026125016.1905945-2-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Change-Id: Ida0097fd2f9adee3bc85526dc6c78c7a87ac8f13

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -342,7 +342,7 @@ struct fuse_file_lock {
 #define FUSE_NO_OPENDIR_SUPPORT (1 << 24)
 #define FUSE_EXPLICIT_INVAL_DATA (1 << 25)
 #define FUSE_MAP_ALIGNMENT	(1 << 26)
-#define FUSE_PASSTHROUGH	(1 << 27)
+#define FUSE_PASSTHROUGH	(1 << 31)
 
 /**
  * CUSE INIT request/reply flags
