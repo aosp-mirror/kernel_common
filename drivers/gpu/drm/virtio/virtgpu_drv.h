df3f7dde16e4106ca405b933f94765a4b39b81c1
Gurchetan Singh <gurchetansingh@chromium.org>
2020-11-16 15:42:33 -0800

    CHROMIUM: drm/virtio: implement context init: track valid capabilities in a mask
    The valid capability IDs are between 1 to 31, and defined in the
    virtio gpu spec.  This is used for error checking the subsequent
    patches.  We're currently only using 2 capability IDs, so this
    should be plenty for the immediate future.
    
    Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>
    
    BUG=b:176158390
    TEST=glxgears and vkcube
    
    Change-Id: I4d3ac88170a8b3d400d28f5f00dec1f4d38f6df6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2645217
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Tested-by: Chia-I Wu <olv@google.com>
    Commit-Queue: Chia-I Wu <olv@google.com>
    Signed-off-by: Richard Fung <richardfung@google.com>

--- drivers/gpu/drm/virtio/virtgpu_drv.h
+++ drivers/gpu/drm/virtio/virtgpu_drv.h
@@ -54,6 +54,8 @@
 #define STATE_OK 1
 #define STATE_ERR 2
 
+#define MAX_CAPSET_ID 31
+
 struct virtio_gpu_object_params {
 	unsigned long size;
 	bool dumb;
@@ -252,6 +254,7 @@ struct virtio_gpu_device {
 
 	struct virtio_gpu_drv_capset *capsets;
 	uint32_t num_capsets;
+	uint32_t capset_id_mask;
 	struct list_head cap_cache;
 
 	/* protects uuid state when exporting */
