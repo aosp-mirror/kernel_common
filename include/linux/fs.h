9ae57211e25098d8363986b15766e2ba2bf5ceb0
Jaegeuk Kim <jaegeuk@google.com>
2020-11-23 08:54:39 -0800

    f2fs: check fiemap parameters
    This patch ported the below mainline patch.
    
    cddf8a2c4a82 ("fs: move fiemap range validation into the file systems instances")
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1758,6 +1758,9 @@ struct fiemap_extent_info {
 	struct fiemap_extent __user *fi_extents_start; /* Start of
 							fiemap_extent array */
 };
+
+int fiemap_prep(struct inode *inode, struct fiemap_extent_info *fieinfo,
+		u64 start, u64 *len, u32 supported_flags);
 int fiemap_fill_next_extent(struct fiemap_extent_info *info, u64 logical,
 			    u64 phys, u64 len, u32 flags);
 int fiemap_check_flags(struct fiemap_extent_info *fieinfo, u32 fs_flags);
