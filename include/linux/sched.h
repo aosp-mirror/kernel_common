e11bee2cc91da9ea946362d81ab6075c26cf4724
Quentin Perret <qperret@google.com>
2020-11-24 15:33:49 +0000

    NOUPSTREAM: ANDROID: sched: Track wake_q length
    Some partners have value-adds based on aosp/540066, which cannot be
    carried in ACK in its entirety as it no longer makes sense as-is (the
    select_idle_capacity() rework upstream solved the issue differently).
    
    It seems that those partners do not actually need the wake-wide tweaks,
    they only need to access the wake_q length for wake-up balance. To
    support this, add minimal tracking to the wake_q infrastructure in the
    core kernel, but do that by adding a pointer to the wake_q_head to
    task_struct directly to not litter all sched classes with an additional
    sibling_count_hint argument to the select_task_rq callbacks.
    
    Modules needing to access the wake_q length can do so by dereferencing
    p->wake_q_head in the wake-up path when it is non-NULL.
    
    [CPNOTE: 30/06/21] Lee: Vendor specific reference counting
    
    Bug: 173981591
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: I9a98167face92e70aba847d9f04d0c216065478c
    Signed-off-by: Lee Jones <joneslee@google.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1150,6 +1150,7 @@ struct task_struct {
 	raw_spinlock_t			pi_lock;
 
 	struct wake_q_node		wake_q;
+	int				wake_q_count;
 
 #ifdef CONFIG_RT_MUTEXES
 	/* PI waiters blocked on a rt_mutex held by this task: */
