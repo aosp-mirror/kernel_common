8c9ed792d12147be775004b8a03a0f757a037c5d
Badhri Jagan Sridharan <badhri@google.com>
2020-12-01 20:08:38 -0800

    FROMLIST: usb: typec: tcpm: Introduce vsafe0v for vbus
    TCPM at present lacks the notion of VSAFE0V. There
    are three vbus threshold levels that are critical to track:
    a. vSafe5V         - VBUS “5 volts” as defined by the USB
                         PD specification.
    b. vSinkDisconnect - Threshold used for transition from
                         Attached.SNK to Unattached.SNK.
    c. vSafe0V         - VBUS “0 volts” as defined by the USB
                         PD specification.
    
    Tracking vSafe0V is crucial for entry into Try.SNK and
    Attached.SRC and turning vbus back on by the source in
    response to hard reset.
    
    From "4.5.2.2.8.2 Exiting from AttachWait.SRC State" section
    in the Type-C spec:
    
    "The port shall transition to Attached.SRC when VBUS is at
    vSafe0V and the SRC.Rd state is detected on exactly one of
    the CC1 or CC2 pins for at least tCCDebounce."
    
    "A DRP that strongly prefers the Sink role may optionally
    transition to Try.SNK instead of Attached.SRC when VBUS
    is at vSafe0V and the SRC.Rd state is detected on exactly
    one of the CC1 or CC2 pins for at least tCCDebounce."
    
    From "7.1.5 Response to Hard Resets" section in the PD spec:
    
    "After establishing the vSafe0V voltage condition on VBUS,
    the Source Shall wait tSrcRecover before re-applying VCONN
    and restoring VBUS to vSafe5V."
    
    vbus_present in the TCPM code tracks vSafe5V(vbus_present is true)
    and vSinkDisconnect(vbus_present is false).
    
    This change adds is_vbus_vsafe0v callback which when set makes
    TCPM query for vSafe0V voltage level when needed.
    
    Since not all TCPC controllers might have the capability
    to report vSafe0V, TCPM assumes that vSafe0V is same as
    vSinkDisconnect when is_vbus_vsafe0v callback is not set.
    This allows TCPM to continue to support controllers which don't
    have the support for reporting vSafe0V.
    
    Introducing vSafe0V helps fix the failure reported at
    "Step 15. CVS verifies PUT remains in AttachWait.SRC for 500ms"
    of "TD 4.7.2 Try. SNK DRP Connect DRP Test" of
    "Universal Serial Bus Type-C (USB Type-C) Functional Test
    Specification Chapters 4 and 5". Here the compliance tester
    intentionally maintains vbus at greater than vSafe0V and expects
    the Product under test to stay in AttachWait.SRC till vbus drops
    to vSafe0V.
    
    Bug: 173747861
    Link: https://lore.kernel.org/linux-usb/20201202040840.663578-1-badhri@google.com/T/#t
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    Change-Id: I2386cd38d8355dc700f00919bbe7f1132f5041d6

--- include/linux/usb/tcpm.h
+++ include/linux/usb/tcpm.h
@@ -87,14 +87,6 @@ enum tcpm_transmit_type {
  *		detected.
  * @mux:	Pointer to multiplexer data
  * @set_bist_data: Turn on/off bist data mode for compliance testing
- * @check_contaminant:
- *		Optional; The callback is called when CC pins report open status
- *		at the end of the toggling period. Chip level drivers are
- *		expected to check for contaminant and re-enable toggling if
- *		needed. When 0 is not returned, check_contaminant is expected to
- *		restart toggling after checking the connector for contaminant.
- *		This forces the TCPM state machine to tranistion to TOGGLING state
- *		without calling start_toggling callback.
  * @enable_frs:
  *		Optional; Called to enable/disable PD 3.0 fast role swap.
  *		Enabling frs is accessory dependent as not all PD3.0
@@ -115,6 +107,20 @@ enum tcpm_transmit_type {
  *		will be turned on. requested_vbus_voltage is set to 0 when vbus
  *		is going to disappear knowingly i.e. during PR_SWAP and
  *		HARD_RESET etc.
+ * @check_contaminant:
+ *		Optional; The callback is called when CC pins report open status
+ *		at the end of the toggling period. Chip level drivers are
+ *		expected to check for contaminant and re-enable toggling if
+ *		needed. Return > 0 when contaminant will restart toggling
+ *		when the connector is free of contaminant; this forces the TCPM
+ *		state machine to tranistion to TOGGLING state without calling
+ *		start_toggling.
+ * @is_vbus_vsafe0v:
+ *		Optional; TCPCI spec based TCPC implementations are expected to
+ *		detect VSAFE0V voltage level at vbus. When detection of VSAFE0V
+ *		is supported by TCPC, set this callback for TCPM to query
+ *		whether vbus is at VSAFE0V when needed.
+ *		Returns true when vbus is at VSAFE0V, false otherwise.
  */
 struct tcpc_dev {
 	struct fwnode_handle *fwnode;
@@ -141,13 +147,14 @@ struct tcpc_dev {
 	int (*pd_transmit)(struct tcpc_dev *dev, enum tcpm_transmit_type type,
 			   const struct pd_message *msg, unsigned int negotiated_rev);
 	int (*set_bist_data)(struct tcpc_dev *dev, bool on);
-	int (*check_contaminant)(struct tcpc_dev *dev);
 	void (*set_pd_capable)(struct tcpc_dev *dev, bool capable);
 	int (*enable_frs)(struct tcpc_dev *dev, bool enable);
 	int (*frs_sourcing_vbus)(struct tcpc_dev *dev);
 	int (*enable_auto_vbus_discharge)(struct tcpc_dev *dev, bool enable);
 	int (*set_auto_vbus_discharge_threshold)(struct tcpc_dev *dev, enum typec_pwr_opmode mode,
 						 bool pps_active, u32 requested_vbus_voltage);
+	int (*check_contaminant)(struct tcpc_dev *dev);
+	bool (*is_vbus_vsafe0v)(struct tcpc_dev *dev);
 };
 
 struct tcpm_port;
