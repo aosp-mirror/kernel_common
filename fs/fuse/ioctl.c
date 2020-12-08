52f089420ac7c793fc4c0cef52931fc5a1d999df
Chirantan Ekbote <chirantan@chromium.org>
2020-12-08 18:38:07 +0900

    FROMLIST: fuse: Move ioctl length calculation to a separate function
    This will make it more readable when we add support for more ioctls.
    
    Signed-off-by: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-by: Eric Biggers <ebiggers@google.com>
    (am from https://patchwork.kernel.org/patch/11958181/)
    (also found at https://lore.kernel.org/r/20201208093808.1572227-2-chirantan@chromium.org)
    
    BUG=b:136127632
    TEST=build the kernel
    
    [5.4-arcvm]: picked from 5.4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2592613
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Tested-by: Chirantan Ekbote <chirantan@chromium.org>
    Commit-Queue: Chirantan Ekbote <chirantan@chromium.org>
    (cherry picked from commit 6b338dec0fe03cb7d5e75d51cb8c97c665b345d9)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2624187
    Auto-Submit: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-by: Hikaru Nishida <hikalium@chromium.org>
    
    Signed-off-by: Hikaru Nishida <hikalium@chromium.org>
    Change-Id: I6c33b7b3b79d49bf2af291f64048e5cbfdf1443a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2996012
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Signed-off-by: Richard Fung <richardfung@google.com>

--- fs/fuse/ioctl.c
+++ fs/fuse/ioctl.c
@@ -96,7 +96,20 @@ static int fuse_copy_ioctl_iovec(struct fuse_conn *fc, struct iovec *dst,
 
 	return 0;
 }
+static int fuse_get_ioctl_len(unsigned int cmd, unsigned long arg, size_t *len)
+{
+	switch (cmd) {
+	case FS_IOC_GETFLAGS:
+	case FS_IOC_SETFLAGS:
+		*len = sizeof(int);
+		break;
+	default:
+		*len = _IOC_SIZE(cmd);
+		break;
+	}
 
+	return 0;
+}
 
 /*
  * For ioctls, there is no generic way to determine how much memory
@@ -196,7 +209,9 @@ long fuse_do_ioctl(struct file *file, unsigned int cmd, unsigned long arg,
 		struct iovec *iov = iov_page;
 
 		iov->iov_base = (void __user *)arg;
-		iov->iov_len = _IOC_SIZE(cmd);
+		err = fuse_get_ioctl_len(cmd, arg, &iov->iov_len);
+		if (err)
+			goto out;
 
 		if (_IOC_DIR(cmd) & _IOC_WRITE) {
 			in_iov = iov;
