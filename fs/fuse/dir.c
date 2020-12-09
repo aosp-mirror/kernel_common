2f0d9c5204633587a7139aaa6d953a1b264b7286
Chirantan Ekbote <chirantan@chromium.org>
2020-12-09 19:14:09 +0900

    CHROMIUM: fuse: virtiofs: Run security hooks on new inodes
    This replaces the FROMLIST patches that were providing the same
    behavior.  Since those patches are basically being ignored by the
    maintainer and they change the protocol, we can't really maintain them
    long term. Upstream has already landed another change that uses the same
    feature bit for a different feature.
    
    This patch implements the same behavior without changing the public
    interface by using security_inode_init_security instead of
    security_dentry_init_security.  One downside of this change is that
    security xattrs are not applied atomically on creation on the host.
    However they are still atomic from the perspective of the guest. This
    isn't a big issue for arcvm because it always runs restorecon on boot
    anyway so even in the case of power loss, the xattrs will be set
    appropriately the next time arcvm boots.
    
    This behavior is always enabled for virtiofs and always disabled for
    regular fuse filesystems, again so that we don't need to make changes to
    the public protocol.
    
    BUG=b:155441848
    TEST=arc.PlayStore.vm and see that selinux xattrs are still being set
         appropriately
    
    [5.4-arcvm: picked from 5.4]
    Signed-off-by: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2581172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2604783
    Commit-Queue: Lepton Wu <lepton@chromium.org>
    Reviewed-by: Lepton Wu <lepton@chromium.org>
    
    [5.10-arcvm: context conflict resolved]
    Change-Id: I82d857e13fbd68a69eda80a70d2d38c08beb1921
    Signed-off-by: Hikaru Nishida <hikalium@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2992595
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Signed-off-by: Richard Fung <richardfung@google.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -17,6 +17,10 @@
 #include <linux/xattr.h>
 #include <linux/iversion.h>
 #include <linux/posix_acl.h>
+#include <linux/security.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/limits.h>
 
 static void fuse_advise_use_readdirplus(struct inode *dir)
 {
@@ -557,6 +561,47 @@ static struct dentry *fuse_lookup(struct inode *dir, struct dentry *entry,
 	return ERR_PTR(err);
 }
 
+static int fuse_initxattrs(struct inode *inode, const struct xattr *xattrs,
+			   void *fs_info)
+{
+	const struct xattr *xattr;
+	int err = 0;
+	int len;
+	char name[XATTR_NAME_MAX + 1];
+
+	for (xattr = xattrs; xattr->name != NULL; ++xattr) {
+		len = scnprintf(name, sizeof(name), XATTR_SECURITY_PREFIX "%s",
+				xattr->name);
+		if (len == sizeof(name)) {
+			err = -EOVERFLOW;
+			break;
+		}
+		err = fuse_setxattr(inode, name, xattr->value, xattr->value_len,
+				    0);
+		if (err < 0)
+			break;
+	}
+
+	return err;
+}
+
+/*
+ * Initialize security xattrs on newly created inodes if supported by the
+ * filesystem.
+ */
+static int fuse_init_security(struct fuse_conn *fc, struct inode *inode,
+			      struct inode *dir, const struct qstr *qstr)
+{
+	int err = 0;
+
+	if (fc->init_security) {
+		err = security_inode_init_security(inode, dir, qstr,
+						   fuse_initxattrs, NULL);
+	}
+
+	return err;
+}
+
 /*
  * Atomic create+open operation
  *
@@ -634,6 +679,14 @@ static int fuse_create_open(struct inode *dir, struct dentry *entry,
 		fuse_sync_release(NULL, ff, flags);
 		fuse_queue_forget(fm->fc, forget, outentry.nodeid, 1);
 		err = -ENOMEM;
+		goto out_err;
+	}
+
+	err = fuse_init_security(fc, inode, dir, &entry->d_name);
+	if (err) {
+		flags &= ~(O_CREAT | O_EXCL | O_TRUNC);
+		fuse_sync_release(NULL, ff, flags);
+		fuse_queue_forget(fc, forget, outentry.nodeid, 1);
 		goto out_err;
 	}
 	kfree(forget);
@@ -714,7 +767,7 @@ no_open:
  */
 static int create_new_entry(struct fuse_mount *fm, struct fuse_args *args,
 			    struct inode *dir, struct dentry *entry,
-			    umode_t mode)
+			    umode_t mode, bool init_security)
 {
 	struct fuse_entry_out outarg;
 	struct inode *inode;
@@ -754,6 +807,13 @@ static int create_new_entry(struct fuse_mount *fm, struct fuse_args *args,
 	if (args->opcode == FUSE_CHROMEOS_TMPFILE && inode->i_nlink != 0) {
 		fuse_queue_forget(fm->fc, forget, outarg.nodeid, 1);
 		return -EIO;
+	}
+	if (init_security) {
+		err = fuse_init_security(fm->fc, inode, dir, &entry->d_name);
+		if (err) {
+			fuse_queue_forget(fm->fc, forget, outarg.nodeid, 1);
+			return err;
+		}
 	}
 	kfree(forget);
 
@@ -809,7 +869,7 @@ static int fuse_mknod(struct inode *dir, struct dentry *entry, umode_t mode,
 	args.in_args[0].value = &inarg;
 	args.in_args[1].size = entry->d_name.len + 1;
 	args.in_args[1].value = entry->d_name.name;
-	return create_new_entry(fm, &args, dir, entry, mode);
+	return create_new_entry(fm, &args, dir, entry, mode, true);
 }
 
 static int fuse_create(struct inode *dir, struct dentry *entry, umode_t mode,
@@ -836,7 +896,7 @@ static int fuse_mkdir(struct inode *dir, struct dentry *entry, umode_t mode)
 	args.in_args[0].value = &inarg;
 	args.in_args[1].size = entry->d_name.len + 1;
 	args.in_args[1].value = entry->d_name.name;
-	return create_new_entry(fm, &args, dir, entry, S_IFDIR);
+	return create_new_entry(fm, &args, dir, entry, S_IFDIR, true);
 }
 
 static int fuse_chromeos_tmpfile(struct inode *dir, struct dentry *entry,
@@ -857,7 +917,7 @@ static int fuse_chromeos_tmpfile(struct inode *dir, struct dentry *entry,
 	args.in_args[0].size = sizeof(inarg);
 	args.in_args[0].value = &inarg;
 
-	return create_new_entry(fm, &args, dir, entry, S_IFREG);
+	return create_new_entry(fm, &args, dir, entry, S_IFREG, true);
 }
 
 static int fuse_symlink(struct inode *dir, struct dentry *entry,
@@ -873,7 +933,7 @@ static int fuse_symlink(struct inode *dir, struct dentry *entry,
 	args.in_args[0].value = entry->d_name.name;
 	args.in_args[1].size = len;
 	args.in_args[1].value = link;
-	return create_new_entry(fm, &args, dir, entry, S_IFLNK);
+	return create_new_entry(fm, &args, dir, entry, S_IFLNK, true);
 }
 
 void fuse_flush_time_update(struct inode *inode)
@@ -1061,7 +1121,7 @@ static int fuse_link(struct dentry *entry, struct inode *newdir,
 	args.in_args[0].value = &inarg;
 	args.in_args[1].size = newent->d_name.len + 1;
 	args.in_args[1].value = newent->d_name.name;
-	err = create_new_entry(fm, &args, newdir, newent, inode->i_mode);
+	err = create_new_entry(fm, &args, newdir, newent, inode->i_mode, false);
 	/* Contrary to "normal" filesystems it can happen that link
 	   makes two "logical" inodes point to the same "physical"
 	   inode.  We invalidate the attributes of the old one, so it
