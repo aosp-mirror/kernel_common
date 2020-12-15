d4584e49a05b7bb98d23886d88f8045a811ac31f
Kalesh Singh <kaleshsingh@google.com>
2020-12-14 19:07:35 -0800

    UPSTREAM: arm64: mremap speedup - enable HAVE_MOVE_PUD
    HAVE_MOVE_PUD enables remapping pages at the PUD level if both the source
    and destination addresses are PUD-aligned.
    
    With HAVE_MOVE_PUD enabled it can be inferred that there is approximately
    a 19x improvement in performance on arm64.  (See data below).
    
    ------- Test Results ---------
    
    The following results were obtained using a 5.4 kernel, by remapping a
    PUD-aligned, 1GB sized region to a PUD-aligned destination.  The results
    from 10 iterations of the test are given below:
    
    Total mremap times for 1GB data on arm64. All times are in nanoseconds.
    
      Control          HAVE_MOVE_PUD
    
      1247761          74271
      1219896          46771
      1094792          59687
      1227760          48385
      1043698          76666
      1101771          50365
      1159896          52500
      1143594          75261
      1025833          61354
      1078125          48697
    
      1134312.6        59395.7    <-- Mean time in nanoseconds
    
    A 1GB mremap completion time drops from ~1.1 milliseconds to ~59
    microseconds on arm64.  (~19x speed up).
    
    Link: https://lkml.kernel.org/r/20201014005320.2233162-5-kaleshsingh@google.com
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Christian Brauner <christian.brauner@ubuntu.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Frederic Weisbecker <frederic@kernel.org>
    Cc: Gavin Shan <gshan@redhat.com>
    Cc: Hassan Naveed <hnaveed@wavecomp.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jia He <justin.he@arm.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Krzysztof Kozlowski <krzk@kernel.org>
    Cc: Lokesh Gidra <lokeshgidra@google.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Masahiro Yamada <masahiroy@kernel.org>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Mina Almasry <almasrymina@google.com>
    Cc: Minchan Kim <minchan@google.com>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Ralph Campbell <rcampbell@nvidia.com>
    Cc: Ram Pai <linuxram@us.ibm.com>
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Cc: Sandipan Das <sandipan@linux.ibm.com>
    Cc: SeongJae Park <sjpark@amazon.de>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Steven Price <steven.price@arm.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit f5308c896d5de211245a9dc73b4e530f75185dd5)
    
    Bug: 151772539
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Change-Id: I30590b7375dde84fe345f4920a06f6a2c0b5aa31

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -408,6 +408,7 @@ static inline pmd_t pmd_mkdevmap(pmd_t pmd)
 #define pfn_pud(pfn,prot)	__pud(__phys_to_pud_val((phys_addr_t)(pfn) << PAGE_SHIFT) | pgprot_val(prot))
 
 #define set_pmd_at(mm, addr, pmdp, pmd)	set_pte_at(mm, addr, (pte_t *)pmdp, pmd_pte(pmd))
+#define set_pud_at(mm, addr, pudp, pud)	set_pte_at(mm, addr, (pte_t *)pudp, pud_pte(pud))
 
 #define __pgd_to_phys(pgd)	__pte_to_phys(pgd_pte(pgd))
 #define __phys_to_pgd_val(phys)	__phys_to_pte_val(phys)
