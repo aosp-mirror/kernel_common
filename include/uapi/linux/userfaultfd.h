22216000b8a361d30896043014191b8972753e4c
Lokesh Gidra <lokeshgidra@google.com>
2020-12-14 19:13:49 -0800

    BACKPORT: userfaultfd: add UFFD_USER_MODE_ONLY
    Patch series "Control over userfaultfd kernel-fault handling", v6.
    
    This patch series is split from [1].  The other series enables SELinux
    support for userfaultfd file descriptors so that its creation and movement
    can be controlled.
    
    It has been demonstrated on various occasions that suspending kernel code
    execution for an arbitrary amount of time at any access to userspace
    memory (copy_from_user()/copy_to_user()/...) can be exploited to change
    the intended behavior of the kernel.  For instance, handling page faults
    in kernel-mode using userfaultfd has been exploited in [2, 3].  Likewise,
    FUSE, which is similar to userfaultfd in this respect, has been exploited
    in [4, 5] for similar outcome.
    
    This small patch series adds a new flag to userfaultfd(2) that allows
    callers to give up the ability to handle kernel-mode faults with the
    resulting UFFD file object.  It then adds a 'user-mode only' option to the
    unprivileged_userfaultfd sysctl knob to require unprivileged callers to
    use this new flag.
    
    The purpose of this new interface is to decrease the chance of an
    unprivileged userfaultfd user taking advantage of userfaultfd to enhance
    security vulnerabilities by lengthening the race window in kernel code.
    
    [1] https://lore.kernel.org/lkml/20200211225547.235083-1-dancol@google.com/
    [2] https://duasynt.com/blog/linux-kernel-heap-spray
    [3] https://duasynt.com/blog/cve-2016-6187-heap-off-by-one-exploit
    [4] https://googleprojectzero.blogspot.com/2016/06/exploiting-recursion-in-linux-kernel_20.html
    [5] https://bugs.chromium.org/p/project-zero/issues/detail?id=808
    
    This patch (of 2):
    
    userfaultfd handles page faults from both user and kernel code.  Add a new
    UFFD_USER_MODE_ONLY flag for userfaultfd(2) that makes the resulting
    userfaultfd object refuse to handle faults from kernel mode, treating
    these faults as if SIGBUS were always raised, causing the kernel code to
    fail with EFAULT.
    
    A future patch adds a knob allowing administrators to give some processes
    the ability to create userfaultfd file objects only if they pass
    UFFD_USER_MODE_ONLY, reducing the likelihood that these processes will
    exploit userfaultfd's ability to delay kernel page faults to open timing
    windows for future exploits.
    
    Link: https://lkml.kernel.org/r/20201120030411.2690816-1-lokeshgidra@google.com
    Link: https://lkml.kernel.org/r/20201120030411.2690816-2-lokeshgidra@google.com
    Signed-off-by: Daniel Colascione <dancol@google.com>
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Reviewed-by: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: <calin@google.com>
    Cc: Daniel Colascione <dancol@dancol.org>
    Cc: Eric Biggers <ebiggers@kernel.org>
    Cc: Iurii Zaikin <yzaikin@google.com>
    Cc: Jeff Vander Stoep <jeffv@google.com>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: "Joel Fernandes (Google)" <joel@joelfernandes.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Kalesh Singh <kaleshsingh@google.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Luis Chamberlain <mcgrof@kernel.org>
    Cc: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Nitin Gupta <nigupta@nvidia.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Shaohua Li <shli@fb.com>
    Cc: Stephen Smalley <stephen.smalley.work@gmail.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 37cd0575b8510159992d279c530c05f872990b02)
    
    Conflicts:
        include/uapi/linux/userfaultfd.h
    (1. Removed uffdio_writeprotect struct part of 63b2d4174c4ad)
    
    Bug: 160737021
    Bug: 169683130
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Change-Id: Ic4cecce08956402f91f866ae08f1d4ec21d64289

--- include/uapi/linux/userfaultfd.h
+++ include/uapi/linux/userfaultfd.h
@@ -231,4 +231,13 @@ struct uffdio_zeropage {
 	__s64 zeropage;
 };
 
+/*
+ * Flags for the userfaultfd(2) system call itself.
+ */
+
+/*
+ * Create a userfaultfd that can handle page faults only in user mode.
+ */
+#define UFFD_USER_MODE_ONLY 1
+
 #endif /* _LINUX_USERFAULTFD_H */
