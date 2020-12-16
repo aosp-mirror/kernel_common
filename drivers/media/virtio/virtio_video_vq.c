2bd1a22a4ee17ee5716b88643c112e6e93111bd4
David Staessens <dstaessens@google.com>
2020-12-16 13:15:54 +0900

    CHROMIUM: driver: media: virtio: Add force keyframe control.
    This CL introduces the VIRTIO_VIDEO_CONTROL_FORCE_KEYFRAME control to
    the virtio encoder, to support the V4L2_CID_MPEG_VIDEO_FORCE_KEY_FRAME
    control.
    
    Signed-off-by: David Staessens <dstaessens@google.com>
    
    BUG=b:161498590,b:174444769,b:175270403
    TEST=tast run DUT arc.VideoEncodeAccel.h264_192p_i420_vm
    
    [5.4-arcvm: picked from 5.4]
    
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2594594
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Tested-by: David Staessens <dstaessens@chromium.org>
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Change-Id: I73d2d2247badff4274a3f86aa605eddd529966b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2996006
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Tested-by: Hikaru Nishida <hikalium@chromium.org>
    Commit-Queue: Hikaru Nishida <hikalium@chromium.org>

--- drivers/media/virtio/virtio_video_vq.c
+++ drivers/media/virtio/virtio_video_vq.c
@@ -1070,6 +1070,9 @@ int virtio_video_cmd_set_control(struct virtio_video *vv, uint32_t stream_id,
 	case VIRTIO_VIDEO_CONTROL_BITRATE:
 		size = sizeof(struct virtio_video_control_val_bitrate);
 		break;
+	case VIRTIO_VIDEO_CONTROL_FORCE_KEYFRAME:
+		size = 0;
+		break;
 	default:
 		return -1;
 	}
@@ -1097,6 +1100,9 @@ int virtio_video_cmd_set_control(struct virtio_video *vv, uint32_t stream_id,
 		ctrl_b = (void *)((char *)req_p +
 				 sizeof(struct virtio_video_set_control));
 		ctrl_b->bitrate = cpu_to_le32(value);
+		break;
+	case VIRTIO_VIDEO_CONTROL_FORCE_KEYFRAME:
+		// Button controls have no value.
 		break;
 	}
 
