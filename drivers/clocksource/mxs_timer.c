88c0859600c28389241f4cf3e36c5d016b885650
Tom Rix <trix@redhat.com>
2021-01-18 13:19:55 -0800

    clocksource/drivers/mxs_timer: Add missing semicolon when DEBUG is defined
    [ Upstream commit 7da390694afbaed8e0f05717a541dfaf1077ba51 ]
    
    When DEBUG is defined this error occurs
    
    drivers/clocksource/mxs_timer.c:138:1: error:
      expected ‘;’ before ‘}’ token
    
    The preceding statement needs a semicolon.
    Replace pr_info() with pr_debug() and remove the unneeded ifdef.
    
    Fixes: eb8703e2ef7c ("clockevents/drivers/mxs: Migrate to new 'set-state' interface")
    Signed-off-by: Tom Rix <trix@redhat.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20210118211955.763609-1-trix@redhat.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Ief2cceab2c27ea9c4885507f34114c9a1a4fb8e0

--- drivers/clocksource/mxs_timer.c
+++ drivers/clocksource/mxs_timer.c
@@ -169,11 +169,9 @@ static void mxs_set_mode(enum clock_event_mode mode,
 		timrot_irq_acknowledge();
 	}
 
-#ifdef DEBUG
-	pr_info("%s: changing mode from %s to %s\n", __func__,
+	pr_debug("%s: changing mode from %s to %s\n", __func__,
 		clock_event_mode_label[mxs_clockevent_mode],
 		clock_event_mode_label[mode]);
-#endif /* DEBUG */
 
 	/* Remember timer mode */
 	mxs_clockevent_mode = mode;
