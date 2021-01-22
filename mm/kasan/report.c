7ae671c7b87eab37edf9459faf1cfcfab681c2e1
Alexander Potapenko <glider@google.com>
2021-01-22 09:18:45 +0000

    BACKPORT: kasan: use error_report_end tracepoint
    Make it possible to trace KASAN error reporting.  A good usecase is
    watching for trace events from the userspace to detect and process memory
    corruption reports from the kernel.
    
    Link: https://lkml.kernel.org/r/20210121131915.1331302-4-glider@google.com
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Suggested-by: Marco Elver <elver@google.com>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Bug: 177201466
    (cherry picked from commit 99bbc9fba9b0c8956b817dd21ecb510da8b676dc
        https://github.com/hnaz/linux-mm v5.11-rc4-mmots-2021-01-21-20-10)
    [glider: resolved conflicts in mm/kasan/report.c]
    Test: CONFIG_KFENCE_KUNIT_TEST=y passes on Cuttlefish
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Change-Id: I60ca062373d13f08ed4ed05bee341a8c28619407

--- mm/kasan/report.c
+++ mm/kasan/report.c
@@ -29,6 +29,7 @@
 #include <linux/kasan.h>
 #include <linux/module.h>
 #include <linux/sched/task_stack.h>
+#include <trace/events/error_report.h>
 
 #include <asm/sections.h>
 
@@ -87,8 +88,9 @@ static void start_report(unsigned long *flags)
 	pr_err("==================================================================\n");
 }
 
-static void end_report(unsigned long *flags)
+static void end_report(unsigned long *flags, unsigned long addr)
 {
+	trace_error_report_end(ERROR_DETECTOR_KASAN, addr);
 	pr_err("==================================================================\n");
 	add_taint(TAINT_BAD_PAGE, LOCKDEP_NOW_UNRELIABLE);
 	spin_unlock_irqrestore(&report_lock, *flags);
@@ -468,7 +470,7 @@ void kasan_report_invalid_free(void *object, unsigned long ip)
 	print_address_description(object, tag);
 	pr_err("\n");
 	print_shadow_for_address(object);
-	end_report(&flags);
+	end_report(&flags, (unsigned long)object);
 }
 
 void __kasan_report(unsigned long addr, size_t size, bool is_write, unsigned long ip)
@@ -510,5 +512,5 @@ void __kasan_report(unsigned long addr, size_t size, bool is_write, unsigned lon
 		dump_stack();
 	}
 
-	end_report(&flags);
+	end_report(&flags, addr);
 }
