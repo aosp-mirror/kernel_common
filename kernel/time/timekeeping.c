c5a7ea0816ac52c31c6744081aad07f21871276d
Hikaru Nishida <hikalium@chromium.org>
2021-02-17 13:53:39 +0900

    CHROMIUM: timekeeping: Advance CLOCK_BOOTTIME 5 seconds at boot
    This is a short-term solution for solving b/162547792. CTS Verifier
    expects CLOCK_BOOTTIME and CLOCK_MONOTONIC are different enough to check
    if sensors are using the right clock source.
    
    This assumption is not satisfied in the current implementation since VM
    guest kernel does not experience suspend/resume cycle and the current
    kernel implementation adjusts a gap between CLOCK_BOOTTIME and
    CLOCK_MONOTONIC only when the kernel has been resumed from suspension.
    
    To make CTS Verifier happy, we need to adjust CLOCK_BOOTTIME in some way
    but there is no viable solution for the real problem yet so we decided
    to advance CLOCK_BOOTTIME a bit at boot.
    
    Once the underlying issue is resolved, this patch should be reverted.
    
    BUG=b:162547792
    TEST=built and passed Device Suspend Tests in CTS Verifier
    
    Signed-off-by: Hikaru Nishida <hikalium@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2699787
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Change-Id: I73aa29f7f58b9d64620a9e4e615524ee5a8b665c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2996015
    Signed-off-by: Richard Fung <richardfung@google.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -1613,7 +1613,7 @@ static bool persistent_clock_exists;
  */
 void __init timekeeping_init(void)
 {
-	struct timespec64 wall_time, boot_offset, wall_to_mono;
+	struct timespec64 wall_time, boot_offset, wall_to_mono, boot_delta;
 	struct timekeeper *tk = &tk_core.timekeeper;
 	struct clocksource *clock;
 	unsigned long flags;
@@ -1649,6 +1649,14 @@ void __init timekeeping_init(void)
 	tk->raw_sec = 0;
 
 	tk_set_wall_to_mono(tk, wall_to_mono);
+
+	// TODO(b/162547792): Remove following CLOCK_BOOTTIME adjustment
+	// once the underlying issue is resolved.
+	// The gap added here is larger enough than
+	// MIN_DELTA_BETWEEN_CLOCKS_MS which is expected by CTS Verifier.
+	boot_delta.tv_sec = 5;
+	boot_delta.tv_nsec = 0;
+	tk_update_sleep_time(tk, timespec64_to_ktime(boot_delta));
 
 	timekeeping_update(tk, TK_MIRROR | TK_CLOCK_WAS_SET);
 
