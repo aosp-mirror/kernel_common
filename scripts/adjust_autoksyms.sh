917cc909c03f1382982c3a86254adb3c9485c97a
Masahiro Yamada <masahiroy@kernel.org>
2021-02-26 15:36:15 +0900

    UPSTREAM: kbuild: do not include include/config/auto.conf from adjust_autoksyms.sh
    Commit cd195bc4775a ("kbuild: split adjust_autoksyms.sh in two parts")
    split out the code that needs include/config/auto.conf.
    
    This script no longer needs to include include/config/auto.conf.
    
    Bug: 254441685
    Fixes: cd195bc4775a ("kbuild: split adjust_autoksyms.sh in two parts")
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    (cherry picked from commit 12e9dea6c9766c7403417d00193940cea33ee81a)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I80233c2685728942c9ec37b4f3e1017742018e8f

--- scripts/adjust_autoksyms.sh
+++ scripts/adjust_autoksyms.sh
@@ -36,9 +36,6 @@ case "$KBUILD_VERBOSE" in
 	;;
 esac
 
-# We need access to CONFIG_ symbols
-. include/config/auto.conf
-
 # Generate a new symbol list file
 $CONFIG_SHELL $srctree/scripts/gen_autoksyms.sh "$new_ksyms_file"
 
