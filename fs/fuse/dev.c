51b0818b61405c9d7d4ab2428598e7c652095e94
Alessio Balsini <balsini@android.com>
2021-03-19 15:05:14 +0000

    UPSTREAM: fuse: fix matching of FUSE_DEV_IOC_CLONE command
    With commit f8425c939663 ("fuse: 32-bit user space ioctl compat for fuse
    device") the matching constraints for the FUSE_DEV_IOC_CLONE ioctl command
    are relaxed, limited to the testing of command type and number.  As Arnd
    noticed, this is wrong as it wouldn't ensure the correctness of the data
    size or direction for the received FUSE device ioctl.
    
    Fix by bringing back the comparison of the ioctl received by the FUSE
    device to the originally generated FUSE_DEV_IOC_CLONE.
    
    Fixes: f8425c939663 ("fuse: 32-bit user space ioctl compat for fuse device")
    Reported-by: Arnd Bergmann <arnd@kernel.org>
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: I372d8399db6d603ba20ef50528acf6645e4d3c66
    (cherry picked from commit 6076f5f341e612152879bfda99f0b76c1953bf0b)

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -2238,11 +2238,8 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 	int oldfd;
 	struct fuse_dev *fud = NULL;
 
-	if (_IOC_TYPE(cmd) != FUSE_DEV_IOC_MAGIC)
-		return -EINVAL;
-
-	switch (_IOC_NR(cmd)) {
-	case _IOC_NR(FUSE_DEV_IOC_CLONE):
+	switch (cmd) {
+	case FUSE_DEV_IOC_CLONE:
 		res = -EFAULT;
 		if (!get_user(oldfd, (__u32 __user *)arg)) {
 			struct file *old = fget(oldfd);
@@ -2267,7 +2264,7 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 			}
 		}
 		break;
-	case _IOC_NR(FUSE_DEV_IOC_PASSTHROUGH_OPEN):
+	case FUSE_DEV_IOC_PASSTHROUGH_OPEN:
 		res = -EFAULT;
 		if (!get_user(oldfd, (__u32 __user *)arg)) {
 			res = -EINVAL;
