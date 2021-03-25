69585c2ad968b06bdfb0dc833725a8c250a867d2
Wang Xiaojun <wangxiaojun11@huawei.com>
2021-03-25 10:19:20 -0400

    f2fs: fix wrong alloc_type in f2fs_do_replace_block
    If the alloc_type of the original curseg is LFS,
    when we change_curseg and then do recover curseg,
    the alloc_type becomes SSR.
    
    Signed-off-by: Wang Xiaojun <wangxiaojun11@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -3571,6 +3571,7 @@ void f2fs_do_replace_block(struct f2fs_sb_info *sbi, struct f2fs_summary *sum,
 	struct seg_entry *se;
 	int type;
 	unsigned short old_blkoff;
+	unsigned char old_alloc_type;
 
 	segno = GET_SEGNO(sbi, new_blkaddr);
 	se = get_seg_entry(sbi, segno);
@@ -3604,6 +3605,7 @@ void f2fs_do_replace_block(struct f2fs_sb_info *sbi, struct f2fs_summary *sum,
 
 	old_cursegno = curseg->segno;
 	old_blkoff = curseg->next_blkoff;
+	old_alloc_type = curseg->alloc_type;
 
 	/* change the current segment */
 	if (segno != curseg->segno) {
@@ -3638,6 +3640,7 @@ void f2fs_do_replace_block(struct f2fs_sb_info *sbi, struct f2fs_summary *sum,
 			change_curseg(sbi, type, true);
 		}
 		curseg->next_blkoff = old_blkoff;
+		curseg->alloc_type = old_alloc_type;
 	}
 
 	up_write(&sit_i->sentry_lock);
