2fb08237404ccbb752caea1f21d1b4f63aeaabaa
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-03-29 16:46:05 -0400

    NFS: Deal correctly with attribute generation counter overflow
    [ Upstream commit 9fdbfad1777cb4638f489eeb62d85432010c0031 ]
    
    We need to use unsigned long subtraction and then convert to signed in
    order to deal correcly with C overflow rules.
    
    Fixes: f5062003465c ("NFS: Set an attribute barrier on all updates")
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: Ia77fe45b93ece1b8584cc05e6c7590ca3d9a3975

--- fs/nfs/inode.c
+++ fs/nfs/inode.c
@@ -1361,12 +1361,12 @@ EXPORT_SYMBOL_GPL(_nfs_display_fhandle);
  */
 static int nfs_inode_attrs_need_update(const struct inode *inode, const struct nfs_fattr *fattr)
 {
-	const struct nfs_inode *nfsi = NFS_I(inode);
+	unsigned long attr_gencount = NFS_I(inode)->attr_gencount;
 
-	return ((long)fattr->gencount - (long)nfsi->attr_gencount) > 0 ||
+	return ((long)fattr->gencount - attr_gencount) > 0 ||
 		nfs_ctime_need_update(inode, fattr) ||
 		nfs_size_need_update(inode, fattr) ||
-		((long)nfsi->attr_gencount - (long)nfs_read_attr_generation_counter() > 0);
+		((long)attr_gencount - nfs_read_attr_generation_counter() > 0);
 }
 
 /*
