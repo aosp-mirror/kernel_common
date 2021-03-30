bf28ed028d5c8c9345d15a244ef4ad3f471f34ee
Kyle Tso <kyletso@google.com>
2021-03-30 22:48:50 +0800

    ANDROID: usb: typec: tcpm: Add vendor hook to store partner source capabilities
    Add a hook after receiving the source capabilities from the partner
    port. Pass the address of the source capability array so that vendor
    code is able to access them.
    
    OOT bug:
    Bug: 181629276
    Bug: 169215197
    Bug: 271294543
    Signed-off-by: Kyle Tso <kyletso@google.com>
    Change-Id: I11c4a7919c248690e63c3bfbebfa6b8d711175a6
    (cherry picked from commit c8314fcb76b4e0484abeb4e2ea7da36898fc8653)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -302,4 +302,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_si_meminfo);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpci_override_toggling);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_typec_tcpci_get_vbus);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_store_partner_src_caps);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpm_get_timer);
