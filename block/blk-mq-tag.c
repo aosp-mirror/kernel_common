f198e0a1afd4eb444e4a5885b8d101bd3a9b36c2
Ming Lei <ming.lei@redhat.com>
2021-05-11 23:22:35 +0800

    BACKPORT: blk-mq: clear stale request in tags->rq[] before freeing one request pool
    refcount_inc_not_zero() in bt_tags_iter() still may read one freed
    request.
    
    Fix the issue by the following approach:
    
    1) hold a per-tags spinlock when reading ->rqs[tag] and calling
    refcount_inc_not_zero in bt_tags_iter()
    
    2) clearing stale request referred via ->rqs[tag] before freeing
    request pool, the per-tags spinlock is held for clearing stale
    ->rq[tag]
    
    So after we cleared stale requests, bt_tags_iter() won't observe
    freed request any more, also the clearing will wait for pending
    request reference.
    
    The idea of clearing ->rqs[] is borrowed from John Garry's previous
    patch and one recent David's patch.
    
    Tested-by: John Garry <john.garry@huawei.com>
    Reviewed-by: David Jeffery <djeffery@redhat.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    
    Bug: 197804811
    Change-Id: If49478d7b05d3f5b0a26966ddf9ae764cf2fb6b0
    (cherry picked from commit bd63141d585bef14f4caf111f6d0e27fe2300ec6)
    [ refactored to avoid breaking KMI ]
    Signed-off-by: Pradeep P V K <pragalla@codeaurora.org>
    Signed-off-by: Todd Kjos <tkjos@google.com>
    (cherry picked from commit bb96e7f45dc6ac1d6ec12190f1f286e3014fb068)
    Signed-off-by: Lee Jones <joneslee@google.com>

--- block/blk-mq-tag.c
+++ block/blk-mq-tag.c
@@ -377,18 +377,21 @@ struct blk_mq_tags *blk_mq_init_tags(unsigned int total_tags,
 				     int node, int alloc_policy)
 {
 	struct blk_mq_tags *tags;
+	struct ext_blk_mq_tags *etags;
 
 	if (total_tags > BLK_MQ_TAG_MAX) {
 		pr_err("blk-mq: tag depth too large\n");
 		return NULL;
 	}
 
-	tags = kzalloc_node(sizeof(*tags), GFP_KERNEL, node);
-	if (!tags)
+	etags = kzalloc_node(sizeof(*etags), GFP_KERNEL, node);
+	if (!etags)
 		return NULL;
 
+	tags = &etags->tags;
 	tags->nr_tags = total_tags;
 	tags->nr_reserved_tags = reserved_tags;
+	spin_lock_init(&etags->lock);
 
 	return blk_mq_init_bitmap_tags(tags, node, alloc_policy);
 }
