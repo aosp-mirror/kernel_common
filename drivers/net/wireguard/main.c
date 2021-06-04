0bd36fc71dc18c8bddd2eb8578113e8f4ea9d08a
Jason A. Donenfeld <Jason@zx2c4.com>
2021-06-04 17:17:34 +0200

    BACKPORT: wireguard: peer: allocate in kmem_cache
    With deployments having upwards of 600k peers now, this somewhat heavy
    structure could benefit from more fine-grained allocations.
    Specifically, instead of using a 2048-byte slab for a 1544-byte object,
    we can now use 1544-byte objects directly, thus saving almost 25%
    per-peer, or with 600k peers, that's a savings of 303 MiB. This also
    makes wireguard's memory usage more transparent in tools like slabtop
    and /proc/slabinfo.
    
    Fixes: 8b5553ace83c ("wireguard: queueing: get rid of per-peer ring buffers")
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Suggested-by: Matthew Wilcox <willy@infradead.org>
    Cc: stable@vger.kernel.org
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit a4e9f8e3287c9eb6bf70df982870980dd3341863)
    [connoro: update goto statement to preserve logic after adding
    err_peer label]
    Bug: 187129171
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Change-Id: I821f8b7f99439021181c4ca9037b92da5c8d77ce

--- drivers/net/wireguard/main.c
+++ drivers/net/wireguard/main.c
@@ -29,9 +29,13 @@ static int __init mod_init(void)
 	ret = -ENOTRECOVERABLE;
 	if (!wg_allowedips_selftest() || !wg_packet_counter_selftest() ||
 	    !wg_ratelimiter_selftest())
-		goto err_device;
+		goto err_peer;
 #endif
 	wg_noise_init();
+
+	ret = wg_peer_init();
+	if (ret < 0)
+		goto err_peer;
 
 	ret = wg_device_init();
 	if (ret < 0)
@@ -49,6 +53,8 @@ static int __init mod_init(void)
 err_netlink:
 	wg_device_uninit();
 err_device:
+	wg_peer_uninit();
+err_peer:
 	wg_allowedips_slab_uninit();
 err_allowedips:
 	return ret;
@@ -58,6 +64,7 @@ static void __exit mod_exit(void)
 {
 	wg_genetlink_uninit();
 	wg_device_uninit();
+	wg_peer_uninit();
 	wg_allowedips_slab_uninit();
 }
 
