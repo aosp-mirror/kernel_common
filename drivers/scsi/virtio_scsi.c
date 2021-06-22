d5f74773bf3acfd7b648c5227646a1f2bb76c040
Hannes Reinecke <hare@suse.de>
2021-06-22 11:11:53 +0200

    UPSTREAM: scsi: virtio_scsi: Do not overwrite SCSI status
    When a sense code is present we should not override the SAM status; the
    driver already sets it based on the response from the hypervisor.
    
    In addition we should only copy the sense buffer if one is actually
    provided by the hypervisor.
    
    Link: https://lore.kernel.org/r/20210622091153.29231-1-hare@suse.de
    Fixes: 464a00c9e0ad ("scsi: core: Kill DRIVER_SENSE")
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Jiri Slaby <jirislaby@kernel.org>
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit c43ddbf97f46b93727718408d60a47ce8c08f30c)
    Bug: 187129171
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Change-Id: I6a42c80e2cbd6786f2e08ebe4226f2cddfbb8e97

--- drivers/scsi/virtio_scsi.c
+++ drivers/scsi/virtio_scsi.c
@@ -156,13 +156,11 @@ static void virtscsi_complete_cmd(struct virtio_scsi *vscsi, void *buf)
 
 	WARN_ON(virtio32_to_cpu(vscsi->vdev, resp->sense_len) >
 		VIRTIO_SCSI_SENSE_SIZE);
-	if (sc->sense_buffer) {
+	if (resp->sense_len) {
 		memcpy(sc->sense_buffer, resp->sense,
 		       min_t(u32,
 			     virtio32_to_cpu(vscsi->vdev, resp->sense_len),
 			     VIRTIO_SCSI_SENSE_SIZE));
-		if (resp->sense_len)
-			set_driver_byte(sc, DRIVER_SENSE);
 	}
 
 	sc->scsi_done(sc);
