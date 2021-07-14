0a5156326b3ecc2b8c7e46f95497d289bb7189ca
Mark Rutland <mark.rutland@arm.com>
2021-07-14 15:38:42 +0100

    UPSTREAM: arm64: kasan: mte: use a constant kernel GCR_EL1 value
    When KASAN_HW_TAGS is selected, KASAN is enabled at boot time, and the
    hardware supports MTE, we'll initialize `kernel_gcr_excl` with a value
    dependent on KASAN_TAG_MAX. While the resulting value is a constant
    which depends on KASAN_TAG_MAX, we have to perform some runtime work to
    generate the value, and have to read the value from memory during the
    exception entry path. It would be better if we could generate this as a
    constant at compile-time, and use it as such directly.
    
    Early in boot within __cpu_setup(), we initialize GCR_EL1 to a safe
    value, and later override this with the value required by KASAN. If
    CONFIG_KASAN_HW_TAGS is not selected, or if KASAN is disabeld at boot
    time, the kernel will not use IRG instructions, and so the initial value
    of GCR_EL1 is does not matter to the kernel. Thus, we can instead have
    __cpu_setup() initialize GCR_EL1 to a value consistent with
    KASAN_TAG_MAX, and avoid the need to re-initialize it during hotplug and
    resume form suspend.
    
    This patch makes arem64 use a compile-time constant KERNEL_GCR_EL1
    value, which is compatible with KASAN_HW_TAGS when this is selected.
    This removes the need to re-initialize GCR_EL1 dynamically, and acts as
    an optimization to the entry assembly, which no longer needs to load
    this value from memory. The redundant initialization hooks are removed.
    
    In order to do this, KASAN_TAG_MAX needs to be visible outside of the
    core KASAN code. To do this, I've moved the KASAN_TAG_* values into
    <linux/kasan-tags.h>.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Konovalov <andreyknvl@gmail.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Peter Collingbourne <pcc@google.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Andrey Konovalov <andreyknvl@gmail.com>
    Tested-by: Andrey Konovalov <andreyknvl@gmail.com>
    Link: https://lore.kernel.org/r/20210714143843.56537-3-mark.rutland@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit 82868247897bea2d69a83dca9a6a557e2c96dac4)
    Bug: 217222520
    Change-Id: I0cd0fd717df3eaba180267baf31fd8730e90e6ef
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>

--- arch/arm64/include/asm/mte-kasan.h
+++ arch/arm64/include/asm/mte-kasan.h
@@ -132,7 +132,6 @@ static inline void mte_set_mem_tag_range(void *addr, size_t size, u8 tag,
 
 void mte_enable_kernel_sync(void);
 void mte_enable_kernel_async(void);
-void mte_init_tags(u64 max_tag);
 
 void mte_set_report_once(bool state);
 bool mte_report_once(void);
@@ -164,10 +163,6 @@ static inline void mte_enable_kernel_sync(void)
 }
 
 static inline void mte_enable_kernel_async(void)
-{
-}
-
-static inline void mte_init_tags(u64 max_tag)
 {
 }
 
