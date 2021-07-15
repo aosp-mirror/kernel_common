edf220fe151691f35ee19f86d32291f932d780f7
Chuck Lever <chuck.lever@oracle.com>
2021-07-15 15:52:25 -0400

    NFS: Add a private local dispatcher for NFSv4 callback operations
    [ Upstream commit 7d34c96217cf3c2d37ca0a56ca0bc3c3bef1e189 ]
    
    The client's NFSv4 callback service is the only remaining user of
    svc_generic_dispatch().
    
    Note that the NFSv4 callback service doesn't use the .pc_encode and
    .pc_decode callouts in any substantial way, so they are removed.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/nfs/callback_xdr.c
+++ fs/nfs/callback_xdr.c
@@ -988,6 +988,15 @@ out_invalidcred:
 	return rpc_success;
 }
 
+static int
+nfs_callback_dispatch(struct svc_rqst *rqstp, __be32 *statp)
+{
+	const struct svc_procedure *procp = rqstp->rq_procinfo;
+
+	*statp = procp->pc_func(rqstp);
+	return 1;
+}
+
 /*
  * Define NFS4 callback COMPOUND ops.
  */
@@ -1076,7 +1085,7 @@ const struct svc_version nfs4_callback_version1 = {
 	.vs_proc = nfs4_callback_procedures1,
 	.vs_count = nfs4_callback_count1,
 	.vs_xdrsize = NFS4_CALLBACK_XDRSIZE,
-	.vs_dispatch = NULL,
+	.vs_dispatch = nfs_callback_dispatch,
 	.vs_hidden = true,
 	.vs_need_cong_ctrl = true,
 };
@@ -1088,7 +1097,7 @@ const struct svc_version nfs4_callback_version4 = {
 	.vs_proc = nfs4_callback_procedures1,
 	.vs_count = nfs4_callback_count4,
 	.vs_xdrsize = NFS4_CALLBACK_XDRSIZE,
-	.vs_dispatch = NULL,
+	.vs_dispatch = nfs_callback_dispatch,
 	.vs_hidden = true,
 	.vs_need_cong_ctrl = true,
 };
