74d2c6e570e5169b34c49579ba9dffce57499724
Ian Kent <raven@themaw.net>
2021-07-16 17:28:40 +0800

    UPSTREAM: kernfs: dont call d_splice_alias() under kernfs node lock
    The call to d_splice_alias() in kernfs_iop_lookup() doesn't depend on
    any kernfs node so there's no reason to hold the kernfs node lock when
    calling it.
    
    Reviewed-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Ian Kent <raven@themaw.net>
    Link: https://lore.kernel.org/r/162642772000.63632.10672683419693513226.stgit@web.messagingengine.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit df6192f47d2311cf40cd4321cc59863a5853b665)
    
    Bug: 320903885
    Bug: 219424218
    Bug: 206126556
    Change-Id: I5feacf76e23f9ff1840f58ebd791158f1a126b64
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- fs/kernfs/dir.c
+++ fs/kernfs/dir.c
@@ -1110,7 +1110,6 @@ static struct dentry *kernfs_iop_lookup(struct inode *dir,
 					struct dentry *dentry,
 					unsigned int flags)
 {
-	struct dentry *ret;
 	struct kernfs_node *parent = dir->i_private;
 	struct kernfs_node *kn;
 	struct inode *inode = NULL;
@@ -1130,11 +1129,10 @@ static struct dentry *kernfs_iop_lookup(struct inode *dir,
 	/* Needed only for negative dentry validation */
 	if (!inode)
 		kernfs_set_rev(parent, dentry);
-	/* instantiate and hash (possibly negative) dentry */
-	ret = d_splice_alias(inode, dentry);
 	up_read(&kernfs_rwsem);
 
-	return ret;
+	/* instantiate and hash (possibly negative) dentry */
+	return d_splice_alias(inode, dentry);
 }
 
 static int kernfs_iop_mkdir(struct inode *dir, struct dentry *dentry,
