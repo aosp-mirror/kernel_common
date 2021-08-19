35008c86efa8449b79739d1911c04f6dccad104c
Chirantan Ekbote <chirantan@chromium.org>
2021-08-19 18:45:37 +0900

    CHROMIUM: Support fsverity in fuse
    FS_IOC_MEASURE_VERITY has a variable length argument so check for it and
    set the iov_len of the ioctl message to the length of the parameter.
    
    BUG=b:141632062
    TEST=`fsverity enable $FILE` and `fsverity measure $FILE` on a virtio-fs
         mountpoint
    
    Signed-off-by: Chirantan Ekbote <chirantan@chromium.org>
    Change-Id: I44f58039776333ebb0c9480f5c067455bc8bda18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3106720
    Commit-Queue: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Signed-off-by: Richard Fung <richardfung@google.com>

--- fs/fuse/ioctl.c
+++ fs/fuse/ioctl.c
@@ -9,6 +9,7 @@
 #include <linux/compat.h>
 #include <linux/fileattr.h>
 #include <linux/fscrypt.h>
+#include <linux/fsverity.h>
 
 /*
  * CUSE servers compiled on 32bit broke on 64bit kernels because the
@@ -117,6 +118,21 @@ static int fuse_get_ioctl_len(unsigned int cmd, unsigned long arg, size_t *len)
 			return -EINVAL;
 
 		*len = sizeof(policy_size) + policy_size;
+		break;
+	}
+	case FS_IOC_MEASURE_VERITY: {
+		__u16 digest_size;
+		struct fsverity_digest __user *uarg =
+			(struct fsverity_digest __user *)arg;
+
+		if (copy_from_user(&digest_size, &uarg->digest_size,
+				   sizeof(digest_size)))
+			return -EFAULT;
+
+		if (digest_size > SIZE_MAX - sizeof(struct fsverity_digest))
+			return -EINVAL;
+
+		*len = sizeof(struct fsverity_digest) + digest_size;
 		break;
 	}
 	default:
