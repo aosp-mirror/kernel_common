b7edb848c4ff9237ac323d53c48431b7a51efd8b
Daeho Jeong <daehojeong@google.com>
2021-08-20 15:29:09 -0700

    f2fs: introduce periodic iostat io latency traces
    Whenever we notice some sluggish issues on our machines, we are always
    curious about how well all types of I/O in the f2fs filesystem are
    handled. But, it's hard to get this kind of real data. First of all,
    we need to reproduce the issue while turning on the profiling tool like
    blktrace, but the issue doesn't happen again easily. Second, with the
    intervention of any tools, the overall timing of the issue will be
    slightly changed and it sometimes makes us hard to figure it out.
    
    So, I added the feature printing out IO latency statistics tracepoint
    events, which are minimal things to understand filesystem's I/O related
    behaviors, into F2FS_IOSTAT kernel config. With "iostat_enable" sysfs
    node on, we can get this statistics info in a periodic way and it
    would cause the least overhead.
    
    [samples]
     f2fs_ckpt-254:1-507     [003] ....  2842.439683: f2fs_iostat_latency:
    dev = (254,11), iotype [peak lat.(ms)/avg lat.(ms)/count],
    rd_data [136/1/801], rd_node [136/1/1704], rd_meta [4/2/4],
    wr_sync_data [164/16/3331], wr_sync_node [152/3/648],
    wr_sync_meta [160/2/4243], wr_async_data [24/13/15],
    wr_async_node [0/0/0], wr_async_meta [0/0/0]
    
     f2fs_ckpt-254:1-507     [002] ....  2845.450514: f2fs_iostat_latency:
    dev = (254,11), iotype [peak lat.(ms)/avg lat.(ms)/count],
    rd_data [60/3/456], rd_node [60/3/1258], rd_meta [0/0/1],
    wr_sync_data [120/12/2285], wr_sync_node [88/5/428],
    wr_sync_meta [52/6/2990], wr_async_data [4/1/3],
    wr_async_node [0/0/0], wr_async_meta [0/0/0]
    
    Signed-off-by: Daeho Jeong <daehojeong@google.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1578,6 +1578,7 @@ static void f2fs_put_super(struct super_block *sb)
 #endif
 	fscrypt_free_dummy_policy(&F2FS_OPTION(sbi).dummy_enc_policy);
 	destroy_percpu_info(sbi);
+	f2fs_destroy_iostat(sbi);
 	for (i = 0; i < NR_PAGE_TYPE; i++)
 		kvfree(sbi->write_io[i]);
 #ifdef CONFIG_UNICODE
@@ -4006,7 +4007,7 @@ try_onemore:
 
 	err = init_percpu_info(sbi);
 	if (err)
-		goto free_bio_info;
+		goto free_iostat;
 
 	if (F2FS_IO_ALIGNED(sbi)) {
 		sbi->write_io_dummy =
@@ -4339,6 +4340,8 @@ free_io_dummy:
 	mempool_destroy(sbi->write_io_dummy);
 free_percpu:
 	destroy_percpu_info(sbi);
+free_iostat:
+	f2fs_destroy_iostat(sbi);
 free_bio_info:
 	for (i = 0; i < NR_PAGE_TYPE; i++)
 		kvfree(sbi->write_io[i]);
@@ -4481,9 +4484,12 @@ static int __init init_f2fs_fs(void)
 	err = f2fs_init_post_read_processing();
 	if (err)
 		goto free_root_stats;
-	err = f2fs_init_bio_entry_cache();
+	err = f2fs_init_iostat_processing();
 	if (err)
 		goto free_post_read;
+	err = f2fs_init_bio_entry_cache();
+	if (err)
+		goto free_iostat;
 	err = f2fs_init_bioset();
 	if (err)
 		goto free_bio_enrty_cache;
@@ -4505,6 +4511,8 @@ free_bioset:
 	f2fs_destroy_bioset();
 free_bio_enrty_cache:
 	f2fs_destroy_bio_entry_cache();
+free_iostat:
+	f2fs_destroy_iostat_processing();
 free_post_read:
 	f2fs_destroy_post_read_processing();
 free_root_stats:
@@ -4539,6 +4547,7 @@ static void __exit exit_f2fs_fs(void)
 	f2fs_destroy_compress_mempool();
 	f2fs_destroy_bioset();
 	f2fs_destroy_bio_entry_cache();
+	f2fs_destroy_iostat_processing();
 	f2fs_destroy_post_read_processing();
 	f2fs_destroy_root_stats();
 	unregister_filesystem(&f2fs_fs_type);
