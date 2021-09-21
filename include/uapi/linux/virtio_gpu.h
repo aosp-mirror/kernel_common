94dca7af8908dfcb8af5e928ebe1c2445ec920b0
Gurchetan Singh <gurchetansingh@chromium.org>
2021-09-21 16:20:13 -0700

    BACKPORT: virtio-gpu api: multiple context types with explicit initialization
    This feature allows for each virtio-gpu 3D context to be created
    with a "context_init" variable.  This variable can specify:
    
     - the type of protocol used by the context via the capset id.
       This is useful for differentiating virgl, gfxstream, and venus
       protocols by host userspace.
    
     - other things in the future, such as the version of the context.
    
    In addition, each different context needs one or more timelines, so
    for example a virgl context's waiting can be independent on a
    gfxstream context's waiting.
    
    VIRTIO_GPU_FLAG_INFO_RING_IDX is introduced to specific to tell the
    host which per-context command ring (or "hardware queue", distinct
    from the virtio-queue) the fence should be associated with.
    
    The new capability sets (gfxstream, venus etc.) are only defined in
    the virtio-gpu spec and not defined in the header.
    
    Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Acked-by: Lingfeng Yang <lfy@google.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20210921232024.817-2-gurchetansingh@chromium.org
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    
    (cherry picked from commit 34268c9dde4cbae0b701b66c44497da068f418ee)
    
    Conflicts:
       include/uapi/linux/virtio_gpu.h
    
    BUG=b:176158390, b:203009046, b:230100768
    TEST=gfxbench and deqp-cts in arcvm
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Change-Id: I6941143b20b55c7542bfe666a12a3b37e2a35016
    Disallow-Recycled-Builds: test-failures
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3469484
    Reviewed-by: Chia-I Wu <olv@google.com>
    Reviewed-by: Ryan Neph <ryanneph@google.com>
    Signed-off-by: Richard Fung <richardfung@google.com>

--- include/uapi/linux/virtio_gpu.h
+++ include/uapi/linux/virtio_gpu.h
@@ -61,7 +61,7 @@
 #define VIRTIO_GPU_F_RESOURCE_BLOB       3
 /*
  * VIRTIO_GPU_CMD_CREATE_CONTEXT with
- * context_init
+ * context_init and multiple timelines
  */
 #define VIRTIO_GPU_F_CONTEXT_INIT        4
 
@@ -132,14 +132,20 @@ enum virtio_gpu_shm_id {
 	VIRTIO_GPU_SHM_ID_HOST_VISIBLE = 1
 };
 
-#define VIRTIO_GPU_FLAG_FENCE (1 << 0)
+#define VIRTIO_GPU_FLAG_FENCE         (1 << 0)
+/*
+ * If the following flag is set, then ring_idx contains the index
+ * of the command ring that needs to used when creating the fence
+ */
+#define VIRTIO_GPU_FLAG_INFO_RING_IDX (1 << 1)
 
 struct virtio_gpu_ctrl_hdr {
 	__le32 type;
 	__le32 flags;
 	__le64 fence_id;
 	__le32 ctx_id;
-	__le32 padding;
+	__u8 ring_idx;
+	__u8 padding[3];
 };
 
 /* data passed in the cursor vq */
@@ -279,7 +285,7 @@ struct virtio_gpu_resource_create_3d {
 };
 
 /* VIRTIO_GPU_CMD_CTX_CREATE */
-#define VIRTIO_GPU_CONTEXT_INIT_CAPSET_ID_MASK 0x00ff
+#define VIRTIO_GPU_CONTEXT_INIT_CAPSET_ID_MASK 0x000000ff
 struct virtio_gpu_ctx_create {
 	struct virtio_gpu_ctrl_hdr hdr;
 	__le32 nlen;
