06a5840b16fd9cd7f076892de86fe995322b2e21
Avri Altman <avri.altman@wdc.com>
2021-10-30 09:23:01 +0300

    UPSTREAM: scsi: ufs: ufshpb: Remove HPB2.0 flows
    The Host Performance Buffer feature allows UFS read commands to carry the
    physical media addresses along with the LBAs, thus allowing less internal
    L2P-table switches in the device.  HPB1.0 allowed a single LBA, while
    HPB2.0 increases this capacity up to 255 blocks.
    
    Carrying more than a single record, the read operation is no longer purely
    of type "read" but a "hybrid" command: Writing the physical address to the
    device in one operation and reading back the required payload in another.
    
    The JEDEC HPB spec defines two commands for this operation:
    HPB-WRITE-BUFFER (0x2) to write the physical addresses to device, and
    HPB-READ to read the payload.
    
    With the current HPB design the UFS driver has no alternative but to divide
    the READ request into 2 separate commands: HPB-WRITE-BUFFER and HPB-READ.
    This causes a great deal of aggravation to the block layer guys who
    demanded that we completely revert the entire HPB driver regardless of the
    huge amount of corporate effort already invested in it.
    
    As a compromise, remove only the pieces that implement the 2.0
    specification. This is done as a matter of urgency for the final 5.15
    release.
    
    Link: https://lore.kernel.org/r/20211030062301.248-1-avri.altman@wdc.com
    Tested-by: Avri Altman <avri.altman@wdc.com>
    Tested-by: Bean Huo <beanhuo@micron.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Bean Huo <beanhuo@micron.com>
    Co-developed-by: James Bottomley <James.Bottomley@HansenPartnership.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>
    Signed-off-by: Avri Altman <avri.altman@wdc.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Bug: 204438323
    (cherry picked from commit 09d9e4d041876684d33f21d02bcdaea6586734f1)
    Change-Id: I81e4df5f95115491005ecea4cc39d1fa7b63b074
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- drivers/scsi/ufs/ufshpb.h
+++ drivers/scsi/ufs/ufshpb.h
@@ -239,8 +239,6 @@ struct ufshpb_lu {
 	int throttle_pre_req;
 	int num_inflight_map_req;
 	struct list_head lh_pre_req_free;
-	int cur_read_id;
-	int pre_req_min_tr_len;
 	int pre_req_max_tr_len;
 
 	/* cached L2P map management worker */
