a7793895b7901d4f8974a552766a54d44d0fc017
Stephen Dickey <quic_dickey@quicinc.com>
2021-11-04 15:23:24 -0700

    ANDROID: sched: Re-enable removed-from-mainline support required for CPU Pause
    __pick_migrate_task() was present in 5.10 and removed in
    5.15 in favor of the balance_push concept.
    
    Restore this function so that the ability to pick a task to migrate
    across all scheduling classes can be used by vendor modules needed to
    implement CPU Pause.
    
    Function Removed In commit 1cf12e08bc4d50a76b80c42a3109c53d8794a0c9
    sched/hotplug: Consolidate task migration on CPU unplug
    
    Bug: 205164003
    Change-Id: I56ecd96c32b49495132daefbfc106568ab84e528
    Signed-off-by: Stephen Dickey <quic_dickey@quicinc.com>
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1608,6 +1608,11 @@ struct rq_flags {
 #endif
 };
 
+#ifdef CONFIG_SMP
+extern struct rq *__migrate_task(struct rq *rq, struct rq_flags *rf,
+				 struct task_struct *p, int dest_cpu);
+#endif
+
 extern struct balance_callback balance_push_callback;
 
 /*
