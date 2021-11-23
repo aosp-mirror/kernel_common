6e8e1de2e790ffc26a8a406163380462374e9eff
Lee Jones <lee.jones@linaro.org>
2021-11-23 10:40:20 +0000

    ANDROID: arm64: process: Match upstream formatting when dumping memory areas
    When looking into running memory, functions are usually represented
    with their names followed by an offset.
    
    Bug: 178379135
    Fixes: 081513b5f5f79 ("ANDROID: arm64: process: dump memory around registers when displaying regs")
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: If24ef9040ce471d43534e20b9a08bdcd9321e437
    (cherry picked from commit df8b534e51541e55a1b1f0009ebcbb6a49f34b18)

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -183,7 +183,7 @@ static void show_data(unsigned long addr, int nbytes, const char *name)
 	if (addr < PAGE_OFFSET || addr > -256UL)
 		return;
 
-	printk("\n%s: %#lx:\n", name, addr);
+	printk("\n%s: %pS:\n", name, addr);
 
 	/*
 	 * round address down to a 32 bit boundary
