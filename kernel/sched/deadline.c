478f3ee1d3e5be03ecd3603643abfc1186468881
Xuewen Yan <xuewen.yan@unisoc.com>
2021-11-24 14:27:27 +0800

    ANDROID: Add vendor hook to the deadline scheduler
    android_rvh_select_task_rq_dl:
        To perform vendor-specific DL task placement.
    
    Bug: 207585485
    Test: build pass
    
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Change-Id: Ibe147276b52a1be877802a2e53e87ba3eb8c9161

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -17,6 +17,7 @@
  */
 #include "sched.h"
 #include "pelt.h"
+#include <trace/hooks/sched.h>
 
 struct dl_bandwidth def_dl_bandwidth;
 
@@ -1675,6 +1676,11 @@ select_task_rq_dl(struct task_struct *p, int cpu, int sd_flag, int flags)
 	struct task_struct *curr;
 	bool select_rq;
 	struct rq *rq;
+	int target_cpu = -1;
+
+	trace_android_rvh_select_task_rq_dl(p, cpu, sd_flag, flags, &target_cpu);
+	if (target_cpu >= 0)
+		return target_cpu;
 
 	if (sd_flag != SD_BALANCE_WAKE)
 		goto out;
