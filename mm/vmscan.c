2cb7ca6e1336cdbfb50623a6f73368cc9df2eb51
Peifeng Li <lipeifeng@oppo.com>
2022-02-28 15:25:30 +0800

    ANDROID: vendor_hooks: export shrink_slab
    Export shrink_slab to module for do shrink-memory action.
    
    Bug: 221768451
    Bug: 231802812
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: I5abe9ad419d64999b714d879c228625a243e90d1
    (cherry picked from commit 71d560e017415ead361cfc10cc50ef36a0545cc2)
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -672,7 +672,7 @@ static unsigned long shrink_slab_memcg(gfp_t gfp_mask, int nid,
  *
  * Returns the number of reclaimed slab objects.
  */
-static unsigned long shrink_slab(gfp_t gfp_mask, int nid,
+unsigned long shrink_slab(gfp_t gfp_mask, int nid,
 				 struct mem_cgroup *memcg,
 				 int priority)
 {
@@ -724,6 +724,7 @@ out:
 	cond_resched();
 	return freed;
 }
+EXPORT_SYMBOL_GPL(shrink_slab);
 
 void drop_slab_node(int nid)
 {
