9e4003f1f6ec1ba4ab4bf89a57c9a335afc7c5c0
Marc Zyngier <maz@kernel.org>
2022-04-19 19:27:50 +0100

    BACKPORT: KVM: arm64: Handle blocking WFIT instruction
    When trapping a blocking WFIT instruction, take it into account when
    computing the deadline of the background timer.
    
    The state is tracked with a new vcpu flag, and is gated by a new
    CPU capability, which isn't currently enabled.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20220419182755.601427-6-maz@kernel.org
    (cherry picked from commit 89f5074c503b6b6f181c0240c931f67bcaf266e9)
    [willdeacon@: Use kvm_vcpu_block() instead of kvm_vcpu_halt() in context]
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 233587962
    Bug: 233588291
    Change-Id: I9819233e1cb0df8a4bfb5018a47249b16fecae49

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -640,6 +640,7 @@ void kvm_vcpu_wfi(struct kvm_vcpu *vcpu)
 	preempt_enable();
 
 	kvm_vcpu_block(vcpu);
+	vcpu->arch.flags &= ~KVM_ARM64_WFIT;
 	kvm_clear_request(KVM_REQ_UNHALT, vcpu);
 
 	preempt_disable();
