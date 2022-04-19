12efc92c13f84eaeed17d09e22cfe38ab9de7de9
Mark Brown <broonie@kernel.org>
2022-04-19 12:22:19 +0100

    UPSTREAM: arm64/sme: Implement vector length configuration prctl()s
    As for SVE provide a prctl() interface which allows processes to
    configure their SME vector length.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20220419112247.711548-12-broonie@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit 9e4ab6c89109472082616f8d2f6ada7deaffe161)
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 233587962
    Bug: 233588291
    Change-Id: I99b778aea77b19bac68f1ddb61d77b51f64b49cb

--- include/uapi/linux/prctl.h
+++ include/uapi/linux/prctl.h
@@ -269,6 +269,15 @@ struct prctl_mm_map {
 # define PR_SCHED_CORE_SHARE_FROM	3 /* pull core_sched cookie to pid */
 # define PR_SCHED_CORE_MAX		4
 
+/* arm64 Scalable Matrix Extension controls */
+/* Flag values must be in sync with SVE versions */
+#define PR_SME_SET_VL			63	/* set task vector length */
+# define PR_SME_SET_VL_ONEXEC		(1 << 18) /* defer effect until exec */
+#define PR_SME_GET_VL			64	/* get task vector length */
+/* Bits common to PR_SME_SET_VL and PR_SME_GET_VL */
+# define PR_SME_VL_LEN_MASK		0xffff
+# define PR_SME_VL_INHERIT		(1 << 17) /* inherit across exec */
+
 #define PR_SET_VMA		0x53564d41
 # define PR_SET_VMA_ANON_NAME		0
 
