2d74ff4319d1c8a4254635d7a611562571a8e39f
Bart Van Assche <bvanassche@google.com>
2022-06-01 13:51:13 -0700

    UPSTREAM: scsi: ufs: Split the drivers/scsi/ufs directory
    Split the drivers/scsi/ufs directory into 'core' and 'host' directories
    under the drivers/ufs/ directory. Move shared header files into the
    include/ufs/ directory. This separation makes it clear which header files
    UFS drivers are allowed to include (include/ufs/*.h) and which header files
    UFS drivers are not allowed to include (drivers/ufs/core/*.h).
    
    Update the MAINTAINERS file. Add myself as a UFS reviewer.
    
    Link: https://lore.kernel.org/r/20220511212552.655341-1-bvanassche@acm.org
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Avri Altman <avri.altman@wdc.com>
    Cc: Bean Huo <beanhuo@micron.com>
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: Keoseong Park <keosung.park@samsung.com>
    Tested-by: Bean Huo <beanhuo@micron.com>
    Tested-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Bean Huo <beanhuo@micron.com>
    Acked-by: Avri Altman <avri.altman@wdc.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Bug: 234653003
    (cherry picked from commit dd11376b9f1b73aca3f8c6eb541486bbb6996f05)
    Change-Id: I9b6a7aae90d1b9806d4bcc784627f22f14870a42
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- include/ufs/ufshcd.h
+++ include/ufs/ufshcd.h
@@ -18,11 +18,11 @@
 #include <linux/devfreq.h>
 #include <linux/pm_runtime.h>
 #include <scsi/scsi_device.h>
-#include "unipro.h"
 #include <linux/android_kabi.h>
-#include "ufs.h"
-#include "ufs_quirks.h"
-#include "ufshci.h"
+#include <ufs/unipro.h>
+#include <ufs/ufs.h>
+#include <ufs/ufs_quirks.h>
+#include <ufs/ufshci.h>
 
 #define UFSHCD "ufshcd"
 
