55e91d79da8831a4299587fcbc4800d3848c5e38
Will Deacon <willdeacon@google.com>
2022-07-07 15:51:30 +0100

    Revert "ANDROID: KVM: arm64: Add __pkvm_host_share_guest hypercall"
    This reverts commit 2570e98d667b0bffe9bf22a5e88ed10fc90e68b6.
    
    Bug: 233587962
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: I90168fb7c16e743da75c19edce7edc8c29ea2002

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -452,12 +452,8 @@ struct kvm_vcpu_arch {
 	/* Don't run the guest (internal implementation need) */
 	bool pause;
 
-	union {
-		/* Cache some mmu pages needed inside spinlock regions */
-		struct kvm_mmu_memory_cache mmu_page_cache;
-		/* Pages to be donated to pkvm/EL2e if it runs out */
-		struct kvm_hyp_memcache pkvm_memcache;
-	};
+	/* Cache some mmu pages needed inside spinlock regions */
+	struct kvm_mmu_memory_cache mmu_page_cache;
 
 	/* Target CPU and feature flags */
 	int target;
