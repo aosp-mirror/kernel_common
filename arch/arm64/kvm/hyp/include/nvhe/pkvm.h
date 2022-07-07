841262de62d0504747465b44a4c4435c48c2628e
Will Deacon <willdeacon@google.com>
2022-07-07 15:51:22 +0100

    Revert "ANDROID: KVM: arm64: Reset primary vCPU according to PVM firmware boot protocol"
    This reverts commit 94d7fa9be48c6af66127abd6ab0a9b2c8fc8d123.
    
    Bug: 233587962
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: Ia079aadbc80cdc6cacc59440392ab64228633b62

--- arch/arm64/kvm/hyp/include/nvhe/pkvm.h
+++ arch/arm64/kvm/hyp/include/nvhe/pkvm.h
@@ -33,9 +33,6 @@ struct kvm_shadow_vm {
 	/* Pointers to the shadow vcpus of the shadow vm. */
 	struct kvm_vcpu *vcpus[KVM_MAX_VCPUS];
 
-	/* Primary vCPU pending entry to the pvmfw */
-	struct kvm_vcpu *pvmfw_entry_vcpu;
-
 	/* The host's kvm structure. */
 	struct kvm *host_kvm;
 
