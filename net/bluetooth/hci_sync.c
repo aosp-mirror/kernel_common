259f198d4fc77ebbb677cdead935ec97d5a47a01
Manish Mandlik <mmandlik@google.com>
2022-08-01 17:15:39 -0700

    FROMLIST: Bluetooth: Disable AdvMonitor SamplingPeriod while active scan
    Some controllers apply Sampling Period even while active scanning.
    So, to keep the behavior consistent across all controllers, don't
    use Sampling Period during active scanning to force the controller
    to report all advertisements even if it matches the monitor.
    
    Signed-off-by: Manish Mandlik <mmandlik@google.com>
    (am from https://patchwork.kernel.org/patch/12935875)
    
    BUG=b:219812269
    TEST=Run advmon_fg_bg_combination_tests
    
    Signed-off-by: Manish Mandlik <mmandlik@google.com>
    Change-Id: I5acde3b5af78dbd2ea078d5eb4158d23b496ac87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3803908
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Commit-Queue: Manish Mandlik <mmandlik@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Tested-by: Manish Mandlik <mmandlik@chromium.org>
    Kcr-patch: cc8dbc01d516ebf03abcd6e7acdae9a79866c2b2aa39fe106d9d8bfe.patch

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -5281,6 +5281,9 @@ int hci_stop_discovery_sync(struct hci_dev *hdev)
 	if (use_ll_privacy(hdev))
 		hci_resume_advertising_sync(hdev);
 
+	/* Sampling Period is disabled while active scanning, re-enable it */
+	msft_set_active_scan(hdev, false);
+
 	/* No further actions needed for LE-only discovery */
 	if (d->type == DISCOV_TYPE_LE)
 		return 0;
@@ -5861,6 +5864,9 @@ int hci_start_discovery_sync(struct hci_dev *hdev)
 
 	if (err)
 		return err;
+
+	/* Disable Sampling Period while active scanning */
+	msft_set_active_scan(hdev, true);
 
 	bt_dev_dbg(hdev, "timeout %u ms", jiffies_to_msecs(timeout));
 
