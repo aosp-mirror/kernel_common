2533256a0e8bf02cb6acace9bf3bd3601d115b5b
Mark Brown <broonie@kernel.org>
2022-08-18 22:36:12 +0100

    UPSTREAM: arm64/sysreg: Guard SYS_FIELD_ macros for asm
    The SYS_FIELD_ macros are not safe for assembly contexts, move them inside
    the guarded section.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20220818213613.733091-3-broonie@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit a10edea4efbba4e8756f493781e953dd3592f24a)
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 233587962
    Bug: 233588291
    Change-Id: Ib3302ddf0c2a1c66edef62f1b3b003ac76609141

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -1419,8 +1419,6 @@
 	par;								\
 })
 
-#endif
-
 #define SYS_FIELD_GET(reg, field, val)		\
 		 FIELD_GET(reg##_##field##_MASK, val)
 
@@ -1429,5 +1427,7 @@
 
 #define SYS_FIELD_PREP_ENUM(reg, field, val)		\
 		 FIELD_PREP(reg##_##field##_MASK, reg##_##field##_##val)
+
+#endif
 
 #endif	/* __ASM_SYSREG_H */
