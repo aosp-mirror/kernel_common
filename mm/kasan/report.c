4810e13608df79f43efdcedb1383dce480740cf2
Andrey Konovalov <andreyknvl@google.com>
2022-09-05 23:05:41 +0200

    UPSTREAM: kasan: introduce complete_report_info
    Introduce a complete_report_info() function that fills in the
    first_bad_addr field of kasan_report_info instead of doing it in
    kasan_report_*().
    
    This function will be extended in the next patch.
    
    Link: https://lkml.kernel.org/r/8eb1a9bd01f5d31eab4524da54a101b8720b469e.1662411799.git.andreyknvl@google.com
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Reviewed-by: Marco Elver <elver@google.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Evgenii Stepanov <eugenis@google.com>
    Cc: Peter Collingbourne <pcc@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Bug: 254721825
    (cherry picked from commit 015b109f1f7a799a51def6be37a53b650c4a8fda)
    Change-Id: I0729c89e04bd675b30849998feb9967475cf240f
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>

--- mm/kasan/report.c
+++ mm/kasan/report.c
@@ -408,6 +408,17 @@ static void print_report(struct kasan_report_info *info)
 	}
 }
 
+static void complete_report_info(struct kasan_report_info *info)
+{
+	void *addr = kasan_reset_tag(info->access_addr);
+
+	if (info->type == KASAN_REPORT_ACCESS)
+		info->first_bad_addr = kasan_find_first_bad_addr(
+					info->access_addr, info->access_size);
+	else
+		info->first_bad_addr = addr;
+}
+
 void kasan_report_invalid_free(void *ptr, unsigned long ip, enum kasan_report_type type)
 {
 	unsigned long flags;
@@ -425,10 +436,11 @@ void kasan_report_invalid_free(void *ptr, unsigned long ip, enum kasan_report_ty
 
 	info.type = type;
 	info.access_addr = ptr;
-	info.first_bad_addr = kasan_reset_tag(ptr);
 	info.access_size = 0;
 	info.is_write = false;
 	info.ip = ip;
+
+	complete_report_info(&info);
 
 	print_report(&info);
 
@@ -458,10 +470,11 @@ bool kasan_report(unsigned long addr, size_t size, bool is_write,
 
 	info.type = KASAN_REPORT_ACCESS;
 	info.access_addr = ptr;
-	info.first_bad_addr = kasan_find_first_bad_addr(ptr, size);
 	info.access_size = size;
 	info.is_write = is_write;
 	info.ip = ip;
+
+	complete_report_info(&info);
 
 	print_report(&info);
 
