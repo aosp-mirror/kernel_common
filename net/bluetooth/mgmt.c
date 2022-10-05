ff94e4fbba53855e83f2087fc1c97db4b16b83bb
Jeremy Wu <jrwu@google.com>
2022-10-05 05:27:40 +0000

    CHROMIUM: bluetooth: Fix mgmt response to get_sco_codec_cap
    Currently there is some inconsistency between the response packet and
    num_rp_codecs returned in `get_sco_codec_capabilities`.
    
    It would always increment `num_rp_codecs` regardless of availability
    (reflected in whether the codecs is pushed into the packet).
    
    In this CL, we prevent the number from being incremented unless
    intended, and do minor refactoring for symmetry and readability.
    
    UPSTREAM-TASK=b:254038486
    BUG=b:250661041
    TEST=Verify codec capabilities
    
    Change-Id: I3f565a33e6922c56115384040ba5fcaf3716b6ea
    Signed-off-by: Jeremy Wu <jrwu@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3958883
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4169802
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>
    Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -9229,8 +9229,7 @@ static int floss_get_sco_codec_capabilities(struct sock *sk,
 	struct mgmt_cp_get_codec_capabilities *cp = data;
 	struct mgmt_rp_get_codec_capabilities *rp;
 	struct codec_list *c;
-	struct hci_codec_caps *caps;
-	int i, j, num_rp_codecs;
+	int i, num_rp_codecs;
 	int err;
 	size_t total_size = sizeof(struct mgmt_rp_get_codec_capabilities);
 	bool wbs_supported = false;
@@ -9261,7 +9260,6 @@ static int floss_get_sco_codec_capabilities(struct sock *sk,
 				total_size += sizeof(struct mgmt_bt_codec);
 			break;
 		case MGMT_SCO_CODEC_MSBC:
-			num_rp_codecs = 0;
 			hci_dev_lock(hdev);
 			list_for_each_entry(c, &hdev->local_codecs, list) {
 				/* 0x01 - HCI Transport (Codec supported over BR/EDR SCO and eSCO)
@@ -9270,12 +9268,7 @@ static int floss_get_sco_codec_capabilities(struct sock *sk,
 				if (c->transport != 0x01 || c->id != 0x05)
 					continue;
 
-				num_rp_codecs++;
-				for (j = 0, caps = c->caps; j < c->num_caps; j++) {
-					total_size += 1 + caps->len;
-					caps = (void *)&caps->data[caps->len];
-				}
-				total_size += sizeof(struct mgmt_bt_codec);
+				total_size += sizeof(struct mgmt_bt_codec) + c->caps->len;
 				break;
 			}
 			hci_dev_unlock(hdev);
@@ -9291,7 +9284,6 @@ static int floss_get_sco_codec_capabilities(struct sock *sk,
 		return -ENOMEM;
 
 	rp->hci_id = hdev->id;
-	rp->offload_capable = false;
 
 	// Copy codec information to return.
 	ptr = (u8 *)rp->codecs;
@@ -9301,41 +9293,40 @@ static int floss_get_sco_codec_capabilities(struct sock *sk,
 		switch (cp->codecs[i]) {
 		case MGMT_SCO_CODEC_CVSD:
 			rc->codec = cp->codecs[i];
-			ptr += sizeof(*rc);
+			ptr += sizeof(struct mgmt_bt_codec);
 			num_rp_codecs++;
 			break;
 		case MGMT_SCO_CODEC_MSBC_TRANSPARENT:
 			if (wbs_supported) {
 				rc->codec = cp->codecs[i];
 				rc->packet_size = hdev->wbs_pkt_len;
-				ptr += sizeof(*rc);
+				ptr += sizeof(struct mgmt_bt_codec);
 				num_rp_codecs++;
 			}
 			break;
 		case MGMT_SCO_CODEC_MSBC:
-			/* Need to read the support from the controller and then assign to TRUE
-			 * for now by default enable it as TRUE
-			 */
-			rp->offload_capable = true;
-
 			hci_dev_lock(hdev);
 			list_for_each_entry(c, &hdev->local_codecs, list) {
 				if (c->transport != 0x01 || c->id != 0x05)
 					continue;
 
+				/* Need to read the support from the controller and then assign
+				 * to TRUE for now by default enable it as TRUE
+				 */
+				rp->offload_capable = true;
+
+				if (hdev->get_data_path_id)
+					hdev->get_data_path_id(hdev, &rc->data_path);
+
 				rc->codec = cp->codecs[i];
 				rc->packet_size = c->len;
 				rc->data_length = c->caps->len;
 				memcpy(rc->data, c->caps, c->caps->len);
+				ptr += sizeof(struct mgmt_bt_codec) + c->caps->len;
+				num_rp_codecs++;
 				break;
 			}
 			hci_dev_unlock(hdev);
-
-			ptr += sizeof(*rc);
-			num_rp_codecs++;
-
-			if (hdev->get_data_path_id)
-				hdev->get_data_path_id(hdev, &rc->data_path);
 			break;
 		default:
 			break;
