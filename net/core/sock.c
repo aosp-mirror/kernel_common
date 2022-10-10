ea6e87db9037f285b3d3d269bb405e0e8dd15d34
Paul Moore <paul@paul-moore.com>
2022-10-10 12:31:21 -0400

    lsm: make security_socket_getpeersec_stream() sockptr_t safe
    [ Upstream commit b10b9c342f7571f287fd422be5d5c0beb26ba974 ]
    
    Commit 4ff09db1b79b ("bpf: net: Change sk_getsockopt() to take the
    sockptr_t argument") made it possible to call sk_getsockopt()
    with both user and kernel address space buffers through the use of
    the sockptr_t type.  Unfortunately at the time of conversion the
    security_socket_getpeersec_stream() LSM hook was written to only
    accept userspace buffers, and in a desire to avoid having to change
    the LSM hook the commit author simply passed the sockptr_t's
    userspace buffer pointer.  Since the only sk_getsockopt() callers
    at the time of conversion which used kernel sockptr_t buffers did
    not allow SO_PEERSEC, and hence the
    security_socket_getpeersec_stream() hook, this was acceptable but
    also very fragile as future changes presented the possibility of
    silently passing kernel space pointers to the LSM hook.
    
    There are several ways to protect against this, including careful
    code review of future commits, but since relying on code review to
    catch bugs is a recipe for disaster and the upstream eBPF maintainer
    is "strongly against defensive programming", this patch updates the
    LSM hook, and all of the implementations to support sockptr_t and
    safely handle both user and kernel space buffers.
    
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    Acked-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Stable-dep-of: 5a287d3d2b9d ("lsm: fix default return value of the socket_getpeersec_*() hooks")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1503,7 +1503,8 @@ static int sk_getsockopt(struct sock *sk, int level, int optname,
 		break;
 
 	case SO_PEERSEC:
-		return security_socket_getpeersec_stream(sock, optval.user, optlen.user, len);
+		return security_socket_getpeersec_stream(sock,
+							 optval, optlen, len);
 
 	case SO_MARK:
 		v.val = sk->sk_mark;
