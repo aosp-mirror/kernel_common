9f22f9085941bed248f3da0494ec7d48003bab6f
Arthur Simchaev <Arthur.Simchaev@wdc.com>
2022-12-11 15:05:09 +0200

    FROMGIT: scsi: ufs: core: Remove redundant desc_size variable from hba
    Always read the descriptor with QUERY_DESC_MAX_SIZE.  According to the
    spec, the device returns the actual size.
    
    Change-Id: I29053a66d1f65f674fba6d7e1a6c6e7f546eec74
    Signed-off-by: Arthur Simchaev <Arthur.Simchaev@wdc.com>
    Reviewed-by: Bean Huo <beanhuo@micron.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Bug: 258234315
    (cherry picked from commit f2a89b071b26b79abbe892ce88c4d674d1f21f63 git://git.kernel.org/pub/scm/linux/kernel/git/mkp/scsi.git for-next)
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- include/ufs/ufshcd.h
+++ include/ufs/ufshcd.h
@@ -991,7 +991,6 @@ struct ufs_hba {
 	bool is_urgent_bkops_lvl_checked;
 
 	struct rw_semaphore clk_scaling_lock;
-	unsigned char desc_size[QUERY_DESC_IDN_MAX];
 	atomic_t scsi_block_reqs_cnt;
 
 	struct device		bsg_dev;
@@ -1261,9 +1260,6 @@ void ufshcd_clkgate_delay_set(struct device *dev, unsigned long value);
 
 int ufshcd_freeze_scsi_devs(struct ufs_hba *hba, u64 timeout_us);
 void ufshcd_unfreeze_scsi_devs(struct ufs_hba *hba);
-
-void ufshcd_map_desc_id_to_length(struct ufs_hba *hba, enum desc_idn desc_id,
-				  int *desc_length);
 
 u32 ufshcd_get_local_unipro_ver(struct ufs_hba *hba);
 
