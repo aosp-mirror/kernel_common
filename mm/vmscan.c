65055e7670266dde6ce57b72faa4225b067ad87e
Koki Ryu <kokiryu@chromium.org>
2022-12-13 03:26:50 +0000

    Revert "Add userspace notification for MGLRU generation creation"
    This reverts commit 02e000eb378687efbff757b64b628c9b8d0bbeee.
    
    Reason for revert: Causing health.DiagnosticsRun.cpu_stress to fail
    See https://buganizer.corp.google.com/issues/261753137#comment9
    https://b.corp.google.com/issues/260967742 seems to be the cause, so the same problem can happen in the arcvm kernel, too.
    
    Original change's description:
    > Add userspace notification for MGLRU generation creation
    >
    > Add sysfs_notify in inc_max_seq function so that every time a new MGLRU
    > generation is created so that user space programs can notice that by
    > polling /sys/kernel/mm/lru_gen/admin.
    >
    > BUG=b:245255700
    > TEST=Follow these steps:
    > - Open /sys/kernel/mm/lru_gen/admin and read it once.
    > - poll the fd you opend and wait for the POLLPRI event.
    > - Put some memory pressure on the kernel and see if the poll ever
    >   returns.
    >
    > Signed-off-by: Koki Ryu <kokiryu@google.com>
    >
    > Change-Id: I11023f002ecf235a9933bb87fbd3183e9b08a9ca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3936130
    > Commit-Queue: Kameron Lutes <kalutes@chromium.org>
    > Tested-by: Koki Ryu <kokiryu@chromium.org>
    > Reviewed-by: Charles William Dick <cwd@google.com>
    > Reviewed-by: Kameron Lutes <kalutes@chromium.org>
    
    Bug: b:245255700
    Change-Id: I40313bea25e026b022da69eb5e2a8bbd6ca838bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4100085
    Auto-Submit: Koki Ryu <kokiryu@chromium.org>
    Commit-Queue: Suleiman Souhlal <suleiman@chromium.org>
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Tested-by: Koki Ryu <kokiryu@chromium.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3770,7 +3770,6 @@ static bool try_to_inc_max_seq(struct lruvec *lruvec, unsigned long max_seq,
 	VM_BUG_ON(max_seq != READ_ONCE(lrugen->max_seq));
 
 	inc_max_seq(lruvec, max_seq);
-	sysfs_notify(mm_kobj, "lru_gen", "admin");
 	/* either we see any waiters or they will see updated max_seq */
 	if (wq_has_sleeper(&mm_list->nodes[nid].wait))
 		wake_up_all(&mm_list->nodes[nid].wait);
