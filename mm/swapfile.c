3f0b6ce989f7f3772da91f787f3893d80452276b
Koki Ryu <kokiryu@google.com>
2023-01-13 12:30:40 +0900

    Revert "BACKPORT: FROMLIST: mm: multigenerational lru: groundwork"
    This reverts commit 0e822f205232b9b22a346d0dd7c9b325050b1f73.
    
    BUG=b:265245396
    TEST=Confirm that the kernel can be successfully built
    
    Change-Id: I5c9124788c209370ae4fd7a1b8a175903a0813b9
    Signed-off-by: Koki Ryu <kokiryu@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4199131
    Commit-Queue: Koki Ryu <kokiryu@chromium.org>
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Tested-by: Koki Ryu <kokiryu@chromium.org>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2756,7 +2756,6 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 	err = 0;
 	atomic_inc(&proc_poll_event);
 	wake_up_interruptible(&proc_poll_wait);
-	lru_gen_set_state(false, false, true);
 
 out_dput:
 	filp_close(victim, NULL);
@@ -3408,7 +3407,6 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 	mutex_unlock(&swapon_mutex);
 	atomic_inc(&proc_poll_event);
 	wake_up_interruptible(&proc_poll_wait);
-	lru_gen_set_state(true, false, true);
 
 	error = 0;
 	goto out;
