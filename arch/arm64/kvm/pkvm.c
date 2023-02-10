00d7a3912c3c671782dc58b40fad4fadac148f34
Vincent Donnefort <vdonnefort@google.com>
2023-02-10 14:17:00 +0000

    ANDROID: KVM: arm64: Enable in-pKVM-module in-hyp event tracing
    pKVM modules? pKVM tracing? Why not both? Bring the tracing flexibility
    to pKVM module developers. Just need to follow this recipe:
    
     A. Declare the hyp events in separated events.h file.
    
     B. Define the hyp events for the host (They will be automatically
        registered for the host by the module loader)
    
        #define HYP_EVENT_FILE events.h
        #include <asm/kvm_define_hypevents.h>
    
     C. Define the hyp events for the hypervisor
    
        #define HYP_EVENT_FILE events,h
        #include <nvhe/define_events.h>
    
     D. Register hyp_event_ids from the hypervisor
    
        ops->register_hyp_event_ids(__hyp_event_ids_start,
                                    __hyp_event_ids_end);
    
     E. Define tracing functions (tracing_{reserve,commit}_entry) using pkvm_module_ops callbacks
    
        void *tracing_reserve_entry (unsigned long length)
        {
            return ops->tracing_reserve_commit(length);
        }
        ...
    
     F. Call your newly created events
    
        #include <nvhe/trace.h>
    
        trace_my_module_event();
    
    Bug: 278749606
    Bug: 268495982
    Change-Id: I20ec67767afae65f82cba43159e282e0b0b3d103
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -28,6 +28,7 @@
 #include <linux/init_syscalls.h>
 
 #include "hyp_constants.h"
+#include "hyp_trace.h"
 
 DEFINE_STATIC_KEY_FALSE(kvm_protected_mode_initialized);
 
@@ -829,6 +830,7 @@ int __pkvm_load_el2_module(struct module *this, unsigned long *token)
 		{ &mod->text, KVM_PGTABLE_PROT_R | KVM_PGTABLE_PROT_X },
 		{ &mod->bss, KVM_PGTABLE_PROT_R | KVM_PGTABLE_PROT_W },
 		{ &mod->rodata, KVM_PGTABLE_PROT_R },
+		{ &mod->event_ids, KVM_PGTABLE_PROT_R },
 		{ &mod->data, KVM_PGTABLE_PROT_R | KVM_PGTABLE_PROT_W },
 	};
 	void *start, *end, *hyp_va;
@@ -892,6 +894,12 @@ int __pkvm_load_el2_module(struct module *this, unsigned long *token)
 	 * inaccessible.
 	 */
 	kmemleak_free_part(start, size);
+
+	ret = hyp_trace_init_mod_events(mod->hyp_events,
+					mod->event_ids.start,
+					mod->nr_hyp_events);
+	if (ret)
+		kvm_err("Failed to init module events: %d\n", ret);
 
 	ret = pkvm_map_module_sections(secs_map + secs_first, hyp_va,
 				       ARRAY_SIZE(secs_map) - secs_first);
