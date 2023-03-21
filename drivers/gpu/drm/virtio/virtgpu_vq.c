7ff20e76d8b2c2bb8e7c210cb2644d34ce1cfe8b
Richard Fung <richardfung@google.com>
2023-03-21 20:18:28 +0000

    Revert "FROMGIT: drm/virtio: Spiff out cmd queue/response traces"
    This prevents the build from working when using the out-of-tree version
    of virtgpu. We probably will want to merge this in there if necessary.
    
    This reverts commit 5aca04ea97cc32743d17210897ae5bc3c3968e2f.
    
    Bug: 269521616
    Test: SKIP_MRPROPER=1 BUILD_CONFIG=common/build.config.gki.x86_64 build/build.sh -j
    LTO=none BUILD_CONFIG=common-modules/virtual-device/build.config.virtual_device.x86_64 build/build.sh
    
    Change-Id: Ia66a0ca7dd6c4384ec6ffccaf18bf83931448cd9
    Signed-off-by: Richard Fung <richardfung@google.com>

--- drivers/gpu/drm/virtio/virtgpu_vq.c
+++ drivers/gpu/drm/virtio/virtgpu_vq.c
@@ -213,7 +213,7 @@ void virtio_gpu_dequeue_ctrl_func(struct work_struct *work)
 	list_for_each_entry(entry, &reclaim_list, list) {
 		resp = (struct virtio_gpu_ctrl_hdr *)entry->resp_buf;
 
-		trace_virtio_gpu_cmd_response(vgdev->ctrlq.vq, resp, entry->seqno);
+		trace_virtio_gpu_cmd_response(vgdev->ctrlq.vq, resp);
 
 		if (resp->type != cpu_to_le32(VIRTIO_GPU_RESP_OK_NODATA)) {
 			if (le32_to_cpu(resp->type) >= VIRTIO_GPU_RESP_ERR_UNSPEC) {
@@ -259,10 +259,6 @@ void virtio_gpu_dequeue_cursor_func(struct work_struct *work)
 	spin_unlock(&vgdev->cursorq.qlock);
 
 	list_for_each_entry_safe(entry, tmp, &reclaim_list, list) {
-		struct virtio_gpu_ctrl_hdr *resp =
-			(struct virtio_gpu_ctrl_hdr *)entry->resp_buf;
-
-		trace_virtio_gpu_cmd_response(vgdev->cursorq.vq, resp, entry->seqno);
 		list_del(&entry->list);
 		free_vbuf(vgdev, entry);
 	}
@@ -355,8 +351,7 @@ again:
 	ret = virtqueue_add_sgs(vq, sgs, outcnt, incnt, vbuf, GFP_ATOMIC);
 	WARN_ON(ret);
 
-	vbuf->seqno = ++vgdev->ctrlq.seqno;
-	trace_virtio_gpu_cmd_queue(vq, virtio_gpu_vbuf_ctrl_hdr(vbuf), vbuf->seqno);
+	trace_virtio_gpu_cmd_queue(vq, virtio_gpu_vbuf_ctrl_hdr(vbuf));
 
 	atomic_inc(&vgdev->pending_commands);
 
@@ -468,10 +463,8 @@ retry:
 		spin_lock(&vgdev->cursorq.qlock);
 		goto retry;
 	} else {
-		vbuf->seqno = ++vgdev->cursorq.seqno;
 		trace_virtio_gpu_cmd_queue(vq,
-			virtio_gpu_vbuf_ctrl_hdr(vbuf),
-			vbuf->seqno);
+			virtio_gpu_vbuf_ctrl_hdr(vbuf));
 
 		notify = virtqueue_kick_prepare(vq);
 	}
