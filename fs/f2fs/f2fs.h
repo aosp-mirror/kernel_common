da79c8ced3a0a79fe56cb499a0bf8c6a91122a1c
Chao Yu <chao@kernel.org>
2023-04-02 19:27:06 +0800

    f2fs: fix to recover quota data correctly
    With -O quota mkfs option, xfstests generic/417 fails due to fsck detects
    data corruption on quota inodes.
    
    [ASSERT] (fsck_chk_quota_files:2051)  --> Quota file is missing or invalid quota file content found.
    
    The root cause is there is a hole f2fs doesn't hold quota inodes,
    so all recovered quota data will be dropped due to SBI_POR_DOING
    flag was set.
    - f2fs_fill_super
     - f2fs_recover_orphan_inodes
      - f2fs_enable_quota_files
      - f2fs_quota_off_umount
    <--- quota inodes were dropped --->
     - f2fs_recover_fsync_data
      - f2fs_enable_quota_files
      - f2fs_quota_off_umount
    
    This patch tries to eliminate the hole by holding quota inodes
    during entire recovery flow as below:
    - f2fs_fill_super
     - f2fs_recover_quota_begin
     - f2fs_recover_orphan_inodes
     - f2fs_recover_fsync_data
     - f2fs_recover_quota_end
    
    Then, recovered quota data can be persisted after SBI_POR_DOING
    is cleared.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1292,6 +1292,7 @@ enum {
 	SBI_QUOTA_NEED_REPAIR,			/* quota file may be corrupted */
 	SBI_IS_RESIZEFS,			/* resizefs is in process */
 	SBI_IS_FREEZING,			/* freezefs is in process */
+	SBI_IS_WRITABLE,			/* remove ro mountoption transiently */
 	MAX_SBI_FLAG,
 };
 
