badfc4d0051bc4fb7548e877859adfc22b60c896
Paul Lawrence <paullawrence@google.com>
2023-04-04 17:04:55 +0000

    ANDROID: fuse: Support errors from fuse daemon in canonical path
    Previously errors from the daemon in FUSE_CANONICAL_PATH were simply
    ignored. In order to block inotifys, it is useful to be able to return
    errors from this opcode.
    
    [drosen: I've combined this patch with a few others that fix dealing
    with error codes (mostly ENOSYS) to make it harder to lose these again
    in the future:
    
    ANDROID: fsnotify: Do not notify lower fs of open when ENOSYS
    ANDROID: fuse: Skip canonical path logic if ENOSYS
    ANDROID: fuse: Fix the issue of fuse_dentry_canonical_path ]
    
    Bug: 238619640
    Test: inotify no longer works on /storage/emulated/0/Android/media but
          does on child folders
    Bug: 326995824
    Test: cts-tradefed run commandAndExit cts -m CtsOsTestCases -t android.os.cts.FileObserverTest
    Bug: 330136711
    Test: ./cts-tradefed run commandAndExit cts -m CtsIncidentHostTestCases -t com.android.server.cts.ErrorsTest#testTombstone
    
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: Icb15c090c6286c174338471a787712f8388de316

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -333,7 +333,7 @@ static struct vfsmount *fuse_dentry_automount(struct path *path)
  * look up paths on its own. Instead, we handle the lookup as a special case
  * inside of the write request.
  */
-static void fuse_dentry_canonical_path(const struct path *path,
+static int fuse_dentry_canonical_path(const struct path *path,
 				       struct path *canonical_path)
 {
 	struct inode *inode = d_inode(path->dentry);
@@ -343,9 +343,12 @@ static void fuse_dentry_canonical_path(const struct path *path,
 	char *path_name;
 	int err;
 
+	if (fm->fc->no_dentry_canonical_path)
+		goto out;
+
 	path_name = (char *)get_zeroed_page(GFP_KERNEL);
 	if (!path_name)
-		goto default_path;
+		return -ENOMEM;
 
 	args.opcode = FUSE_CANONICAL_PATH;
 	args.nodeid = get_node_id(inode);
@@ -359,11 +362,18 @@ static void fuse_dentry_canonical_path(const struct path *path,
 	err = fuse_simple_request(fm, &args);
 	free_page((unsigned long)path_name);
 	if (err > 0)
-		return;
-default_path:
+		return 0;
+	if (err < 0 && err != -ENOSYS)
+		return err;
+
+	if (err == -ENOSYS)
+		fm->fc->no_dentry_canonical_path = 1;
+
+out:
 	canonical_path->dentry = path->dentry;
 	canonical_path->mnt = path->mnt;
 	path_get(canonical_path);
+	return 0;
 }
 
 const struct dentry_operations fuse_dentry_operations = {
