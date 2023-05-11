10bca2e53c44ec50932a94c1555befd810c7ba24
Qais Yousef <qyousef@google.com>
2023-05-11 15:34:13 +0000

    ANDROID: Add new hook to enable overriding uclamp_validate()
    We want to add more special values, specifically for uclamp_max so that
    it can be set automatically to the most efficient value based on the
    core it's running on.
    
    Bug: 283761535
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: I57343c4544f6cac621c855cbb94de0b8d80c51fa

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -379,6 +379,11 @@ DECLARE_RESTRICTED_HOOK(android_rvh_util_est_update,
 	TP_PROTO(struct cfs_rq *cfs_rq, struct task_struct *p, bool task_sleep, int *ret),
 	TP_ARGS(cfs_rq, p, task_sleep, ret), 1);
 
+DECLARE_HOOK(android_vh_uclamp_validate,
+	TP_PROTO(struct task_struct *p, const struct sched_attr *attr,
+		 bool user, int *ret, bool *done),
+	TP_ARGS(p, attr, user, ret, done));
+
 DECLARE_HOOK(android_vh_setscheduler_uclamp,
 	TP_PROTO(struct task_struct *tsk, int clamp_id, unsigned int value),
 	TP_ARGS(tsk, clamp_id, value));
