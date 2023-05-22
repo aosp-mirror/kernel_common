f17cd56e4e4273eef892e424adb030ec8e96b095
zhengyan <zhengyan@asrmicro.com>
2023-05-22 19:06:43 +0800

    BACKPORT: irqchip/gic-v3: Work around affinity issues on ASR8601
    The ASR8601 SoC combines ARMv8.2 CPUs from ARM with a GIC-500,
    also from ARM. However, the two are incompatible as the former
    expose an affinity in the form of (cluster, core, thread),
    while the latter can only deal with (cluster, core). If nothing
    is done, the GIC simply cannot route interrupts to the CPUs.
    
    Implement a workaround that shifts the affinity down by a level,
    ensuring the delivery of interrupts despite the implementation
    mismatch.
    
    Signed-off-by: zhengyan <zhengyan@asrmicro.com>
    [maz: rewrote commit message, reimplemented the workaround
     in a manageable way]
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    
    Bug: 282025214
    Change-Id: Id62a4f45ec52c1de543bbd712879dc34688d7904
    (cherry picked from commit b4d81fab1ed0b302c71a869e5b93d81dfbfd3175)
    [meitao: Resolved minor conflict in drivers/irqchip/irq-gic-v3.c ]
    Signed-off-by: meitaogao <meitaogao@asrmicro.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -41,6 +41,7 @@
 
 #define FLAGS_WORKAROUND_GICR_WAKER_MSM8996	(1ULL << 0)
 #define FLAGS_WORKAROUND_CAVIUM_ERRATUM_38539	(1ULL << 1)
+#define FLAGS_WORKAROUND_ASR_ERRATUM_8601001	(1ULL << 3)
 
 #define GIC_IRQ_TYPE_PARTITION	(GIC_IRQ_TYPE_LPI + 1)
 
@@ -613,6 +614,11 @@ static u64 gic_cpu_to_affinity(int cpu)
 {
 	u64 mpidr = cpu_logical_map(cpu);
 	u64 aff;
+
+	/* ASR8601 needs to have its affinities shifted down... */
+	if (unlikely(gic_data.flags & FLAGS_WORKAROUND_ASR_ERRATUM_8601001))
+		mpidr = (MPIDR_AFFINITY_LEVEL(mpidr, 1)	|
+			 (MPIDR_AFFINITY_LEVEL(mpidr, 2) << 8));
 
 	aff = ((u64)MPIDR_AFFINITY_LEVEL(mpidr, 3) << 32 |
 	       MPIDR_AFFINITY_LEVEL(mpidr, 2) << 16 |
@@ -1641,11 +1647,25 @@ static bool gic_enable_quirk_hip06_07(void *data)
 	return false;
 }
 
+static bool gic_enable_quirk_asr8601(void *data)
+{
+	struct gic_chip_data *d = data;
+
+	d->flags |= FLAGS_WORKAROUND_ASR_ERRATUM_8601001;
+
+	return true;
+}
+
 static const struct gic_quirk gic_quirks[] = {
 	{
 		.desc	= "GICv3: Qualcomm MSM8996 broken firmware",
 		.compatible = "qcom,msm8996-gic-v3",
 		.init	= gic_enable_quirk_msm8996,
+	},
+	{
+		.desc	= "GICv3: ASR erratum 8601001",
+		.compatible = "asr,asr8601-gic-v3",
+		.init	= gic_enable_quirk_asr8601,
 	},
 	{
 		.desc	= "GICv3: HIP06 erratum 161010803",
