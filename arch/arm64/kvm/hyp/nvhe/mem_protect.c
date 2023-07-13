b93bbe36081d6cb137e42c8241c1eab8fe20854a
Will Deacon <willdeacon@google.com>
2023-07-13 13:31:13 +0100

    Revert "ANDROID: KVM: arm64: Introduce concept of pKVM moveable regions"
    This reverts commit 51a84221b108408dfd76e08652698ccf371e60b9.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 278750296
    Change-Id: Ib7870003c17ec184cb475eca99c22b6a347620aa

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -25,9 +25,6 @@
 
 struct host_mmu host_mmu;
 
-struct pkvm_moveable_reg pkvm_moveable_regs[PKVM_NR_MOVEABLE_REGS];
-unsigned int pkvm_moveable_regs_nr;
-
 static struct hyp_pool host_s2_pool;
 
 static DEFINE_PER_CPU(struct pkvm_hyp_vm *, __current_vm);
@@ -435,7 +432,7 @@ int __pkvm_prot_finalize(void)
 	return 0;
 }
 
-int host_stage2_unmap_reg_locked(phys_addr_t start, u64 size)
+int host_stage2_unmap_dev_locked(phys_addr_t start, u64 size)
 {
 	int ret;
 
@@ -449,24 +446,21 @@ int host_stage2_unmap_reg_locked(phys_addr_t start, u64 size)
 	return 0;
 }
 
-static int host_stage2_unmap_unmoveable_regs(void)
+static int host_stage2_unmap_dev_all(void)
 {
 	struct kvm_pgtable *pgt = &host_mmu.pgt;
-	struct pkvm_moveable_reg *reg;
+	struct memblock_region *reg;
 	u64 addr = 0;
 	int i, ret;
 
-	/* Unmap all unmoveable regions to recycle the pages */
-	for (i = 0; i < pkvm_moveable_regs_nr; i++) {
-		reg = &pkvm_moveable_regs[i];
-		if (reg->start > addr) {
-			ret = host_stage2_unmap_reg_locked(addr, reg->start - addr);
-			if (ret)
-				return ret;
-		}
-		addr = max(addr, reg->start + reg->size);
+	/* Unmap all non-memory regions to recycle the pages */
+	for (i = 0; i < hyp_memblock_nr; i++, addr = reg->base + reg->size) {
+		reg = &hyp_memory[i];
+		ret = host_stage2_unmap_dev_locked(addr, reg->base - addr);
+		if (ret)
+			return ret;
 	}
-	return host_stage2_unmap_reg_locked(addr, BIT(pgt->ia_bits) - addr);
+	return host_stage2_unmap_dev_locked(addr, BIT(pgt->ia_bits) - addr);
 }
 
 struct kvm_mem_range {
@@ -558,10 +552,10 @@ static inline int __host_stage2_idmap(u64 start, u64 end,
 }
 
 /*
- * The pool has been provided with enough pages to cover all of moveable regions
- * with page granularity, but it is difficult to know how much of the
- * non-moveable regions we will need to cover upfront, so we may need to
- * 'recycle' the pages if we run out.
+ * The pool has been provided with enough pages to cover all of memory with
+ * page granularity, but it is difficult to know how much of the MMIO range
+ * we will need to cover upfront, so we may need to 'recycle' the pages if we
+ * run out.
  */
 #define host_stage2_try(fn, ...)					\
 	({								\
@@ -569,7 +563,7 @@ static inline int __host_stage2_idmap(u64 start, u64 end,
 		hyp_assert_lock_held(&host_mmu.lock);			\
 		__ret = fn(__VA_ARGS__);				\
 		if (__ret == -ENOMEM) {					\
-			__ret = host_stage2_unmap_unmoveable_regs();		\
+			__ret = host_stage2_unmap_dev_all();		\
 			if (!__ret)					\
 				__ret = fn(__VA_ARGS__);		\
 		}							\
