4760d4fcc58b293a4a6f33646f450d9692e7f646
Will Deacon <willdeacon@google.com>
2023-07-13 13:43:18 +0100

    Revert "ANDROID: KVM: arm64: Introduce predicates to check for protected state"
    This reverts commit 9c16ae1dd849f70390655d7d5e5419769a37cda8.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 278750296
    Change-Id: I789082f1c729b4adf830553707acaa890d260b67

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -183,7 +183,6 @@ struct kvm_protected_vm {
 	pkvm_handle_t handle;
 	struct kvm_hyp_memcache teardown_mc;
 	struct list_head pinned_pages;
-	bool enabled;
 };
 
 struct kvm_arch {
@@ -1033,7 +1032,10 @@ int kvm_set_ipa_limit(void);
 #define __KVM_HAVE_ARCH_VM_ALLOC
 struct kvm *kvm_arch_alloc_vm(void);
 
-#define kvm_vm_is_protected(kvm)	((kvm)->arch.pkvm.enabled)
+static inline bool kvm_vm_is_protected(struct kvm *kvm)
+{
+	return false;
+}
 
 void kvm_init_protected_traps(struct kvm_vcpu *vcpu);
 
