925d68c8d0594de389eafbd1f0d8b6db8832483c
Will Deacon <willdeacon@google.com>
2023-07-13 13:26:03 +0100

    Revert "ANDROID: ring-buffer: Introducing external writer support"
    This reverts commit 5ecbcb61e172d82faf1bd8c00b17837d3c658c59.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 278750296
    Change-Id: I3aeccd45bec935df9e95b68ae5c2dab11ae77405

--- include/linux/ring_buffer.h
+++ include/linux/ring_buffer.h
@@ -4,7 +4,6 @@
 
 #include <linux/mm.h>
 #include <linux/poll.h>
-#include <linux/ring_buffer_ext.h>
 #include <linux/seq_file.h>
 
 #include <asm/local.h>
@@ -141,14 +140,6 @@ __ring_buffer_alloc(unsigned long size, unsigned flags, struct lock_class_key *k
 	__ring_buffer_alloc((size), (flags), &__key);	\
 })
 
-struct ring_buffer_ext_cb {
-	int (*update_footers)(int cpu);
-	int (*swap_reader)(int cpu);
-};
-
-struct trace_buffer *
-ring_buffer_alloc_ext(unsigned long size, struct ring_buffer_ext_cb *cb);
-
 int ring_buffer_wait(struct trace_buffer *buffer, int cpu, int full);
 __poll_t ring_buffer_poll_wait(struct trace_buffer *buffer, int cpu,
 			  struct file *filp, poll_table *poll_table, int full);
@@ -262,9 +253,5 @@ int trace_rb_cpu_prepare(unsigned int cpu, struct hlist_node *node);
 #else
 #define trace_rb_cpu_prepare	NULL
 #endif
-
-size_t trace_buffer_pack_size(struct trace_buffer *trace_buffer);
-int trace_buffer_pack(struct trace_buffer *trace_buffer, struct trace_buffer_pack *pack);
-int ring_buffer_poke(struct trace_buffer *buffer, int cpu);
 
 #endif /* _LINUX_RING_BUFFER_H */
