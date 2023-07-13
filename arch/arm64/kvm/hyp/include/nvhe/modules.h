d12c5fffcedcc3f87851c0efeed31f6c4f2cfdd6
Will Deacon <willdeacon@google.com>
2023-07-13 13:25:50 +0100

    Revert "ANDROID: KVM: arm64: Cleanup useless declarations"
    This reverts commit bf019165fec551395cb4391fe4df0a783bc258ec.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 278750296
    Change-Id: I42947db260dee9532834aa3484f0eec9b58e1cb7

--- arch/arm64/kvm/hyp/include/nvhe/modules.h
+++ arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -17,6 +17,9 @@ int reset_pkvm_priv_hcall_limit(void);
 int __pkvm_init_module(void *module_init);
 int __pkvm_register_hcall(unsigned long hfn_hyp_va);
 int handle_host_dynamic_hcall(struct kvm_cpu_context *host_ctxt);
+void pkvm_modules_lock(void);
+void pkvm_modules_unlock(void);
+bool pkvm_modules_enabled(void);
 int __pkvm_close_module_registration(void);
 #else
 static inline int __pkvm_init_module(void *module_init) { return -EOPNOTSUPP; }
@@ -26,5 +29,8 @@ static inline int handle_host_dynamic_hcall(struct kvm_cpu_context *host_ctxt)
 {
 	return HCALL_UNHANDLED;
 }
+static inline void pkvm_modules_lock(void) { }
+static inline void pkvm_modules_unlock(void) { }
+static inline bool pkvm_modules_enabled(void) { return false; }
 static inline int __pkvm_close_module_registration(void) { return -EOPNOTSUPP; }
 #endif
