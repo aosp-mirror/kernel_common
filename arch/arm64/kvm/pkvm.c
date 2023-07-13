024099450fa93e602869b50b90cc91b2ed7f16dd
Will Deacon <willdeacon@google.com>
2023-07-13 13:25:43 +0100

    Revert "ANDROID: KVM: arm64: Add a custom module path for pKVM module loading"
    This reverts commit c8e53ed372705663ee2752c71cf015bb2d17d739.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 278750296
    Change-Id: I87726c8c558abf9bbe85eee8d4c688fe924e0ff6

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -606,6 +606,7 @@ early_param("kvm-arm.protected_modules", early_pkvm_modules_cfg);
 
 static void free_modprobe_argv(struct subprocess_info *info)
 {
+	kfree(info->argv[3]);
 	kfree(info->argv);
 }
 
@@ -615,7 +616,7 @@ static void free_modprobe_argv(struct subprocess_info *info)
  * security is enforced by making sure this can be called only when pKVM is
  * enabled, not yet completely initialized.
  */
-static int __init pkvm_request_early_module(char *module_name, char *module_path)
+static int __init pkvm_request_early_module(char *module_name)
 {
 	char *modprobe_path = CONFIG_MODPROBE_PATH;
 	struct subprocess_info *info;
@@ -626,7 +627,6 @@ static int __init pkvm_request_early_module(char *module_name, char *module_path
 		NULL
 	};
 	char **argv;
-	int idx = 0;
 
 	if (!is_protected_kvm_enabled())
 		return -EACCES;
@@ -634,30 +634,33 @@ static int __init pkvm_request_early_module(char *module_name, char *module_path
 	if (static_branch_likely(&kvm_protected_mode_initialized))
 		return -EACCES;
 
-	argv = kmalloc(sizeof(char *) * 7, GFP_KERNEL);
+	argv = kmalloc(sizeof(char *[5]), GFP_KERNEL);
 	if (!argv)
 		return -ENOMEM;
 
-	argv[idx++] = modprobe_path;
-	argv[idx++] = "-q";
-	if (*module_path != '\0') {
-		argv[idx++] = "-d";
-		argv[idx++] = module_path;
-	}
-	argv[idx++] = "--";
-	argv[idx++] = module_name;
-	argv[idx++] = NULL;
+	module_name = kstrdup(module_name, GFP_KERNEL);
+	if (!module_name)
+		goto free_argv;
+
+	argv[0] = modprobe_path;
+	argv[1] = "-q";
+	argv[2] = "--";
+	argv[3] = module_name;
+	argv[4] = NULL;
 
 	info = call_usermodehelper_setup(modprobe_path, argv, envp, GFP_KERNEL,
 					 NULL, free_modprobe_argv, NULL);
 	if (!info)
-		goto err;
+		goto free_module_name;
 
 	/* Even with CONFIG_STATIC_USERMODEHELPER we really want this path */
 	info->path = modprobe_path;
 
 	return call_usermodehelper_exec(info, UMH_WAIT_PROC | UMH_KILLABLE);
-err:
+
+free_module_name:
+	kfree(module_name);
+free_argv:
 	kfree(argv);
 
 	return -ENOMEM;
@@ -666,7 +669,6 @@ err:
 int __init pkvm_load_early_modules(void)
 {
 	char *token, *buf = early_pkvm_modules;
-	char *module_path = CONFIG_PKVM_MODULE_PATH;
 	int err;
 
 	while (true) {
@@ -676,7 +678,7 @@ int __init pkvm_load_early_modules(void)
 			break;
 
 		if (*token) {
-			err = pkvm_request_early_module(token, module_path);
+			err = pkvm_request_early_module(token);
 			if (err) {
 				pr_err("Failed to load pkvm module %s: %d\n",
 				       token, err);
