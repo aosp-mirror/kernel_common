e422c71a04ea6e854ba5d73448134c7e73c45550
Will Deacon <willdeacon@google.com>
2023-07-13 13:31:22 +0100

    Revert "ANDROID: KVM: arm64: Introduce PKVM_PAGE_RESTRICTED_PROT"
    This reverts commit c0faf0f3e756aee933e77f8e7e21895c2ae9c0f1.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 278750296
    Change-Id: I83613c2ea2691f2cffe48d56d0bf21749d6b16d9

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -824,17 +824,10 @@ static int check_page_state_range(struct kvm_pgtable *pgt, u64 addr, u64 size,
 
 static enum pkvm_page_state host_get_page_state(kvm_pte_t pte)
 {
-	enum pkvm_page_state state = 0;
-	enum kvm_pgtable_prot prot;
-
 	if (!kvm_pte_valid(pte) && pte)
 		return PKVM_NOPAGE;
 
-	prot = kvm_pgtable_stage2_pte_prot(pte);
-	if (kvm_pte_valid(pte) && ((prot & KVM_PGTABLE_PROT_RWX) != PKVM_HOST_MEM_PROT))
-		state = PKVM_PAGE_RESTRICTED_PROT;
-
-	return state | pkvm_getstate(prot);
+	return pkvm_getstate(kvm_pgtable_stage2_pte_prot(pte));
 }
 
 static int __host_check_page_state_range(u64 addr, u64 size,
@@ -980,17 +973,10 @@ static int host_complete_donation(u64 addr, const struct pkvm_mem_transition *tx
 
 static enum pkvm_page_state hyp_get_page_state(kvm_pte_t pte)
 {
-	enum pkvm_page_state state = 0;
-	enum kvm_pgtable_prot prot;
-
 	if (!kvm_pte_valid(pte))
 		return PKVM_NOPAGE;
 
-	prot = kvm_pgtable_hyp_pte_prot(pte);
-	if (kvm_pte_valid(pte) && ((prot & KVM_PGTABLE_PROT_RWX) != PAGE_HYP))
-		state = PKVM_PAGE_RESTRICTED_PROT;
-
-	return state | pkvm_getstate(prot);
+	return pkvm_getstate(kvm_pgtable_hyp_pte_prot(pte));
 }
 
 static int __hyp_check_page_state_range(u64 addr, u64 size,
@@ -1099,17 +1085,10 @@ static int hyp_complete_donation(u64 addr,
 
 static enum pkvm_page_state guest_get_page_state(kvm_pte_t pte)
 {
-	enum pkvm_page_state state = 0;
-	enum kvm_pgtable_prot prot;
-
 	if (!kvm_pte_valid(pte))
 		return PKVM_NOPAGE;
 
-	prot = kvm_pgtable_stage2_pte_prot(pte);
-	if (kvm_pte_valid(pte) && ((prot & KVM_PGTABLE_PROT_RWX) != KVM_PGTABLE_PROT_RWX))
-		state = PKVM_PAGE_RESTRICTED_PROT;
-
-	return state | pkvm_getstate(prot);
+	return pkvm_getstate(kvm_pgtable_stage2_pte_prot(pte));
 }
 
 static int __guest_check_page_state_range(struct pkvm_hyp_vcpu *vcpu, u64 addr,
