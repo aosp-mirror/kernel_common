d96fc5f2917254fccbf8a7edc64d1808d44e14e2
Greg Kroah-Hartman <gregkh@google.com>
2023-08-04 11:35:54 +0000

    Revert "bpf: Remove extra lock_sock for TCP_ZEROCOPY_RECEIVE"
    This reverts commit 08f61a34913558e06576e7b6318bf583f273c1be which is
    commit 9cacf81f8161111db25f98e78a7a0e32ae142b3f upstream.
    
    It breaks the Android abi.  If it is required in the future, it can come
    back in an abi-safe way.
    
    Bug: 161946584
    Change-Id: I83c4a094c650eee44a2f7160aab6ccc6e442e726
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1216,8 +1216,6 @@ struct proto {
 
 	int			(*backlog_rcv) (struct sock *sk,
 						struct sk_buff *skb);
-	bool			(*bpf_bypass_getsockopt)(int level,
-							 int optname);
 
 	void		(*release_cb)(struct sock *sk);
 
