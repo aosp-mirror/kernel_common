8ce6b72bfbb686359b2cd1a753a1597ed60d7a38
Mostafa Saleh <smostafa@google.com>
2023-08-16 15:02:44 +0000

    ANDROID: KVM: arm64: Add functions needed by EL2 modules in module ops
    EL2 IOMMU moduels need extra functions from module ops, to map memory,
    interact with the IOMMU layer and use the hyp allocator.
    
    Also, export ___pkvm_host_donate_hyp, instead of a function that always
    accept MMIO, as modules would use this function for both memory and
    MMIO.
    
    Bug: 277989609
    Bug: 278749606
    Change-Id: If3b807ef2181a0e9465599fdf260ba2904000f45
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/include/asm/kvm_pkvm_module.h
+++ arch/arm64/include/asm/kvm_pkvm_module.h
@@ -8,6 +8,7 @@
 #include <linux/export.h>
 
 typedef void (*dyn_hcall_t)(struct user_pt_regs *);
+struct kvm_hyp_iommu;
 
 #ifdef CONFIG_MODULES
 enum pkvm_psci_notification {
@@ -102,6 +103,8 @@ enum pkvm_psci_notification {
  *				is called before remasking SErrors.
  * @host_donate_hyp:		The page @pfn is unmapped from the host and
  *				full control is given to the hypervisor.
+ * @host_donate_hyp_prot:	As host_donate_hyp_prot, but this variant sets
+ *				the prot of the hyp.
  * @hyp_donate_host:		The page @pfn whom control has previously been
  *				given to the hypervisor (@host_donate_hyp) is
  *				given back to the host.
@@ -124,6 +127,18 @@ enum pkvm_psci_notification {
  * @hyp_va:			Convert a physical address into a virtual one.
  * @kern_hyp_va:		Convert a kernel virtual address into an
  *				hypervisor virtual one.
+ * @hyp_alloc:			Allocate memory in hyp VA space.
+ * @hyp_alloc_errno:		Error in case hyp_alloc() returns NULL.
+ * @hyp_free:			Free memory allocated  from hyp_alloc().
+ * @iommu_donate_pages:		Allocate memory from IOMMU pool.
+ * @iommu_reclaim_pages:	Reclaim memory from iommu_donate_pages()
+ * @iommu_request:		Fill a request that is returned from the entry HVC (see hyp-main.c).
+ * @iommu_init_device:		Initialize common IOMMU fields.
+ * @udelay:			Delay in us.
+ * @hyp_alloc_missing_donations:
+				Missing donations if allocator returns NULL
+ * @__list_add_valid:		Needed if the code uses linked lists.
+ * @__list_del_entry_valid:	Needed if the code uses linked lists.
  */
 struct pkvm_module_ops {
 	int (*create_private_mapping)(phys_addr_t phys, size_t size,
@@ -151,7 +166,8 @@ struct pkvm_module_ops {
 	int (*register_psci_notifier)(void (*cb)(enum pkvm_psci_notification, struct user_pt_regs *));
 	int (*register_hyp_panic_notifier)(void (*cb)(struct user_pt_regs *));
 	int (*register_unmask_serror)(bool (*unmask)(void), void (*mask)(void));
-	int (*host_donate_hyp)(u64 pfn, u64 nr_pages);
+	int (*host_donate_hyp)(u64 pfn, u64 nr_pages, bool accept_mmio);
+	int (*host_donate_hyp_prot)(u64 pfn, u64 nr_pages, bool accept_mmio, enum kvm_pgtable_prot prot);
 	int (*hyp_donate_host)(u64 pfn, u64 nr_pages);
 	int (*host_share_hyp)(u64 pfn);
 	int (*host_unshare_hyp)(u64 pfn);
@@ -162,6 +178,18 @@ struct pkvm_module_ops {
 	phys_addr_t (*hyp_pa)(void *x);
 	void* (*hyp_va)(phys_addr_t phys);
 	unsigned long (*kern_hyp_va)(unsigned long x);
+	void * (*hyp_alloc)(size_t size);
+	int (*hyp_alloc_errno)(void);
+	void (*hyp_free)(void *addr);
+	void * (*iommu_donate_pages)(u8 order, bool request);
+	void (*iommu_reclaim_pages)(void *p, u8 order);
+	int (*iommu_request)(struct kvm_hyp_req *req);
+	int (*iommu_init_device)(struct kvm_hyp_iommu *iommu);
+	void (*udelay)(unsigned long usecs);
+	u8 (*hyp_alloc_missing_donations)(void);
+	bool (*list_add_valid)(struct list_head *new, struct list_head *prev,
+				 struct list_head *next);
+	bool (*list_del_entry_valid)(struct list_head *entry);
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
