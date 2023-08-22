0f170559c4dbba505b5c3b6a6366a9a75bed569e
Joel Fernandes <joelaf@google.com>
2023-08-22 17:36:33 +0000

    Revert "UPSTREAM: kvfree_rcu: Fix comments according to current code"
    This reverts commit 50de8a2018f9477d9c7c32b9979343fe41ce9360.
    
    BUG=b:222463781
    Change-Id: Ibcd7720adfb2d9b459bc24a2c2f1824bf9011b07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4802960
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Commit-Queue: Joel Fernandes <joelaf@google.com>
    Tested-by: Joel Fernandes <joelaf@google.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -3292,11 +3292,9 @@ static void kfree_rcu_work(struct work_struct *work)
 	}
 
 	/*
-	 * This is used when the "bulk" path can not be used for the
-	 * double-argument of kvfree_rcu().  This happens when the
-	 * page-cache is empty, which means that objects are instead
-	 * queued on a linked list through their rcu_head structures.
-	 * This list is named "Channel 3".
+	 * Emergency case only. It can happen under low memory
+	 * condition when an allocation gets failed, so the "bulk"
+	 * path can not be temporary maintained.
 	 */
 	for (; head; head = next) {
 		unsigned long offset = (unsigned long)head->func;
@@ -3342,8 +3340,8 @@ static inline bool queue_kfree_rcu_work(struct kfree_rcu_cpu *krcp)
 		if ((krcp->bkvhead[0] && !krwp->bkvhead_free[0]) ||
 			(krcp->bkvhead[1] && !krwp->bkvhead_free[1]) ||
 				(krcp->head && !krwp->head_free)) {
-			// Channel 1 corresponds to the SLAB-pointer bulk path.
-			// Channel 2 corresponds to vmalloc-pointer bulk path.
+			// Channel 1 corresponds to SLAB ptrs.
+			// Channel 2 corresponds to vmalloc ptrs.
 			for (j = 0; j < FREE_N_CHANNELS; j++) {
 				if (!krwp->bkvhead_free[j]) {
 					krwp->bkvhead_free[j] = krcp->bkvhead[j];
@@ -3351,8 +3349,7 @@ static inline bool queue_kfree_rcu_work(struct kfree_rcu_cpu *krcp)
 				}
 			}
 
-			// Channel 3 corresponds to both SLAB and vmalloc
-			// objects queued on the linked list.
+			// Channel 3 corresponds to emergency path.
 			if (!krwp->head_free) {
 				krwp->head_free = krcp->head;
 				krcp->head = NULL;
