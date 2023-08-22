451ed3aa0118dc4265e7f1b9d21eb719209817c7
Joel Fernandes <joelaf@google.com>
2023-08-22 17:35:37 +0000

    Revert "CHROMIUM: rcu: Disable lazy if call_rcu() called when GPs expedited"
    This reverts commit 748e589e4c924c83ad8bd159d1b42a0df4111e33.
    
    BUG=b:222463781
    Change-Id: Ie51cad3eb1b20953fe1ddcc224cd23fcacdcdcab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4802944
    Commit-Queue: Joel Fernandes <joelaf@google.com>
    Tested-by: Joel Fernandes <joelaf@google.com>
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -2959,11 +2959,11 @@ static void check_cb_ovld(struct rcu_data *rdp)
 }
 
 static void
-__call_rcu_common(struct rcu_head *head, rcu_callback_t func, bool lazy_in)
+__call_rcu_common(struct rcu_head *head, rcu_callback_t func, bool lazy)
 {
 	unsigned long flags;
 	struct rcu_data *rdp;
-	bool was_alldone, lazy;
+	bool was_alldone;
 
 	/* Misaligned rcu_head! */
 	WARN_ON_ONCE((unsigned long)head & (sizeof(void *) - 1));
@@ -2984,7 +2984,6 @@ __call_rcu_common(struct rcu_head *head, rcu_callback_t func, bool lazy_in)
 	local_irq_save(flags);
 	kasan_record_aux_stack(head);
 	rdp = this_cpu_ptr(&rcu_data);
-	lazy = lazy_in && !rcu_gp_is_expedited();
 
 	/* Add the callback to our list. */
 	if (unlikely(!rcu_segcblist_is_enabled(&rdp->cblist))) {
