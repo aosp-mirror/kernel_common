a2510f3d063769c7080f87ed0c2135c69c45c869
Joel Fernandes <joelaf@google.com>
2023-08-22 17:36:39 +0000

    Revert "UPSTREAM: kvfree_rcu: Use [READ/WRITE]_ONCE() macros to access to nr_bkv_objs"
    This reverts commit dedab6eb5f77b6ae20e18a23019daf1c50c5d82d.
    
    BUG=b:222463781
    Change-Id: I2883eaa2422c07cdfde7677f56dcccb6a82ba7b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4804264
    Commit-Queue: Joel Fernandes <joelaf@google.com>
    Tested-by: Joel Fernandes <joelaf@google.com>
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -3187,7 +3187,7 @@ get_cached_bnode(struct kfree_rcu_cpu *krcp)
 	if (!krcp->nr_bkv_objs)
 		return NULL;
 
-	WRITE_ONCE(krcp->nr_bkv_objs, krcp->nr_bkv_objs - 1);
+	krcp->nr_bkv_objs--;
 	return (struct kvfree_rcu_bulk_data *)
 		llist_del_first(&krcp->bkvcache);
 }
@@ -3201,8 +3201,9 @@ put_cached_bnode(struct kfree_rcu_cpu *krcp,
 		return false;
 
 	llist_add((struct llist_node *) bnode, &krcp->bkvcache);
-	WRITE_ONCE(krcp->nr_bkv_objs, krcp->nr_bkv_objs + 1);
+	krcp->nr_bkv_objs++;
 	return true;
+
 }
 
 static int
@@ -3214,7 +3215,7 @@ drain_page_cache(struct kfree_rcu_cpu *krcp)
 
 	raw_spin_lock_irqsave(&krcp->lock, flags);
 	page_list = llist_del_all(&krcp->bkvcache);
-	WRITE_ONCE(krcp->nr_bkv_objs, 0);
+	krcp->nr_bkv_objs = 0;
 	raw_spin_unlock_irqrestore(&krcp->lock, flags);
 
 	llist_for_each_safe(pos, n, page_list) {
@@ -3596,13 +3597,18 @@ kfree_rcu_shrink_count(struct shrinker *shrink, struct shrink_control *sc)
 {
 	int cpu;
 	unsigned long count = 0;
+	unsigned long flags;
 
 	/* Snapshot count of all CPUs */
 	for_each_possible_cpu(cpu) {
 		struct kfree_rcu_cpu *krcp = per_cpu_ptr(&krc, cpu);
 
 		count += READ_ONCE(krcp->count);
-		count += READ_ONCE(krcp->nr_bkv_objs);
+
+		raw_spin_lock_irqsave(&krcp->lock, flags);
+		count += krcp->nr_bkv_objs;
+		raw_spin_unlock_irqrestore(&krcp->lock, flags);
+
 		atomic_set(&krcp->backoff_page_cache_fill, 1);
 	}
 
