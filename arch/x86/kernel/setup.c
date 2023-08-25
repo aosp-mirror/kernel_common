4bc9f041a2e46b70019a58e3d1481cf437f1bd2c
Saurabh Sengar <ssengar@linux.microsoft.com>
2023-08-25 00:47:36 -0700

    UPSTREAM: x86/of: Move the x86_flattree_get_config() call out of x86_dtb_init()
    Fetching the device tree configuration before initmem_init() is necessary
    to allow the parsing of NUMA node information. However moving the entire
    x86_dtb_init() call before initmem_init() is not correct as APIC/IO-APIC enumeration
    has to be after initmem_init().
    
    Thus, move the x86_flattree_get_config() call out of x86_dtb_init(),
    into setup_arch(), to call it before initmem_init(), and
    leave the ACPI/IOAPIC registration sequence as-is.
    
    [ mingo: Updated the changelog for clarity. ]
    
    Signed-off-by: Saurabh Sengar <ssengar@linux.microsoft.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Link: https://lore.kernel.org/r/1692949657-16446-1-git-send-email-ssengar@linux.microsoft.com
    (cherry picked from commit 0d294c8c4efa5c0f283a6dfc82dc014a5dbd9308)
    
    BUG=b:349369441
    TEST='cat /sys/devices/system/cpu/smt/control' on nautilus yields
         'notsupported'
    
    Signed-off-by: Paweł Anikiel <panikiel@google.com>
    Signed-off-by: Hubert Mazur <hmazur@google.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1218,6 +1218,8 @@ void __init setup_arch(char **cmdline_p)
 	early_acpi_boot_init();
 	x86_init.mpparse.early_parse_smp_cfg();
 
+	x86_flattree_get_config();
+
 	initmem_init();
 	dma_contiguous_reserve(max_pfn_mapped << PAGE_SHIFT);
 
