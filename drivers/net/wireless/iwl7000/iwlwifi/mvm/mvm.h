76715e6474d25b98fecb716fc79fb66ec88e2e87
Benjamin Berg <benjamin.berg@intel.com>
2023-09-21 16:31:47 +0300

    CHROMIUM: iwl7000: mvm: Limit RX antenna using correct mask
    The function checked whether set_rx_ant is set and should be used to
    limit the RX antenna setting. However, it then continued to mask it
    using the set TX antenna value. If the RX/TX antenna mask differs, this
    could result in a prohibited TX antenna to be returned for RX purposes.
    
    Change-Id: Idb0040e01438b8775187d1c87c83964494d5a64c
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: d86d8e70286a3773ec56244308206779017dd326

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/mvm.h
@@ -1784,7 +1784,7 @@ static inline u8 iwl_mvm_get_valid_rx_ant(struct iwl_mvm *mvm)
 		rx_ant &= mvm->nvm_data->valid_tx_ant;
 
 	if (mvm->set_rx_ant)
-		rx_ant &= mvm->set_tx_ant;
+		rx_ant &= mvm->set_rx_ant;
 
 	return rx_ant;
 
