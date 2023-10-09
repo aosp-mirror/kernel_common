b9929a8c1ba7f3e73594c686dcedbb8961523bdb
Dmitry Antipov <dmantipov@yandex.ru>
2023-10-09 20:04:49 +0300

    CHROMIUM: iwl7000: check for kmemdup() return value in iwl_parse_tlv_firmware()
    In 'iwl_parse_tlv_firmware()', check for 'kmemdup()' return value
    when handling IWL_UCODE_TLV_CURRENT_PC and set the number of parsed
    entries only if an allocation was successful (just like it does with
    handling IWL_UCODE_TLV_CMD_VERSIONS above). Compile tested only.
    
    Signed-off-by: Dmitry Antipov <dmantipov@yandex.ru>
    Link: https://lore.kernel.org/r/20231009170453.149905-1-dmantipov@yandex.ru
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>
    Change-Id: Ib1486591349848d54f3a83171e591cc4d1e51e25
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 929a73c159fb04b6d9306d2b191152187cbf275f

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-drv.c
@@ -1593,9 +1593,13 @@ fw_dbg_conf:
 		case IWL_UCODE_TLV_CURRENT_PC:
 			if (tlv_len < sizeof(struct iwl_pc_data))
 				goto invalid_tlv_len;
-			drv->trans->dbg.num_pc = tlv_len / sizeof(struct iwl_pc_data);
-			drv->trans->dbg.pc_data = kmemdup(tlv_data, tlv_len, GFP_KERNEL);
-		break;
+			drv->trans->dbg.pc_data =
+				kmemdup(tlv_data, tlv_len, GFP_KERNEL);
+			if (!drv->trans->dbg.pc_data)
+				return -ENOMEM;
+			drv->trans->dbg.num_pc =
+				tlv_len / sizeof(struct iwl_pc_data);
+			break;
 		default:
 			IWL_DEBUG_INFO(drv, "unknown TLV: %d\n", tlv_type);
 			break;
