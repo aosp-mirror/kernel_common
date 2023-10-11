de164c2185376a63f49d6b4c0e9be7b159dce9a3
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2023-10-11 13:58:24 +0300

    UPSTREAM: usb: typec: Link enumerated USB devices with Type-C partner
    Adding functions that USB hub code can use to inform the
    Type-C class about connected USB devices.
    
    Once taken into use, it will allow the Type-C port drivers
    to power off components that are not needed, for example if
    USB2 device is enumerated, everything that is only relevant
    for USB3 (retimers, etc.), can be powered off.
    
    This will also create a symlink "typec" for the USB devices
    pointing to the USB Type-C partner device.
    
    Suggested-by: Benson Leung <bleung@chromium.org>
    Tested-by: Benson Leung <bleung@chromium.org>
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Link: https://lore.kernel.org/r/20231011105825.320062-2-heikki.krogerus@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 59de2a56d127890cc610f3896d5fc31887c54ac2)
    
    BUG=b:297014254
    TEST=manually confirmed on v6.6 kernel including those changes, that links in port-partner directory are created when device is attached
    
    Change-Id: I42130355e815df39af5c4d7a439770b0c0149ff2
    Signed-off-by: Bartosz Szpila <bszpila@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5455154
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Jameson Thies <jthies@google.com>
    Kcr-patch: 888d5a5c84185fb5242eae31247de2b71aca75d6431f264b738c6ad3.patch

--- drivers/usb/typec/class.h
+++ drivers/usb/typec/class.h
@@ -9,6 +9,7 @@
 
 struct typec_mux;
 struct typec_switch;
+struct usb_device;
 
 struct typec_plug {
 	struct device			dev;
@@ -38,6 +39,9 @@ struct typec_partner {
 	enum usb_pd_svdm_ver		svdm_version;
 
 	struct usb_power_delivery	*pd;
+
+	void (*attach)(struct typec_partner *partner, struct device *dev);
+	void (*deattach)(struct typec_partner *partner, struct device *dev);
 	ANDROID_KABI_RESERVE(1);
 };
 
@@ -63,6 +67,18 @@ struct typec_port {
 
 	const struct typec_capability	*cap;
 	const struct typec_operations   *ops;
+
+	struct typec_connector		con;
+
+	/*
+	 * REVISIT: Only USB devices for now. If there are others, these need to
+	 * be converted into a list.
+	 *
+	 * NOTE: These may be registered first before the typec_partner, so they
+	 * will always have to be kept here instead of struct typec_partner.
+	 */
+	struct device			*usb2_dev;
+	struct device			*usb3_dev;
 	ANDROID_KABI_RESERVE(1);
 };
 
