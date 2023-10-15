c13400a797930d6fa70d04d5b395dac62438493f
Vineeth Pillai (Google) <vineeth@bitbyteword.org>
2023-10-15 10:21:05 -0400

    CHROMIUM: kvm: x86: debugfs: vcpu boost related information
    Add pvsched specific debugfs files that shows pvsched related info and
    also helps in enable/disable per-vm pvsched and set/get default policy
    and priority.
    
    UPSTREAM-TASK=b:303645537
    BUG=b:262267726
    TEST=boot
    
    Co-developed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Vineeth Pillai <vineeth@bitbyteword.org>
    (cherry picked from commit 57f6922a574b0face43d9c99e049003ee2c46492)
    
    Change-Id: Id6a335b33558f6917a473dde1e98b3f8a782eb1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5426731
    Reviewed-by: Joel Fernandes <joelaf@google.com>
    Tested-by: Vineeth Pillai <vineethrp@google.com>
    Commit-Queue: Vineeth Pillai <vineethrp@google.com>
    Kcr-patch: 87b0ffc9bee610901f5d032c642bc82d21aff23e2bc0a6155fda890b.patch
    Signed-off-by: Hubert Mazur <hmazur@google.com>

--- arch/x86/kvm/debugfs.c
+++ arch/x86/kvm/debugfs.c
@@ -56,6 +56,110 @@ static int vcpu_get_tsc_scaling_frac_bits(void *data, u64 *val)
 
 DEFINE_SIMPLE_ATTRIBUTE(vcpu_tsc_scaling_frac_fops, vcpu_get_tsc_scaling_frac_bits, NULL, "%llu\n");
 
+#ifdef CONFIG_PARAVIRT_SCHED_KVM
+#include <linux/sched.h>
+
+static int vcpu_pv_sched_get_enabled(void *data, u64 *val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	*val = kvm_vcpu_sched_enabled(vcpu);
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(vcpu_pv_sched_enabled_fops, vcpu_pv_sched_get_enabled, NULL, "%llu\n");
+
+static int vcpu_get_boosted(void *data, u64 *val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	*val = kvm_arch_vcpu_is_boosted(&vcpu->arch);
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(vcpu_boosted_fops, vcpu_get_boosted, NULL, "%llu\n");
+
+static int get_kerncs_prio(void *data, u64 *val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	*val = kvm_arch_vcpu_kerncs_prio(&vcpu->arch);
+	return 0;
+}
+
+static int set_kerncs_prio(void *data, u64 val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+
+	return kvm_arch_vcpu_set_kerncs_prio(&vcpu->arch, val);
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(kerncs_prio_fops, get_kerncs_prio,
+		set_kerncs_prio, "%llu\n");
+
+static int kerncs_policy_show(struct seq_file *m, void *data)
+{
+	char *policy;
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) m->private;
+
+	if (kvm_arch_vcpu_kerncs_policy(&vcpu->arch) == SCHED_FIFO)
+		policy = "SCHED_FIFO";
+	else
+		policy = "SCHED_RR";
+	seq_printf(m, "%s\n", policy);
+
+	return 0;
+}
+
+static ssize_t
+kerncs_policy_write(struct file *filp, const char __user *ubuf,
+		size_t cnt, loff_t *ppos)
+{
+	int ret;
+	char *cmp;
+	int policy;
+	char buf[12];
+	struct inode *inode;
+	struct kvm_vcpu *vcpu;
+
+	if (cnt > 12)
+		cnt = 12;
+
+	if (copy_from_user(&buf, ubuf, cnt))
+		return -EFAULT;
+
+	cmp = strstrip(buf);
+	if (!strcmp(cmp, "SCHED_FIFO"))
+		policy = SCHED_FIFO;
+	else if (!strcmp(cmp, "SCHED_RR"))
+		policy = SCHED_RR;
+	else
+		return -EINVAL;
+
+	inode = file_inode(filp);
+	inode_lock(inode);
+	vcpu = (struct kvm_vcpu *)inode->i_private;
+	ret = kvm_arch_vcpu_set_kerncs_policy(&vcpu->arch, policy);
+	inode_unlock(inode);
+
+	if (ret)
+		return ret;
+
+	*ppos += cnt;
+	return cnt;
+}
+
+static int kerncs_policy_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, kerncs_policy_show, inode->i_private);
+}
+
+static const struct file_operations kerncs_policy_fops = {
+	.open		= kerncs_policy_open,
+	.write		= kerncs_policy_write,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+#endif
+
 void kvm_arch_create_vcpu_debugfs(struct kvm_vcpu *vcpu, struct dentry *debugfs_dentry)
 {
 	debugfs_create_file("guest_mode", 0444, debugfs_dentry, vcpu,
@@ -76,6 +180,21 @@ void kvm_arch_create_vcpu_debugfs(struct kvm_vcpu *vcpu, struct dentry *debugfs_
 				    debugfs_dentry, vcpu,
 				    &vcpu_tsc_scaling_frac_fops);
 	}
+
+#ifdef CONFIG_PARAVIRT_SCHED_KVM
+	debugfs_create_file("pv_sched_enabled", 0444,
+			    debugfs_dentry, vcpu,
+			    &vcpu_pv_sched_enabled_fops);
+	debugfs_create_file("pv_sched_boosted", 0444,
+			    debugfs_dentry, vcpu,
+			    &vcpu_boosted_fops);
+	debugfs_create_file("kerncs_policy", 0644,
+			    debugfs_dentry, vcpu,
+			    &kerncs_policy_fops);
+	debugfs_create_file("kerncs_prio", 0644,
+			    debugfs_dentry, vcpu,
+			    &kerncs_prio_fops);
+#endif
 }
 
 /*
@@ -188,9 +307,37 @@ static const struct file_operations mmu_rmaps_stat_fops = {
 	.release	= kvm_mmu_rmaps_stat_release,
 };
 
+#ifdef CONFIG_PARAVIRT_SCHED_KVM
+static int pv_sched_get_enabled(void *data, u64 *val)
+{
+	struct kvm *kvm = (struct kvm *) data;
+	*val = kvm->pv_sched_enabled;
+	return 0;
+}
+
+static int pv_sched_set_enabled(void *data, u64 val)
+{
+	struct kvm *kvm = (struct kvm *) data;
+
+	if (val != 0 && val != 1)
+		return -EINVAL;
+
+	kvm_set_pv_sched_enabled(kvm, val);
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(pv_sched_enabled_fops, pv_sched_get_enabled,
+		pv_sched_set_enabled, "%llu\n");
+#endif
+
 int kvm_arch_create_vm_debugfs(struct kvm *kvm)
 {
 	debugfs_create_file("mmu_rmaps_stat", 0644, kvm->debugfs_dentry, kvm,
 			    &mmu_rmaps_stat_fops);
+#ifdef CONFIG_PARAVIRT_SCHED_KVM
+	debugfs_create_file("pv_sched_enabled", 0644, kvm->debugfs_dentry, kvm,
+			    &pv_sched_enabled_fops);
+#endif
+
 	return 0;
 }
