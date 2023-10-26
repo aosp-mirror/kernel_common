317c6c346ab25c8964d159f8bba067794770f6c7
Greg Kroah-Hartman <gregkh@google.com>
2023-10-26 11:13:10 +0000

    Revert "spi: zynqmp-gqspi: Convert to platform remove callback returning void"
    This reverts commit e514f897ad66560215f70cd3e343e43ee6dc4ca3 which is
    commit 3ffefa1d9c9eba60c7f8b4a9ce2df3e4c7f4a88e upstream.
    
    It breaks the build due to the 'remove_new' callback not being present.
    Revert it in order to fix the build in the android13-5.15 tree.
    
    Change-Id: I22d25ef61b65ccf0f14a71f4e2a76c3f78486286
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/spi/spi-zynqmp-gqspi.c
+++ drivers/spi/spi-zynqmp-gqspi.c
@@ -1221,7 +1221,7 @@ remove_master:
  *
  * Return:	0 Always
  */
-static void zynqmp_qspi_remove(struct platform_device *pdev)
+static int zynqmp_qspi_remove(struct platform_device *pdev)
 {
 	struct zynqmp_qspi *xqspi = platform_get_drvdata(pdev);
 
@@ -1234,6 +1234,7 @@ static void zynqmp_qspi_remove(struct platform_device *pdev)
 	pm_runtime_set_suspended(&pdev->dev);
 	clk_disable_unprepare(xqspi->refclk);
 	clk_disable_unprepare(xqspi->pclk);
+	return 0;
 }
 
 static const struct of_device_id zynqmp_qspi_of_match[] = {
@@ -1245,7 +1246,7 @@ MODULE_DEVICE_TABLE(of, zynqmp_qspi_of_match);
 
 static struct platform_driver zynqmp_qspi_driver = {
 	.probe = zynqmp_qspi_probe,
-	.remove_new = zynqmp_qspi_remove,
+	.remove = zynqmp_qspi_remove,
 	.driver = {
 		.name = "zynqmp-qspi",
 		.of_match_table = zynqmp_qspi_of_match,
