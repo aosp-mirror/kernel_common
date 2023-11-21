fb806d83a0eb340b0e0475e7edd7834256ee2bbf
Rick Yiu <rickyiu@google.com>
2023-11-21 08:43:15 +0000

    ANDROID: sched: Add trace_android_rvh_set_user_nice_locked
    We will need vendor hook of set_user_nice with lock to avoid race
    condition.
    
    Bug: 300872872
    Change-Id: I24fc1e13cc6578dcc418d956a5146ad29ff76a56
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -117,6 +117,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_rtmutex_prepare_setprio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_prepare_prio_fork);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_finish_prio_fork);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_user_nice);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_user_nice_locked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_setscheduler);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_sk_alloc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_sk_free);
