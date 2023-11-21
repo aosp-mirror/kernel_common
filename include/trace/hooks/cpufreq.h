9bd7b75557ef9b8fc213e471599d20018f8c1642
vincent wang <vincentwang3@lenovo.com>
2023-11-21 17:38:28 +0800

    ANDROID: GKI: add a vendor hook in cpufreq_online
    In this vendor hook, we can register the energy model before sending the
    notification to avoid pd_init failure for some clusters.
    
    more detail info about pd_init failure,  pls refer to the link:
    https://lore.kernel.org/lkml/20230118044733.29391-1-bhuwz@163.com/T/
    
    But the change patch cannot be submitted upstream due to differences in the
    cpufreq code, the failure only appears on the aosp kernel.
    
    Bug: 312392542
    
    Change-Id: Ied4c72d73083d794ad6d8af1eb305be4fb382de3
    Signed-off-by: vincent wang <vincentwang3@lenovo.com>

--- include/trace/hooks/cpufreq.h
+++ include/trace/hooks/cpufreq.h
@@ -20,6 +20,10 @@ DECLARE_HOOK(android_vh_freq_table_limits,
 		 unsigned int max_freq),
 	TP_ARGS(policy, min_freq, max_freq));
 
+DECLARE_HOOK(android_vh_cpufreq_online,
+	TP_PROTO(struct cpufreq_policy *policy),
+	TP_ARGS(policy));
+
 #endif /* _TRACE_HOOK_CPUFREQ_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
