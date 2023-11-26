ee2dd1e1d6089e852e36d7b0f807e5293b73d7d6
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2023-11-26 15:52:40 +0200

    CHROMIUM: iwl7000: read SPLC from UEFI
    Try to read the SPLC table from UEFI first,
    and if the WIFI UEFI tables are unlocked or the
    table doesn't exist - try to read it from ACPI
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I7ea262df9431ced787b77c87149c6d7bddb7e7d6
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 67f2151b59af6e1faaf8a8fc03cca002f3e2d571

--- drivers/net/wireless/iwl7000/iwlwifi/fw/uefi.c
+++ drivers/net/wireless/iwl7000/iwlwifi/fw/uefi.c
@@ -619,4 +619,29 @@ out:
 	return ret;
 }
 IWL_EXPORT_SYMBOL(iwl_uefi_get_tas_table);
+
+int iwl_uefi_get_pwr_limit(struct iwl_fw_runtime *fwrt,
+			   u64 *dflt_pwr_limit)
+{
+	struct uefi_cnv_var_splc *data;
+	int ret = 0;
+
+	data = (struct uefi_cnv_var_splc *)
+		iwl_uefi_get_verified_variable(fwrt->trans, IWL_UEFI_SPLC_NAME,
+					       "SPLC", sizeof(*data), NULL);
+	if (IS_ERR(data))
+		return -EINVAL;
+
+	if (data->revision != IWL_UEFI_SPLC_REVISION) {
+		ret = -EINVAL;
+		IWL_DEBUG_RADIO(fwrt, "Unsupported UEFI SPLC revision:%d\n",
+				data->revision);
+		goto out;
+	}
+	*dflt_pwr_limit = data->default_pwr_limit;
+out:
+	kfree(data);
+	return ret;
+}
+IWL_EXPORT_SYMBOL(iwl_uefi_get_pwr_limit);
 #endif /* >= 5.4 */
