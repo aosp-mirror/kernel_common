801f5b8893f2bbc1af00ceb5012ea36638a1c7bd
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2023-11-28 13:51:25 +0200

    CHROMIUM: iwl7000: read ECKV table from UEFI
    Try to read the ECKV table from UEFI first,
    and if the WIFI UEFI tables are unlocked or the
    table doesn't exist - try to read it from ACPI.
    
    Change iwl_acpi_get_eckv() to receive fwrt as argument so
    it will be the same as all iwl_acpi_get_x() functions
    (so the iwl_bios_get_eckv() can be generated by the macro).
    
    While at it - move the reading of ECKV to INIT stage. There is no
    reason to read it each time we load the FW.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I36e5fc7f7850229b9b377c80b5203aa47137c97c
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: a46468362a5e1bef66fb6b50d959839c5f8ea247

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/fw.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/fw.c
@@ -1557,6 +1557,9 @@ void iwl_mvm_get_bios_tables(struct iwl_mvm *mvm)
 	}
 
 	iwl_acpi_get_phy_filters(&mvm->fwrt, &mvm->phy_filters);
+
+	if (iwl_bios_get_eckv(&mvm->fwrt, &mvm->ext_clock_valid))
+		IWL_DEBUG_RADIO(mvm, "ECKV table doesn't exist in BIOS\n");
 }
 
 static void iwl_mvm_disconnect_iterator(void *data, u8 *mac,
@@ -1917,9 +1920,6 @@ int iwl_mvm_up(struct iwl_mvm *mvm)
 						 IWL_TIME_SYNC_PROTOCOL_TM |
 						 IWL_TIME_SYNC_PROTOCOL_FTM);
 	}
-
-	if (iwl_acpi_get_eckv(mvm->dev, &mvm->ext_clock_valid))
-		IWL_DEBUG_INFO(mvm, "ECKV table doesn't exist in BIOS\n");
 
 	ret = iwl_mvm_ppag_init(mvm);
 	if (ret)
