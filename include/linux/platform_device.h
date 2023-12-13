01b6b46d8134ffa0211a08f007851ce1a3dc1957
Lee Jones <joneslee@google.com>
2023-12-13 10:40:42 +0000

    Revert "ANDROID: fix up platform_device ABI break"
    This reverts commit cd70c9caf108b50cf25452f8fb371d42960e345d.
    
    Change-Id: Ieb8cc29eb5bb81afa6f6ad043540892a51d05a5f
    Signed-off-by: Lee Jones <joneslee@google.com>

--- include/linux/platform_device.h
+++ include/linux/platform_device.h
@@ -36,7 +36,7 @@ struct platform_device {
 	 * Driver name to force a match.  Do not set directly, because core
 	 * frees it.  Use driver_set_override() to set or clear it.
 	 */
-	char *driver_override;
+	const char *driver_override;
 
 	/* MFD cell pointer */
 	struct mfd_cell *mfd_cell;
