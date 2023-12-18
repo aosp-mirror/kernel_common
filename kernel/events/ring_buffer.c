c6c759d1d88054ca631fba1624c8a2e61ed2d0bb
Greg Kroah-Hartman <gregkh@google.com>
2023-12-18 18:47:06 +0000

    Revert "perf/core: Add a new read format to get a number of lost samples"
    This reverts commit ece0857258cbaf20b9828157035999f46ca060c8 which is
    commit 119a784c81270eb88e573174ed2209225d646656 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I9f973774a99437a3dcfecf32eff8e7ddcfd418ed
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/events/ring_buffer.c
+++ kernel/events/ring_buffer.c
@@ -165,10 +165,8 @@ __perf_output_begin(struct perf_output_handle *handle,
 		goto out;
 
 	if (unlikely(rb->paused)) {
-		if (rb->nr_pages) {
+		if (rb->nr_pages)
 			local_inc(&rb->lost);
-			atomic64_inc(&event->lost_samples);
-		}
 		goto out;
 	}
 
@@ -251,7 +249,6 @@ __perf_output_begin(struct perf_output_handle *handle,
 
 fail:
 	local_inc(&rb->lost);
-	atomic64_inc(&event->lost_samples);
 	perf_output_put_handle(handle);
 out:
 	rcu_read_unlock();
