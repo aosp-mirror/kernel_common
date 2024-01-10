fb5aa97544fd07f9e0229d1fec9f01668f376e48
hao lv <hao.lv5@transsion.com>
2024-01-10 19:49:07 +0800

    ANDROID: add hooks into blk-mq.c for customized I/O scheduler.
    Our scheduler uses the ANDROID_OEM_DATA field in the request_queue
    structure to customize a set of ops operations for the request queue
    applied by the ufs/emmc driver. Adopt fine-grained IO scheduling
    strategies based on specific request types and user scenarios
    
    android_rvh_internal_blk_mq_alloc_request: Obtain a new tag.
    This process may be scheduled because the tag cannot be allocated
    quickly, so a restricted hook is required.
    
    android_vh_internal_blk_mq_free_request: Release individually allocated
    tags
    
    android_vh_blk_mq_complete_request: Record the time when the request was
    completed
    
    android_vh_blk_mq_add_to_requeue_list: Add requests to a separate
    distribution queue
    
    android_rvh_blk_mq_delay_run_hw_queue: The process of calling queue_rq
    to handle the request may be scheduled, so restricted hooks need to be
    used
    
    android_vh_blk_mq_run_hw_queue: Set need_run to true
    
    android_vh_blk_mq_insert_request: Insert the request into a customized
    queue
    
    android_rvh_blk_mq_alloc_rq_map: The process of assigning customized
    tags may be scheduled, so restricted hooks need to be used
    
    android_rvh_blk_mq_init_allocated_queue: The customized scheduler is
    initialized and needs to allocate some resources. This process may be
    scheduled, so restricted hooks need to be used
    
    android_vh_blk_mq_exit_queue: Release resources allocated by the
    customized scheduler
    
    android_vh_blk_mq_alloc_tag_set: Get tagset information
    
    Bug: 319582497
    Bug: 327716807
    
    Change-Id: I2b16d69a1e7085a4f5f82660b75188c517b01894
    Signed-off-by: hao lv <hao.lv5@transsion.com>

--- include/trace/hooks/block.h
+++ include/trace/hooks/block.h
@@ -14,15 +14,18 @@
 struct blk_mq_tags;
 struct blk_mq_alloc_data;
 struct blk_mq_tag_set;
+struct blk_mq_hw_ctx;
 #else
 /* struct blk_mq_tags */
 #include <../block/blk-mq-tag.h>
 /* struct blk_mq_alloc_data */
 #include <../block/blk-mq.h>
-/* struct blk_mq_tag_set */
+/* struct blk_mq_tag_set struct blk_mq_hw_ctx*/
 #include <linux/blk-mq.h>
 #endif /* __GENKSYMS__ */
 struct bio;
+struct request_queue;
+struct request;
 
 DECLARE_HOOK(android_vh_blk_alloc_rqs,
 	TP_PROTO(size_t *rq_size, struct blk_mq_tag_set *set,
@@ -37,6 +40,63 @@ DECLARE_HOOK(android_vh_blk_rq_ctx_init,
 DECLARE_HOOK(android_vh_bio_free,
 	TP_PROTO(struct bio *bio),
 	TP_ARGS(bio));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_internal_blk_mq_alloc_request,
+	TP_PROTO(bool *skip, int *tag, struct blk_mq_alloc_data *data),
+	TP_ARGS(skip, tag, data), 1);
+
+DECLARE_HOOK(android_vh_internal_blk_mq_free_request,
+	TP_PROTO(bool *skip, struct request *rq, struct blk_mq_hw_ctx *hctx),
+	TP_ARGS(skip, rq, hctx));
+
+DECLARE_HOOK(android_vh_blk_mq_complete_request,
+	TP_PROTO(bool *skip, struct request *rq),
+	TP_ARGS(skip, rq));
+
+DECLARE_HOOK(android_vh_blk_mq_add_to_requeue_list,
+	TP_PROTO(bool *skip, struct request *rq, bool kick_requeue_list),
+	TP_ARGS(skip, rq, kick_requeue_list));
+
+DECLARE_HOOK(android_vh_blk_mq_get_driver_tag,
+	TP_PROTO(struct request *rq),
+	TP_ARGS(rq));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_blk_mq_delay_run_hw_queue,
+	TP_PROTO(bool *skip, struct blk_mq_hw_ctx *hctx, bool async),
+	TP_ARGS(skip, hctx, async), 1);
+
+DECLARE_HOOK(android_vh_blk_mq_run_hw_queue,
+	TP_PROTO(bool *need_run, struct blk_mq_hw_ctx *hctx),
+	TP_ARGS(need_run, hctx));
+
+DECLARE_HOOK(android_vh_blk_mq_insert_request,
+	TP_PROTO(bool *skip, struct blk_mq_hw_ctx *hctx, struct request *rq),
+	TP_ARGS(skip, hctx, rq));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_blk_mq_alloc_rq_map,
+	TP_PROTO(bool *skip, struct blk_mq_tags **tags,
+		struct blk_mq_tag_set *set, int node, unsigned int flags),
+	TP_ARGS(skip, tags, set, node, flags), 1);
+
+DECLARE_HOOK(android_vh_blk_mq_hctx_notify_dead,
+	TP_PROTO(bool *skip, struct blk_mq_hw_ctx *hctx),
+	TP_ARGS(skip, hctx));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_blk_mq_init_allocated_queue,
+	TP_PROTO(struct request_queue *q),
+	TP_ARGS(q), 1);
+
+DECLARE_HOOK(android_vh_blk_mq_exit_queue,
+	TP_PROTO(struct request_queue *q),
+	TP_ARGS(q));
+
+DECLARE_HOOK(android_vh_blk_mq_alloc_tag_set,
+	TP_PROTO(struct blk_mq_tag_set *set),
+	TP_ARGS(set));
+
+DECLARE_HOOK(android_vh_blk_mq_update_nr_requests,
+	TP_PROTO(bool *skip, struct request_queue *q),
+	TP_ARGS(skip, q));
 
 #endif /* _TRACE_HOOK_BLOCK_H */
 
