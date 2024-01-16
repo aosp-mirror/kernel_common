ce0c9bdc4b69a4a9bef92ef6ca6c4c48a0960080
Johannes Berg <johannes.berg@intel.com>
2024-01-16 12:59:08 +0100

    CHROMIUM: iwl7000: mvm: show skb_mac_gso_segment() failure reason
    If this warning triggers we don't really know why, print out
    the return value so we can see it.
    
    Change-Id: Ib8ee9c90bd8f1af69969981ff0c63e9cc3123e1f
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: cb96c50e27aa32b847a06581659599c4a1a0e421

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/tx.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/tx.c
@@ -952,7 +952,8 @@ iwl_mvm_tx_tso_segment(struct sk_buff *skb, unsigned int num_subframes,
 	next = skb_gso_segment(skb, netdev_flags);
 	skb_shinfo(skb)->gso_size = mss;
 	skb_shinfo(skb)->gso_type = ipv4 ? SKB_GSO_TCPV4 : SKB_GSO_TCPV6;
-	if (WARN_ON_ONCE(IS_ERR(next)))
+	if (WARN_ONCE(IS_ERR(next),
+		      "skb_gso_segment error: %d\n", (int)PTR_ERR(next)))
 		return -EINVAL;
 	else if (next)
 		consume_skb(skb);
