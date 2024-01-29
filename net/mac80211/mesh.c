d8b9743579a2222371d566eb43a78f787775b9ae
Johannes Berg <johannes.berg@intel.com>
2024-01-29 19:34:36 +0100

    BACKPORT: wifi: mac80211: clean up connection process
    Rewrite the station-side connection handling. The connection
    flags (IEEE80211_DISABLE_*) are rather confusing, and they're
    not always maintained well. Additionally, for wider-bandwidth
    OFDMA support we need to know the precise bandwidth of the AP,
    which is currently somewhat difficult.
    
    Rewrite this to have a 'mode' (S1G/legacy/HT/...) and a limit
    on the bandwidth. This is not entirely clean because some of
    those modes aren't completely sequenced (as this assumes in
    some places), e.g. VHT doesn't exist on 2.4 GHz, but HE does.
    However, it still simplifies things and gives us a good idea
    what we're operating as, so we can parse elements accordingly
    etc.
    
    This leaves a FIXME for puncturing, this is addressed in a
    later patch.
    
    Reviewed-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://msgid.link/20240129194108.9451722c0110.I3e61f4cfe9da89008e1854160093c76a1e69dc2a@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit 310c8387c63830bc375827242e0f9fa689f82e21
     https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    
    Conflicts:
       net/mac80211/mlme.c
       net/mac80211/util.c
    
    BUG=b:366169724
    TEST=Merge patch from upstream
    
    Change-Id: I3e61f4cfe9da89008e1854160093c76a1e69dc2a
    Signed-off-by: Sean Wang <objelf@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5828182
    Commit-Queue: Jintao Lin <jintaolin@chromium.org>
    Reviewed-by: Jintao Lin <jintaolin@chromium.org>
    Tested-by: Jintao Lin <jintaolin@chromium.org>
    Signed-off-by: Mateusz Urbańczyk <urbanczyk@google.com>

--- net/mac80211/mesh.c
+++ net/mac80211/mesh.c
@@ -582,7 +582,7 @@ int mesh_add_he_cap_ie(struct ieee80211_sub_if_data *sdata,
 		return -ENOMEM;
 
 	pos = skb_put(skb, ie_len);
-	ieee80211_ie_build_he_cap(0, pos, he_cap, pos + ie_len);
+	ieee80211_ie_build_he_cap(NULL, he_cap, pos, pos + ie_len);
 
 	return 0;
 }
@@ -1294,7 +1294,7 @@ ieee80211_mesh_process_chnswitch(struct ieee80211_sub_if_data *sdata,
 	struct ieee80211_if_mesh *ifmsh = &sdata->u.mesh;
 	struct ieee80211_supported_band *sband;
 	int err;
-	ieee80211_conn_flags_t conn_flags = 0;
+	struct ieee80211_conn_settings conn = ieee80211_conn_settings_unlimited;
 	u32 vht_cap_info = 0;
 
 	sdata_assert_lock(sdata);
@@ -1305,13 +1305,16 @@ ieee80211_mesh_process_chnswitch(struct ieee80211_sub_if_data *sdata,
 
 	switch (sdata->vif.bss_conf.chandef.width) {
 	case NL80211_CHAN_WIDTH_20_NOHT:
-		conn_flags |= IEEE80211_CONN_DISABLE_HT;
-		fallthrough;
+		conn.mode = IEEE80211_CONN_MODE_LEGACY;
+		conn.bw_limit = IEEE80211_CONN_BW_LIMIT_20;
+		break;
 	case NL80211_CHAN_WIDTH_20:
-		conn_flags |= IEEE80211_CONN_DISABLE_40MHZ;
-		fallthrough;
+		conn.mode = IEEE80211_CONN_MODE_HT;
+		conn.bw_limit = IEEE80211_CONN_BW_LIMIT_20;
+		break;
 	case NL80211_CHAN_WIDTH_40:
-		conn_flags |= IEEE80211_CONN_DISABLE_VHT;
+		conn.mode = IEEE80211_CONN_MODE_HT;
+		conn.bw_limit = IEEE80211_CONN_BW_LIMIT_40;
 		break;
 	default:
 		break;
@@ -1323,8 +1326,8 @@ ieee80211_mesh_process_chnswitch(struct ieee80211_sub_if_data *sdata,
 
 	memset(&params, 0, sizeof(params));
 	err = ieee80211_parse_ch_switch_ie(sdata, elems, sband->band,
-					   vht_cap_info,
-					   conn_flags, sdata->vif.addr,
+					   vht_cap_info, &conn,
+					   sdata->vif.addr,
 					   &csa_ie);
 	if (err < 0)
 		return false;
