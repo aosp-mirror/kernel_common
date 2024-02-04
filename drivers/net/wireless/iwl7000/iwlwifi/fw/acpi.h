98e0ae4357d1ae3b800f92c6134f0232f4085edf
Johannes Berg <johannes.berg@intel.com>
2024-02-04 10:48:18 +0100

    CHROMIUM: iwl7000: fw: fix compile w/o CONFIG_ACPI
    The user of this function passes a pointer to a value that
    doesn't exist when compiled w/o CONFIG_ACPI. Since we don't
    need the value then, make the non-ACPI version a macro to
    allow it to still build.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202402031454.syX4cSGN-lkp@intel.com/
    Fixes: c4c954547755 ("wifi: iwlwifi: implement WPFC ACPI table loading")
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: Ic1b31f538de617d1fca29259ce20d948e1d9c97c
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: a3f0d0f8e5626f9a1e3c7592b8be3a7686fb98e9

--- drivers/net/wireless/iwl7000/iwlwifi/fw/acpi.h
+++ drivers/net/wireless/iwl7000/iwlwifi/fw/acpi.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
 /*
  * Copyright (C) 2017 Intel Deutschland GmbH
- * Copyright (C) 2018-2023 Intel Corporation
+ * Copyright (C) 2018-2024 Intel Corporation
  */
 #ifndef __iwl_fw_acpi__
 #define __iwl_fw_acpi__
@@ -193,10 +193,8 @@ static inline int iwl_acpi_get_ppag_table(struct iwl_fw_runtime *fwrt)
 	return -ENOENT;
 }
 
-static inline void iwl_acpi_get_phy_filters(struct iwl_fw_runtime *fwrt,
-					    struct iwl_phy_specific_cfg *filters)
-{
-}
+/* macro since the second argument doesn't always exist */
+#define iwl_acpi_get_phy_filters(fwrt, filters) do { } while (0)
 
 static inline void iwl_acpi_get_guid_lock_status(struct iwl_fw_runtime *fwrt)
 {
