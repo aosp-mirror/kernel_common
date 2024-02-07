c44d78e0a7ac64a2d68b95346d863064f1ef024f
Carlos Llamas <cmllamas@google.com>
2024-02-07 20:48:14 +0000

    Reapply "net: add sysctl accept_ra_min_rtr_lft"
    This reverts commit 53ca2e0248aa5816543f45bd43a1cd49e910b72d.
    
    Bug: 330729479
    Bug: 320243175
    Change-Id: Ia31692230f2f5cbd2ac349edcb2be320497c52f3
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -33,6 +33,7 @@ struct ipv6_devconf {
 	__s32		max_addresses;
 	__s32		accept_ra_defrtr;
 	__s32		accept_ra_min_hop_limit;
+	__s32		accept_ra_min_rtr_lft;
 	__s32		accept_ra_pinfo;
 	__s32		ignore_routes_with_linkdown;
 #ifdef CONFIG_IPV6_ROUTER_PREF
