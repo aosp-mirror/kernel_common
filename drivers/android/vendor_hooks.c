c9ca12bfd28b84e448bbbfc40c00671b3fd2176c
Suren Baghdasaryan <surenb@google.com>
2024-02-08 14:19:04 -0800

    ANDROID: introduce a vendor hook to allow speculative swap pagefaults
    Since SPF is an out-of-tree feature, the risks of changing its behavior
    are higher. Add a vendor hook to enable speculative swap pagefaults. By
    default it's disabled and should not cause troubles for current users.
    
    Bug: 322762567
    Change-Id: I3df7c545aa27d2707ee51ea42368f785c5faa735
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -514,6 +514,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_err_handler);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_err_check_ctrl);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_err_print_ctrl);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_vmscan_kswapd_done);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_swap_page_spf);
 /*
  * For type visibility
  */
