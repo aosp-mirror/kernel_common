41ff0c19a919633a322613c6c7ceded2cec75431
David E. Box <david.e.box@linux.intel.com>
2024-02-23 14:58:47 -0600

    BACKPORT: PCI/ASPM: Move pci_configure_ltr() to aspm.c
    The Latency Tolerance Reporting (LTR) mechanism supports the ASPM L1.2
    state and is only configured when CONFIG_PCIEASPM is set.
    
    Move pci_configure_ltr() and pci_bridge_reconfigure_ltr() into aspm.c since
    they only build when CONFIG_PCIEASPM is set.  No functional change
    intended.
    
    Suggested-by: Bjorn Helgaas <bhelgaas@google.com>
    Link: https://lore.kernel.org/r/20240128233212.1139663-2-david.e.box@linux.intel.com
    [bhelgaas: commit log, split build change from function moves]
    Link: https://lore.kernel.org/r/20240223205851.114931-2-helgaas@kernel.org
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    BUG: 371115070
    Change-Id: Iba157969297de12b94d11df0f8ebf8313c941ce1
    (cherry picked from commit fa84f4435a6202dd90248517f41e54bf3fb85bc5)
    [sushrut: pci_bridge_reconfigure_ltr() doesn't exist, so ignoring
    changes specific to that]
    Signed-off-by: Sushrut Shree Trivedi <quic_sushruts@quicinc.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -598,11 +598,13 @@ void pcie_aspm_init_link_state(struct pci_dev *pdev);
 void pcie_aspm_exit_link_state(struct pci_dev *pdev);
 void pcie_aspm_pm_state_change(struct pci_dev *pdev);
 void pcie_aspm_powersave_config_link(struct pci_dev *pdev);
+void pci_configure_ltr(struct pci_dev *pdev);
 #else
 static inline void pcie_aspm_init_link_state(struct pci_dev *pdev) { }
 static inline void pcie_aspm_exit_link_state(struct pci_dev *pdev) { }
 static inline void pcie_aspm_pm_state_change(struct pci_dev *pdev) { }
 static inline void pcie_aspm_powersave_config_link(struct pci_dev *pdev) { }
+static inline void pci_configure_ltr(struct pci_dev *pdev) { }
 #endif
 
 #ifdef CONFIG_PCIE_ECRC
