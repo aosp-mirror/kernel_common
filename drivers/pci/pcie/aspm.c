15e5c134b44445d6afd97085a4cab1a375d24967
David E. Box <david.e.box@linux.intel.com>
2024-02-23 14:58:48 -0600

    UPSTREAM: PCI/ASPM: Always build aspm.c
    Some ASPM-related tasks, such as save and restore of LTR and L1SS
    capabilities, still need to be performed when CONFIG_PCIEASPM is not
    enabled. To prepare for these changes, wrap the current code in aspm.c
    with an #ifdef and always build the file.
    
    Link: https://lore.kernel.org/r/20240128233212.1139663-2-david.e.box@linux.intel.com
    [bhelgaas: split build change from function moves]
    Link: https://lore.kernel.org/r/20240223205851.114931-3-helgaas@kernel.org
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    (cherry picked from commit f3994bba8200b49e3eacbe5914cd13f228e0db37)
    
    BUG=b:297527919
    BUG=b:356740517
    TEST=CQ
    
    Change-Id: Id443271bd2e8fce45170fcc4a1f452d469fc8971
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5894852
    Reviewed-by: Łukasz Majczak <lmajczak@google.com>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Signed-off-by: Mateusz Urbańczyk <urbanczyk@google.com>

--- drivers/pci/pcie/aspm.c
+++ drivers/pci/pcie/aspm.c
@@ -21,6 +21,8 @@
 #include <linux/delay.h>
 #include "../pci.h"
 
+#ifdef CONFIG_PCIEASPM
+
 #ifdef MODULE_PARAM_PREFIX
 #undef MODULE_PARAM_PREFIX
 #endif
@@ -1513,3 +1515,5 @@ bool pcie_aspm_support_enabled(void)
 {
 	return aspm_support_enabled;
 }
+
+#endif /* CONFIG_PCIEASPM */
