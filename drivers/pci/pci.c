eaddd7157d019ff9415b6f65ae506f169a5d54c6
David E. Box <david.e.box@linux.intel.com>
2024-02-23 14:58:49 -0600

    BACKPORT: PCI/ASPM: Move pci_save_ltr_state() to aspm.c
    Even when CONFIG_PCIEASPM is not set, we save and restore the LTR
    Capability so that if ASPM L1.2 and LTR were configured by the platform,
    ASPM L1.2 will still work after suspend/resume, when that platform
    configuration may be lost. See dbbfadf23190 ("PCI/ASPM: Save LTR Capability
    for suspend/resume").
    
    Since ASPM L1.2 depends on the LTR Capability, move the save/restore code
    to the part of aspm.c that is always compiled regardless of
    CONFIG_PCIEASPM.  No functional change intended.
    
    Suggested-by: Bjorn Helgaas <bhelgaas@google.com>
    Link: https://lore.kernel.org/r/20240128233212.1139663-5-david.e.box@linux.intel.com
    [bhelgaas: commit log, reorder to make this a pure move]
    Link: https://lore.kernel.org/r/20240223205851.114931-4-helgaas@kernel.org
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    BUG: 371115070
    Change-Id: If61407263095c0bff496c71dc1f559012965de9f
    (cherry picked from commit 1e11b5494c3dbb1e5fce7e95021c1698799c7288)
    [sushrut: In contrast to upstream, we are enabling save/restore
    only when CONFIG_PCIEASPM is enabled to avoid making changes in
    pci_dev struct. Added required inline prototypes for save_ltr()
    and restore_ltr() APIs when CONFIG_PCIEASPM is disabled.]
    Signed-off-by: Sushrut Shree Trivedi <quic_sushruts@quicinc.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -1533,46 +1533,6 @@ static void pci_restore_pcix_state(struct pci_dev *dev)
 	pci_write_config_word(dev, pos + PCI_X_CMD, cap[i++]);
 }
 
-static void pci_save_ltr_state(struct pci_dev *dev)
-{
-	int ltr;
-	struct pci_cap_saved_state *save_state;
-	u16 *cap;
-
-	if (!pci_is_pcie(dev))
-		return;
-
-	ltr = pci_find_ext_capability(dev, PCI_EXT_CAP_ID_LTR);
-	if (!ltr)
-		return;
-
-	save_state = pci_find_saved_ext_cap(dev, PCI_EXT_CAP_ID_LTR);
-	if (!save_state) {
-		pci_err(dev, "no suspend buffer for LTR; ASPM issues possible after resume\n");
-		return;
-	}
-
-	cap = (u16 *)&save_state->cap.data[0];
-	pci_read_config_word(dev, ltr + PCI_LTR_MAX_SNOOP_LAT, cap++);
-	pci_read_config_word(dev, ltr + PCI_LTR_MAX_NOSNOOP_LAT, cap++);
-}
-
-static void pci_restore_ltr_state(struct pci_dev *dev)
-{
-	struct pci_cap_saved_state *save_state;
-	int ltr;
-	u16 *cap;
-
-	save_state = pci_find_saved_ext_cap(dev, PCI_EXT_CAP_ID_LTR);
-	ltr = pci_find_ext_capability(dev, PCI_EXT_CAP_ID_LTR);
-	if (!save_state || !ltr)
-		return;
-
-	cap = (u16 *)&save_state->cap.data[0];
-	pci_write_config_word(dev, ltr + PCI_LTR_MAX_SNOOP_LAT, *cap++);
-	pci_write_config_word(dev, ltr + PCI_LTR_MAX_NOSNOOP_LAT, *cap++);
-}
-
 /**
  * pci_save_state - save the PCI configuration space of a device before
  *		    suspending
