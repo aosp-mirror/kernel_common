af6f58c8bdb6856ee01f7f694e664b5b6d219f40
Animesh Manna <animesh.manna@intel.com>
2024-02-29 10:07:16 +0530

    UPSTREAM: drm/i915/panelreplay: Move out psr_init_dpcd() from init_connector()
    Move psr_init_dpcd() from init-connector to connector-detect
    function. The dpcd probe for checking panel replay capability
    for external dp connector is causing delay during boot which can
    be optimized by moving dpcd probe to connector specific detect().
    
    v1: Initial version.
    v2: Add details in commit description. [Jani]
    
    Suggested-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Closes: https://gitlab.freedesktop.org/drm/intel/-/issues/10284
    Signed-off-by: Animesh Manna <animesh.manna@intel.com>
    Fixes: cceeaa312d39 ("drm/i915/panelreplay: Enable panel replay dpcd initialization for DP")
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240229043716.4065760-1-animesh.manna@intel.com
    (cherry picked from commit 1cca19bf296fae0636a637b48d195ac6b4d430c9)
    
    BUG=b:308455498
    TEST=Verify Asus PG32UQXR 4K160 mode on Rex using Big Joiner
    
    Change-Id: I67695063b68d8ea93ee4785dd38cf60326d48e89
    Signed-off-by: Rakshith M O <rakshith.m.o@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5802788
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Manasi Navare <navaremanasi@chromium.org>
    Signed-off-by: Hubert Mazur <hmazur@google.com>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -5877,6 +5877,9 @@ intel_dp_detect(struct drm_connector *connector,
 	if (ret == 1)
 		intel_connector->base.epoch_counter++;
 
+	if (!intel_dp_is_edp(intel_dp))
+		intel_psr_init_dpcd(intel_dp);
+
 	intel_dp_detect_dsc_caps(intel_dp, intel_connector);
 
 	intel_dp_mst_configure(intel_dp);
