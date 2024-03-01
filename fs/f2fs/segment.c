bffffc7bc2d3d9d4bc499fcece7bfa24bc87afb8
Zhiguo Niu <zhiguo.niu@unisoc.com>
2024-03-01 16:25:54 +0800

    BACKPORT: f2fs: fix to check return value __allocate_new_segment
    __allocate_new_segment may return error when get_new_segment
    fails, so its caller should check its return value.
    
    Bug: 337626258
    Signed-off-by: Zhiguo Niu <zhiguo.niu@unisoc.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit ca702aed64d80e34027eb0ed7a47249d45ed7021)
    [xiuhong: Resolved minor conflict in fs/f2fs/f2fs.h]
    Change-Id: Ia9fdd89b41f53a4cd12ba719393e4759b0b6136a
    Signed-off-by: Xiuhong Wang <xiuhong.wang@unisoc.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -2978,16 +2978,19 @@ void f2fs_allocate_new_section(struct f2fs_sb_info *sbi, int type, bool force)
 	f2fs_up_read(&SM_I(sbi)->curseg_lock);
 }
 
-void f2fs_allocate_new_segments(struct f2fs_sb_info *sbi)
+int f2fs_allocate_new_segments(struct f2fs_sb_info *sbi)
 {
 	int i;
+	int err = 0;
 
 	f2fs_down_read(&SM_I(sbi)->curseg_lock);
 	down_write(&SIT_I(sbi)->sentry_lock);
 	for (i = CURSEG_HOT_DATA; i <= CURSEG_COLD_DATA; i++)
-		__allocate_new_segment(sbi, i, false, false);
+		err += __allocate_new_segment(sbi, i, false, false);
 	up_write(&SIT_I(sbi)->sentry_lock);
 	f2fs_up_read(&SM_I(sbi)->curseg_lock);
+
+	return err;
 }
 
 static const struct segment_allocation default_salloc_ops = {
