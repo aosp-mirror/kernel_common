241e0d860fd101a70fbe4cc6fa99249b4425cfbf
Greg Kroah-Hartman <gregkh@google.com>
2024-03-05 10:18:04 +0000

    Revert "hrtimer: Report offline hrtimer enqueue"
    This reverts commit a012efe0df044134cefe49f92cb71ec898fe135e which is
    commit dad6a09f3148257ac1773cd90934d721d68ab595 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I60b729d7bdef7b6fdfcd1b32dd5f8b10242283f8
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -2069,7 +2069,6 @@ int hrtimers_prepare_cpu(unsigned int cpu)
 	cpu_base->softirq_next_timer = NULL;
 	cpu_base->expires_next = KTIME_MAX;
 	cpu_base->softirq_expires_next = KTIME_MAX;
-	cpu_base->online = 1;
 	hrtimer_cpu_base_init_expiry_lock(cpu_base);
 	return 0;
 }
