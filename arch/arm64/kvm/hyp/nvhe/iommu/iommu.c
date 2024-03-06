CodeLinaro-mirror/kernel-platform-2-0-os_kernel_common

ANDROID: KVM: arm64: iommu: Register IOMMU driver dynamically
As step to modularize the driver, make IOMMU registration dynamic.
Remove static calls to the SMMUv3 driver, and remove the IOMMU driver
from pkvm_init HVC.

Add a new file for IOMMU EL1 functions.

Bug: 277989609
Bug: 278749606
Change-Id: Ie742f12c2307248f77e85af9480c4745e188ebb9
Signed-off-by: Mostafa Saleh <smostafa@google.com>

arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -106,7 +106,7 @@ int kvm_iommu_alloc_domain(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return -EINVAL;
 
@@ -119,7 +119,7 @@ int kvm_iommu_alloc_domain(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 		goto out_unlock;
 
 	iopt = domain_to_iopt(iommu, domain, domain_id);
-	ret = kvm_iommu_ops.alloc_iopt(&iopt, pgd_hva);
+	ret = kvm_iommu_ops->alloc_iopt(&iopt, pgd_hva);
 	if (ret)
 		goto out_unlock;
 
@@ -137,7 +137,7 @@ int kvm_iommu_free_domain(pkvm_handle_t iommu_id, pkvm_handle_t domain_id)
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return -EINVAL;
 
@@ -162,7 +162,7 @@ int kvm_iommu_free_domain(pkvm_handle_t iommu_id, pkvm_handle_t domain_id)
 out_unlock:
 	hyp_spin_unlock(&iommu->lock);
 	if (!ret)
-		ret = kvm_iommu_ops.free_iopt(&iopt);
+		ret = kvm_iommu_ops->free_iopt(&iopt);
 	return ret;
 }
 
@@ -173,7 +173,7 @@ int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return -EINVAL;
 
@@ -182,7 +182,7 @@ int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	if (!domain || domain_get(domain))
 		goto out_unlock;
 
-	ret = kvm_iommu_ops.attach_dev(iommu, domain_id, domain, endpoint_id);
+	ret = kvm_iommu_ops->attach_dev(iommu, domain_id, domain, endpoint_id);
 	if (ret)
 		goto err_put_domain;
 
@@ -201,7 +201,7 @@ int kvm_iommu_detach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return -EINVAL;
 
@@ -210,7 +210,7 @@ int kvm_iommu_detach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	if (!domain || atomic_read(&domain->refs) <= 1)
 		goto out_unlock;
 
-	ret = kvm_iommu_ops.detach_dev(iommu, domain_id, domain, endpoint_id);
+	ret = kvm_iommu_ops->detach_dev(iommu, domain_id, domain, endpoint_id);
 	if (ret)
 		goto out_unlock;
 
@@ -236,6 +236,9 @@ size_t kvm_iommu_map_pages(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
+	if (!kvm_iommu_ops)
+		return 0;
+
 	if (prot & ~IOMMU_PROT_MASK)
 		return 0;
 
@@ -243,7 +246,7 @@ size_t kvm_iommu_map_pages(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	    iova + size < iova || paddr + size < paddr)
 		return 0;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return 0;
 
@@ -307,6 +310,9 @@ size_t kvm_iommu_unmap_pages(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
+	if (!kvm_iommu_ops)
+		return 0;
+
 	if (!pgsize || !pgcount)
 		return 0;
 
@@ -314,7 +320,7 @@ size_t kvm_iommu_unmap_pages(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	    iova + size < iova)
 		return 0;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return 0;
 
@@ -359,7 +365,7 @@ phys_addr_t kvm_iommu_iova_to_phys(pkvm_handle_t iommu_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return 0;
 
@@ -425,11 +431,11 @@ int kvm_iommu_init(void)
 {
 	enum kvm_pgtable_prot prot;
 
-	if (WARN_ON(!kvm_iommu_ops.get_iommu_by_id ||
-		    !kvm_iommu_ops.alloc_iopt ||
-		    !kvm_iommu_ops.free_iopt ||
-		    !kvm_iommu_ops.attach_dev ||
-		    !kvm_iommu_ops.detach_dev))
+	if (WARN_ON(!kvm_iommu_ops->get_iommu_by_id ||
+		    !kvm_iommu_ops->alloc_iopt ||
+		    !kvm_iommu_ops->free_iopt ||
+		    !kvm_iommu_ops->attach_dev ||
+		    !kvm_iommu_ops->detach_dev))
 		return -ENODEV;
 
 	/* The memcache is shared with the host */

arch/arm64/kvm/iommu.c
@@ -1,0 +1,46 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2023 Google LLC
+ * Author: Mostafa Saleh <smostafa@google.com>
+ */
+
+#include <asm/kvm_mmu.h>
+#include <linux/kvm_host.h>
+#include <kvm/iommu.h>
+
+struct kvm_iommu_driver *iommu_driver;
+extern struct kvm_iommu_ops *kvm_nvhe_sym(kvm_iommu_ops);
+
+int kvm_iommu_register_driver(struct kvm_iommu_driver *kern_ops, struct kvm_iommu_ops *el2_ops)
+{
+	int ret;
+
+	BUG_ON(!kern_ops || !el2_ops);
+
+	/*
+	 * Paired with smp_load_acquire(&iommu_driver)
+	 * Ensure memory stores happening during a driver
+	 * init are observed before executing kvm iommu callbacks.
+	 */
+	ret = cmpxchg_release(&iommu_driver, NULL, kern_ops) ? -EBUSY : 0;
+	if (ret)
+		return ret;
+
+	kvm_nvhe_sym(kvm_iommu_ops) = el2_ops;
+	return 0;
+}
+EXPORT_SYMBOL_GPL(kvm_iommu_register_driver);
+
+int kvm_iommu_init_driver(void)
+{
+	if (WARN_ON(!smp_load_acquire(&iommu_driver)))
+		return -ENODEV;
+
+	return iommu_driver->init_driver();
+}
+
+void kvm_iommu_remove_driver(void)
+{
+	if (smp_load_acquire(&iommu_driver))
+		iommu_driver->remove_driver();
+}

aosp-mirror/kernel_common

arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -80,7 +80,7 @@ int kvm_iommu_alloc_domain(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return -EINVAL;
 
@@ -94,7 +94,7 @@ int kvm_iommu_alloc_domain(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 
 	domain->domain_id = domain_id;
 	domain->iommu = iommu;
-	ret = kvm_iommu_ops.alloc_domain(domain, pgd_hva);
+	ret = kvm_iommu_ops->alloc_domain(domain, pgd_hva);
 	if (ret)
 		goto out_unlock;
 
@@ -110,7 +110,7 @@ int kvm_iommu_free_domain(pkvm_handle_t iommu_id, pkvm_handle_t domain_id)
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return -EINVAL;
 
@@ -122,13 +122,14 @@ int kvm_iommu_free_domain(pkvm_handle_t iommu_id, pkvm_handle_t domain_id)
 	if (domain->refs != 1)
 		goto out_unlock;
 
-	kvm_iommu_ops.free_domain(domain);
+	kvm_iommu_ops->free_domain(domain);
 
 	/* Set domain->refs to 0 and mark it as unused. */
 	memset(domain, 0, sizeof(*domain));
 
 out_unlock:
 	hyp_spin_unlock(&iommu->lock);
+
 	return ret;
 }
 
@@ -139,7 +140,7 @@ int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return -EINVAL;
 
@@ -148,7 +149,7 @@ int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	if (!domain || !domain->refs || domain->refs == UINT_MAX)
 		goto out_unlock;
 
-	ret = kvm_iommu_ops.attach_dev(iommu, domain, endpoint_id);
+	ret = kvm_iommu_ops->attach_dev(iommu, domain, endpoint_id);
 	if (ret)
 		goto out_unlock;
 
@@ -165,7 +166,7 @@ int kvm_iommu_detach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return -EINVAL;
 
@@ -174,7 +175,7 @@ int kvm_iommu_detach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	if (!domain || domain->refs <= 1)
 		goto out_unlock;
 
-	ret = kvm_iommu_ops.detach_dev(iommu, domain, endpoint_id);
+	ret = kvm_iommu_ops->detach_dev(iommu, domain, endpoint_id);
 	if (ret)
 		goto out_unlock;
 
@@ -199,6 +200,9 @@ size_t kvm_iommu_map_pages(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
+	if (!kvm_iommu_ops)
+		return 0;
+
 	if (prot & ~IOMMU_PROT_MASK)
 		return 0;
 
@@ -206,7 +210,7 @@ size_t kvm_iommu_map_pages(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	    iova + size < iova || paddr + size < paddr)
 		return 0;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return 0;
 
@@ -282,6 +286,9 @@ size_t kvm_iommu_unmap_pages(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 		.arg = cache,
 	};
 
+	if (!kvm_iommu_ops)
+		return 0;
+
 	if (!pgsize || !pgcount)
 		return 0;
 
@@ -289,7 +296,7 @@ size_t kvm_iommu_unmap_pages(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	    iova + size < iova)
 		return 0;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return 0;
 
@@ -327,7 +334,7 @@ phys_addr_t kvm_iommu_iova_to_phys(pkvm_handle_t iommu_id,
 	struct kvm_hyp_iommu *iommu;
 	struct kvm_hyp_iommu_domain *domain;
 
-	iommu = kvm_iommu_ops.get_iommu_by_id(iommu_id);
+	iommu = kvm_iommu_ops->get_iommu_by_id(iommu_id);
 	if (!iommu)
 		return 0;
 
@@ -357,11 +364,11 @@ int kvm_iommu_init(void)
 {
 	enum kvm_pgtable_prot prot;
 
-	if (WARN_ON(!kvm_iommu_ops.get_iommu_by_id ||
-		    !kvm_iommu_ops.alloc_domain ||
-		    !kvm_iommu_ops.free_domain ||
-		    !kvm_iommu_ops.attach_dev ||
-		    !kvm_iommu_ops.detach_dev))
+	if (WARN_ON(!kvm_iommu_ops->get_iommu_by_id ||
+		    !kvm_iommu_ops->alloc_domain ||
+		    !kvm_iommu_ops->free_domain ||
+		    !kvm_iommu_ops->attach_dev ||
+		    !kvm_iommu_ops->detach_dev))
 		return -ENODEV;
 
 	/* The memcache is shared with the host */

arch/arm64/kvm/iommu.c
@@ -1,0 +1,46 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2023 Google LLC
+ * Author: Mostafa Saleh <smostafa@google.com>
+ */
+
+#include <asm/kvm_mmu.h>
+#include <linux/kvm_host.h>
+#include <kvm/iommu.h>
+
+struct kvm_iommu_driver *iommu_driver;
+extern struct kvm_iommu_ops *kvm_nvhe_sym(kvm_iommu_ops);
+
+int kvm_iommu_register_driver(struct kvm_iommu_driver *kern_ops, struct kvm_iommu_ops *el2_ops)
+{
+	int ret;
+
+	BUG_ON(!kern_ops || !el2_ops);
+
+	/*
+	 * Paired with smp_load_acquire(&iommu_driver)
+	 * Ensure memory stores happening during a driver
+	 * init are observed before executing kvm iommu callbacks.
+	 */
+	ret = cmpxchg_release(&iommu_driver, NULL, kern_ops) ? -EBUSY : 0;
+	if (ret)
+		return ret;
+
+	kvm_nvhe_sym(kvm_iommu_ops) = el2_ops;
+	return 0;
+}
+EXPORT_SYMBOL(kvm_iommu_register_driver);
+
+int kvm_iommu_init_driver(void)
+{
+	if (WARN_ON(!smp_load_acquire(&iommu_driver)))
+		return -ENODEV;
+
+	return iommu_driver->init_driver();
+}
+
+void kvm_iommu_remove_driver(void)
+{
+	if (smp_load_acquire(&iommu_driver))
+		iommu_driver->remove_driver();
+}