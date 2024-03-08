4a4450b4a7205367d1e8ed43a8792cbaf2cfb1e9
Lokesh Gidra <lokeshgidra@google.com>
2024-03-08 10:04:39 -0800

    ANDROID: userfaultfd: add MMAP_TRYLOCK mode for COPY/ZEROPAGE
    In case mmap_lock is contended, it is possible that userspace can spend
    time performing other tasks rather than waiting in uninterruptible-sleep
    state for the lock to become available. Even if no other task is
    available, it is better to yield or sleep rather than adding contention
    to already contended lock.
    
    We introduce MMAP_TRYLOCK mode so that when possible, userspace can
    request to use mmap_read_trylock(), returning -EAGAIN if and when it
    fails.
    
    Bug: 320478828
    Change-Id: I2d196fd317e054af03dbd35ac1b0c7634cb370dc
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>

--- include/linux/userfaultfd_k.h
+++ include/linux/userfaultfd_k.h
@@ -33,6 +33,9 @@
 #define UFFD_SHARED_FCNTL_FLAGS (O_CLOEXEC | O_NONBLOCK)
 #define UFFD_FLAGS_SET (EFD_SHARED_FCNTL_FLAGS)
 
+static_assert(UFFDIO_ZEROPAGE_MODE_MMAP_TRYLOCK == UFFDIO_COPY_MODE_MMAP_TRYLOCK);
+#define UFFDIO_MODE_MMAP_TRYLOCK UFFDIO_COPY_MODE_MMAP_TRYLOCK
+
 extern int sysctl_unprivileged_userfaultfd;
 
 extern vm_fault_t handle_userfault(struct vm_fault *vmf, unsigned long reason);
@@ -65,9 +68,8 @@ extern ssize_t mcopy_atomic(struct mm_struct *dst_mm, unsigned long dst_start,
 			    unsigned long src_start, unsigned long len,
 			    bool *mmap_changing, __u64 mode);
 extern ssize_t mfill_zeropage(struct mm_struct *dst_mm,
-			      unsigned long dst_start,
-			      unsigned long len,
-			      bool *mmap_changing);
+			      unsigned long dst_start, unsigned long len,
+			      bool *mmap_changing, __u64 mode);
 extern ssize_t mcopy_continue(struct mm_struct *dst_mm, unsigned long dst_start,
 			      unsigned long len, bool *mmap_changing);
 extern int mwriteprotect_range(struct mm_struct *dst_mm,
