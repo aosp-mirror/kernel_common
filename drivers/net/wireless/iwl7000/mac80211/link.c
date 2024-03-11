def44c9e19c7b7c1c0424301541df6e5db985204
Johannes Berg <johannes.berg@intel.com>
2024-03-11 12:28:15 +0100

    CHROMIUM: iwl7000: mac80211: don't enter idle during link switch
    When doing link switch with a disjoint set of links before
    and after the switch, we end up removing all channel contexts,
    adding new ones later. This looks like 'idle' to the code now,
    and we enter idle which also includes flushing queues. But we
    can't actually flush since we don't have a link active (bound
    to a channel context), and entering idle just to leave it again
    is also wrong.
    
    Fix this by passing through an indication that we shouldn't do
    any idle checks in this case.
    
    Change-Id: If4a522a9dd3133b91983854b909a4de13aa635da
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    (cherry picked from commit 058c30f1131e35607277dc2561a66eb4ee3590a3)
    PerCI-Ready: Golan Ben Ami <golan.ben.ami@intel.com>
    iwl7000-tree: 4f70dc32c2a2f96cb53f44cd683dd7b061f0fbbd

--- drivers/net/wireless/iwl7000/mac80211/link.c
+++ drivers/net/wireless/iwl7000/mac80211/link.c
@@ -362,7 +362,7 @@ static int _ieee80211_set_active_links(struct ieee80211_sub_if_data *sdata,
 
 		ieee80211_teardown_tdls_peers(link);
 
-		ieee80211_link_release_channel(link);
+		__ieee80211_link_release_channel(link, true);
 
 		/*
 		 * If CSA is (still) active while the link is deactivated,
