f68e05cd5b29f241f096c676186002f25803c6a1
seanwang1 <seanwang1@lenovo.com>
2024-04-25 17:08:03 +0800

    ANDROID: GKI: Export css_task_iter_start()
    Export css_task_iter_start() and css_task_iter_next() and
    css_task_iter_end() inorder to support task iteration in a cgroup in
    vendor modules.
    
    Bug: 348564818
    Bug: 353112770
    
    Change-Id: Id93963ddd30ab02c7a4d5086f19d15310e4eda14
    Signed-off-by: seanwang1 <seanwang1@lenovo.com>
    (cherry picked from commit 978f805a2d4b5a0bed9576750152af33f1b4fe45)
    (cherry picked from commit 0383c45728a68989b76a7afeb99d6e9e611c99d4)

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -4941,6 +4941,7 @@ void css_task_iter_start(struct cgroup_subsys_state *css, unsigned int flags,
 
 	spin_unlock_irq(&css_set_lock);
 }
+EXPORT_SYMBOL_GPL(css_task_iter_start);
 
 /**
  * css_task_iter_next - return the next task for the iterator
@@ -4974,6 +4975,7 @@ struct task_struct *css_task_iter_next(struct css_task_iter *it)
 
 	return it->cur_task;
 }
+EXPORT_SYMBOL_GPL(css_task_iter_next);
 
 /**
  * css_task_iter_end - finish task iteration
@@ -4996,6 +4998,7 @@ void css_task_iter_end(struct css_task_iter *it)
 	if (it->cur_task)
 		put_task_struct(it->cur_task);
 }
+EXPORT_SYMBOL_GPL(css_task_iter_end);
 
 static void cgroup_procs_release(struct kernfs_open_file *of)
 {
