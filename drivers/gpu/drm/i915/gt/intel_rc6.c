greenforce-project/chromeos-kernel-mirror

CHROMIUM: drm/i915: disable software rc6 control on JSL platform
This is a temporary WA on JSL platform, which does not manually
unpark/park GT in order to prevent system hang while playing
video. The HW rc6 control is still running after such software
rc6 control is disabled.

BUG=b:303841978
UPSTREAM-TASK=b:307840654
TEST=Make sure the rc6 residency is 0 after DUT boots up,
  via command "cat /sys/class/drm/card0/power/rc6_residency_ms".

Change-Id: I5b11696c32bf9101208a8c8267669e7bc67875cf
Signed-off-by: William Tseng <william.tseng@intel.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4978297
Reviewed-by: Derek Huang <derekhuang@google.com>
Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
Reviewed-by: Prahlad Kilambi <prahladk@google.com>
Reviewed-by: Shinni Tsai <shinni.tsai@intel.com>
Tested-by: Alvin1 Chen <alvin1.chen@lcfc.corp-partner.google.com>
Reviewed-by: Shawn C Lee <shawn.c.lee@intel.com>
Reviewed-by: Miguel Casas <mcasas@google.com>

drivers/gpu/drm/i915/gt/intel_rc6.c
@@ -5,7 +5,6 @@
  */
 
 #include <linux/pm_runtime.h>
-#include <linux/dmi.h>
 
 #include "i915_drv.h"
 #include "intel_gt.h"
@@ -457,25 +456,6 @@ static bool bxt_check_bios_rc6_setup(struct intel_rc6 *rc6)
 	return enable_rc6;
 }
 
-static int intel_rc6_support_disabled(const struct dmi_system_id *id)
-{
-	DRM_DEBUG_KMS("A temporary WA to prevent system hang on %s device\n",
-			id->ident);
-	return 1;
-}
-
-static const struct dmi_system_id intel_rc6_support_detect[] = {
-	{
-		.callback = intel_rc6_support_disabled,
-		.ident = "Google Bugzzy",
-		.matches = {
-			DMI_MATCH(DMI_BOARD_VENDOR, "Google"),
-			DMI_MATCH(DMI_BOARD_NAME, "Bugzzy"),
-		}
-	},
-	{}
-};
-
 static bool rc6_supported(struct intel_rc6 *rc6)
 {
 	struct drm_i915_private *i915 = rc6_to_i915(rc6);
@@ -495,8 +475,21 @@ static bool rc6_supported(struct intel_rc6 *rc6)
 		return false;
 	}
 
-	if (dmi_check_system(intel_rc6_support_detect))
-		return false;
+	/*
+	 * The temporary WA to prevent system hang is to stop doing
+	 * software RC6 control, which manually force GT entering
+	 * and exiting from RC6. Noted that HW RC6 is still enabled
+	 * by default.
+	 *
+	 * TODO: enable software RC6 contorl after a fix is found.
+	 */
+	if (INTEL_GEN(i915) == 11) {
+		if (IS_PLATFORM(i915, INTEL_JASPERLAKE)) {
+			drm_notice(&i915->drm,
+			   "WA: Software RC6 disabled for Jasperlake platform\n");
+			return false;
+		}
+	}
 
 	return true;
 }

aosp-mirror/kernel_common

(cherry picked from commit f12c11336c76561cc538f67ff694a8de80bd5b0d)
 Conflicts:
	drivers/gpu/drm/i915/gt/intel_rc6.c
    -dmi.h include was removed and string_helpers.h was added
    -Other platforms were added to rc6_supported()
    -INTEL_GEN() -> DISPLAY_VER()
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5155369
Tested-by: Mark Hasemeyer <markhas@google.com>
Reviewed-by: Ilja Friedel <ihf@chromium.org>
Commit-Queue: Mark Hasemeyer <markhas@google.com>

drivers/gpu/drm/i915/gt/intel_rc6.c
@@ -531,6 +531,22 @@ static bool rc6_supported(struct intel_rc6 *rc6)
 		return false;
 	}
 
+	/*
+	 * The temporary WA to prevent system hang is to stop doing
+	 * software RC6 control, which manually force GT entering
+	 * and exiting from RC6. Noted that HW RC6 is still enabled
+	 * by default.
+	 *
+	 * TODO: enable software RC6 contorl after a fix is found.
+	 */
+	if (DISPLAY_VER(i915) == 11) {
+		if (IS_PLATFORM(i915, INTEL_JASPERLAKE)) {
+			drm_notice(&i915->drm,
+			   "WA: Software RC6 disabled for Jasperlake platform\n");
+			return false;
+		}
+	}
+
 	return true;
 }
 