rockchip-linux/kernel

CHROMIUM: drm/rockchip: Add GEM create ioctl support
Rockchip Socs have GPU, we need allocate GPU accelerated buffers.
So add special ioctls GEM_CREATE/GEM_MAP_OFFSET to support
accelerated buffers.

Signed-off-by: Mark Yao <mark.yao@rock-chips.com>

BUG=chromium:399935
TEST=With rest of patch set, can boot to UI on eDP

Change-Id: Ia4b13798aac97d16214da7a75a2479e6e334313e
Reviewed-on: https://chromium-review.googlesource.com/222153
Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
Tested-by: Daniel Kurtz <djkurtz@chromium.org>
Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>

drivers/gpu/drm/rockchip/rockchip_drm_gem.c
@@ -15,6 +15,7 @@
 #include <drm/drm.h>
 #include <drm/drmP.h>
 #include <drm/drm_vma_manager.h>
+#include <drm/rockchip_drm.h>
 
 #include <linux/dma-attrs.h>
 
@@ -248,6 +249,26 @@ int rockchip_gem_dumb_create(struct drm_file *file_priv,
 	rk_obj = rockchip_gem_create_with_handle(file_priv, dev, args->size,
 						 &args->handle);
 
+	return PTR_ERR_OR_ZERO(rk_obj);
+}
+
+int rockchip_gem_map_offset_ioctl(struct drm_device *drm, void *data,
+				  struct drm_file *file_priv)
+{
+	struct drm_rockchip_gem_map_off *args = data;
+
+	return rockchip_gem_dumb_map_offset(file_priv, drm, args->handle,
+					    &args->offset);
+}
+
+int rockchip_gem_create_ioctl(struct drm_device *dev, void *data,
+			      struct drm_file *file_priv)
+{
+	struct drm_rockchip_gem_create *args = data;
+	struct rockchip_gem_object *rk_obj;
+
+	rk_obj = rockchip_gem_create_with_handle(file_priv, dev, args->size,
+						 &args->handle);
 	return PTR_ERR_OR_ZERO(rk_obj);
 }
 

aosp-mirror/kernel_common

BUG=chromium:941638
Conflicts:
	drivers/gpu/drm/rockchip/rockchip_drm_drv.c
(cherry picked from cros/chromeos-3.14 commit c29c5a3037e18815937d8af664738e499ada94d1)
Signed-off-by: ZhengShunQian <zhengsq@rock-chips.com>
Reviewed-on: https://chromium-review.googlesource.com/346321
Commit-Ready: Yakir Yang <ykk@rock-chips.com>
Tested-by: Yakir Yang <ykk@rock-chips.com>
Reviewed-by: Tomasz Figa <tfiga@chromium.org>
Change-Id: I2fb5a2011c973bbab16b47713754de44cfa0252f
	drivers/gpu/drm/rockchip/rockchip_drm_gem.h
[dianders: Fixed to account for commit 8cfd4f5de496 ("drm/rockchip:
 Use .dumb_map_offset and .dumb_destroy defaults") upstream]; also
 removed 2nd copy of rockchip_gem_map_offset_ioctl prototype; use SPDX
 header now too]
(cherry picked from commit a6c56450f406dc75a3be59724cefd6075244fb15)
Signed-off-by: Douglas Anderson <dianders@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/1538897
Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
  drivers/gpu/drm/rockchip/rockchip_drm_drv.c
[rebase54(mka): added 'WITH Linux-syscall-note' to SPDX license identifer
  fixed context delta in includes]
[rebase54(groeck): Added missing include files to
	include/uapi/drm/rockchip_drm.h; do not rely on drm.h]
Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
[rebase510(groeck): Squashed:
	CHROMIUM: drm/rockchip: Enable render node
]
Signed-off-by: Guenter Roeck <groeck@chromium.org>
Change-Id: I0261e4980c178876e84dd5049b9ea49657b13bf9
Kcr-patch: 2d20a9d5a2a2e6ccff03906048cdfcfec946366c66cbaf702f64a6dc.patch

drivers/gpu/drm/rockchip/rockchip_drm_gem.c
@@ -14,6 +14,7 @@
 #include <drm/drm_gem_dma_helper.h>
 #include <drm/drm_prime.h>
 #include <drm/drm_vma_manager.h>
+#include <drm/rockchip_drm.h>
 
 #include "rockchip_drm_drv.h"
 #include "rockchip_drm_gem.h"
@@ -414,6 +415,26 @@ int rockchip_gem_dumb_create(struct drm_file *file_priv,
 	rk_obj = rockchip_gem_create_with_handle(file_priv, dev, args->size,
 						 &args->handle);
 
+	return PTR_ERR_OR_ZERO(rk_obj);
+}
+
+int rockchip_gem_map_offset_ioctl(struct drm_device *drm, void *data,
+				  struct drm_file *file_priv)
+{
+	struct drm_rockchip_gem_map_off *args = data;
+
+	return drm_gem_dumb_map_offset(file_priv, drm, args->handle,
+				       &args->offset);
+}
+
+int rockchip_gem_create_ioctl(struct drm_device *dev, void *data,
+			      struct drm_file *file_priv)
+{
+	struct drm_rockchip_gem_create *args = data;
+	struct rockchip_gem_object *rk_obj;
+
+	rk_obj = rockchip_gem_create_with_handle(file_priv, dev, args->size,
+						 &args->handle);
 	return PTR_ERR_OR_ZERO(rk_obj);
 }
 
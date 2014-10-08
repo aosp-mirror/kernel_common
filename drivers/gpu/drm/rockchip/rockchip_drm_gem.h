3a9d38c1da277a78868f5b6a7831b62637936e96
Mark Yao <yzq@rock-chips.com>
2014-10-08 17:11:15 +0800

    CHROMIUM: drm/rockchip: Add GEM create ioctl support
    Rockchip Socs have GPU, we need allocate GPU accelerated buffers.
    So add special ioctls GEM_CREATE/GEM_MAP_OFFSET to support
    accelerated buffers.
    
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>
    
    BUG=chromium:941638
    TEST=With rest of patch set, can boot to UI on eDP
    
    Reviewed-on: https://chromium-review.googlesource.com/222153
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>
    Conflicts:
    	drivers/gpu/drm/rockchip/rockchip_drm_drv.c
    (cherry picked from cros/chromeos-3.14 commit c29c5a3037e18815937d8af664738e499ada94d1)
    Signed-off-by: ZhengShunQian <zhengsq@rock-chips.com>
    
    Reviewed-on: https://chromium-review.googlesource.com/346321
    Commit-Ready: Yakir Yang <ykk@rock-chips.com>
    Tested-by: Yakir Yang <ykk@rock-chips.com>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    
    Change-Id: I2fb5a2011c973bbab16b47713754de44cfa0252f
    Conflicts:
    	drivers/gpu/drm/rockchip/rockchip_drm_gem.h
    [dianders: Fixed to account for commit 8cfd4f5de496 ("drm/rockchip:
     Use .dumb_map_offset and .dumb_destroy defaults") upstream]; also
     removed 2nd copy of rockchip_gem_map_offset_ioctl prototype; use SPDX
     header now too]
    (cherry picked from commit a6c56450f406dc75a3be59724cefd6075244fb15)
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1538897
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    
    Conflicts:
      drivers/gpu/drm/rockchip/rockchip_drm_drv.c
    
    [rebase54(mka): added 'WITH Linux-syscall-note' to SPDX license identifer
      fixed context delta in includes]
    [rebase54(groeck): Added missing include files to
    	include/uapi/drm/rockchip_drm.h; do not rely on drm.h]
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    [rebase510(groeck): Squashed:
    	CHROMIUM: drm/rockchip: Enable render node
    ]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    Change-Id: I0261e4980c178876e84dd5049b9ea49657b13bf9
    Kcr-patch: 2d20a9d5a2a2e6ccff03906048cdfcfec946366c66cbaf702f64a6dc.patch

--- drivers/gpu/drm/rockchip/rockchip_drm_gem.h
+++ drivers/gpu/drm/rockchip/rockchip_drm_gem.h
@@ -44,4 +44,17 @@ void rockchip_gem_free_object(struct drm_gem_object *obj);
 int rockchip_gem_dumb_create(struct drm_file *file_priv,
 			     struct drm_device *dev,
 			     struct drm_mode_create_dumb *args);
+
+/*
+ * request gem object creation and buffer allocation as the size
+ * that it is calculated with framebuffer information such as width,
+ * height and bpp.
+ */
+int rockchip_gem_create_ioctl(struct drm_device *dev, void *data,
+			      struct drm_file *file_priv);
+
+/* get buffer offset to map to user space. */
+int rockchip_gem_map_offset_ioctl(struct drm_device *dev, void *data,
+				  struct drm_file *file_priv);
+
 #endif /* _ROCKCHIP_DRM_GEM_H */
