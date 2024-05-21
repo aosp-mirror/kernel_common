c5c25552d6b46e7f9598ae5a51fb8ef15427ae54
Greg Kroah-Hartman <gregkh@google.com>
2024-05-21 16:11:15 +0000

    Revert "PCI: Cache PCIe Device Capabilities register"
    This reverts commit 619013d79739746bfa8139b4f210509438e772d5 which is
    commit 69139244806537f9d51364f37fe146bb2ee88a05 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: Ic2bd4ea9575eb5c79e0a77f0f65a88f0245c3b4a
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -31,7 +31,6 @@
 #include <linux/vmalloc.h>
 #include <asm/dma.h>
 #include <linux/aer.h>
-#include <linux/bitfield.h>
 #include <trace/hooks/pci.h>
 #include "pci.h"
 
@@ -4577,10 +4576,13 @@ EXPORT_SYMBOL(pci_wait_for_pending_transaction);
  */
 bool pcie_has_flr(struct pci_dev *dev)
 {
+	u32 cap;
+
 	if (dev->dev_flags & PCI_DEV_FLAGS_NO_FLR_RESET)
 		return false;
 
-	return FIELD_GET(PCI_EXP_DEVCAP_FLR, dev->devcap) == 1;
+	pcie_capability_read_dword(dev, PCI_EXP_DEVCAP, &cap);
+	return cap & PCI_EXP_DEVCAP_FLR;
 }
 EXPORT_SYMBOL_GPL(pcie_has_flr);
 
