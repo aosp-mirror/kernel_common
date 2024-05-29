dcd4aed396a4743731d84d921099709fe486a518
James Morse <james.morse@arm.com>
2024-05-29 14:34:46 +0100

    UPSTREAM: cpumask: Add enabled cpumask for present CPUs that can be brought online
    The 'offline' file in sysfs shows all offline CPUs, including those
    that aren't present. User-space is expected to remove not-present CPUs
    from this list to learn which CPUs could be brought online.
    
    CPUs can be present but not-enabled. These CPUs can't be brought online
    until the firmware policy changes, which comes with an ACPI notification
    that will register the CPUs.
    
    With only the offline and present files, user-space is unable to
    determine which CPUs it can try to bring online. Add a new CPU mask
    that shows this based on all the registered CPUs.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Tested-by: Miguel Luis <miguel.luis@oracle.com>
    Tested-by: Vishnu Pajjuri <vishnu@os.amperecomputing.com>
    Tested-by: Jianyong Wu <jianyong.wu@arm.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Link: https://lore.kernel.org/r/20240529133446.28446-20-Jonathan.Cameron@huawei.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit 4e1a7df4548003fc081360b0f4edce3f7a991bfc)
    
    BUG=b:336854404
    TEST=build kernel 6.6 with USE=gcov
    
    Change-Id: I49a6197830c0945fe3e4cb0a365d34f085d3e178
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5906222
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Mateusz Urbańczyk <urbanczyk@google.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -92,6 +92,7 @@ static inline void set_nr_cpu_ids(unsigned int nr)
  *
  *     cpu_possible_mask- has bit 'cpu' set iff cpu is populatable
  *     cpu_present_mask - has bit 'cpu' set iff cpu is populated
+ *     cpu_enabled_mask  - has bit 'cpu' set iff cpu can be brought online
  *     cpu_online_mask  - has bit 'cpu' set iff cpu available to scheduler
  *     cpu_active_mask  - has bit 'cpu' set iff cpu available to migration
  *
@@ -124,11 +125,13 @@ static inline void set_nr_cpu_ids(unsigned int nr)
 
 extern struct cpumask __cpu_possible_mask;
 extern struct cpumask __cpu_online_mask;
+extern struct cpumask __cpu_enabled_mask;
 extern struct cpumask __cpu_present_mask;
 extern struct cpumask __cpu_active_mask;
 extern struct cpumask __cpu_dying_mask;
 #define cpu_possible_mask ((const struct cpumask *)&__cpu_possible_mask)
 #define cpu_online_mask   ((const struct cpumask *)&__cpu_online_mask)
+#define cpu_enabled_mask   ((const struct cpumask *)&__cpu_enabled_mask)
 #define cpu_present_mask  ((const struct cpumask *)&__cpu_present_mask)
 #define cpu_active_mask   ((const struct cpumask *)&__cpu_active_mask)
 #define cpu_dying_mask    ((const struct cpumask *)&__cpu_dying_mask)
@@ -1046,6 +1049,7 @@ extern const DECLARE_BITMAP(cpu_all_bits, NR_CPUS);
 #else
 #define for_each_possible_cpu(cpu) for_each_cpu((cpu), cpu_possible_mask)
 #define for_each_online_cpu(cpu)   for_each_cpu((cpu), cpu_online_mask)
+#define for_each_enabled_cpu(cpu)   for_each_cpu((cpu), cpu_enabled_mask)
 #define for_each_present_cpu(cpu)  for_each_cpu((cpu), cpu_present_mask)
 #endif
 
@@ -1066,6 +1070,15 @@ set_cpu_possible(unsigned int cpu, bool possible)
 		cpumask_set_cpu(cpu, &__cpu_possible_mask);
 	else
 		cpumask_clear_cpu(cpu, &__cpu_possible_mask);
+}
+
+static inline void
+set_cpu_enabled(unsigned int cpu, bool can_be_onlined)
+{
+	if (can_be_onlined)
+		cpumask_set_cpu(cpu, &__cpu_enabled_mask);
+	else
+		cpumask_clear_cpu(cpu, &__cpu_enabled_mask);
 }
 
 static inline void
@@ -1149,12 +1162,18 @@ static __always_inline unsigned int num_online_cpus(void)
 	return raw_atomic_read(&__num_online_cpus);
 }
 #define num_possible_cpus()	cpumask_weight(cpu_possible_mask)
+#define num_enabled_cpus()	cpumask_weight(cpu_enabled_mask)
 #define num_present_cpus()	cpumask_weight(cpu_present_mask)
 #define num_active_cpus()	cpumask_weight(cpu_active_mask)
 
 static inline bool cpu_online(unsigned int cpu)
 {
 	return cpumask_test_cpu(cpu, cpu_online_mask);
+}
+
+static inline bool cpu_enabled(unsigned int cpu)
+{
+	return cpumask_test_cpu(cpu, cpu_enabled_mask);
 }
 
 static inline bool cpu_possible(unsigned int cpu)
@@ -1181,6 +1200,7 @@ static inline bool cpu_dying(unsigned int cpu)
 
 #define num_online_cpus()	1U
 #define num_possible_cpus()	1U
+#define num_enabled_cpus()	1U
 #define num_present_cpus()	1U
 #define num_active_cpus()	1U
 
@@ -1190,6 +1210,11 @@ static inline bool cpu_online(unsigned int cpu)
 }
 
 static inline bool cpu_possible(unsigned int cpu)
+{
+	return cpu == 0;
+}
+
+static inline bool cpu_enabled(unsigned int cpu)
 {
 	return cpu == 0;
 }
