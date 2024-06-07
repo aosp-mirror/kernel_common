cdb09f7ea3366d2d6e4431f1329b26bd8317beb5
Wang Jianzheng <11134417@vivo.corp-partner.google.com>
2024-06-07 11:18:44 +0800

    ANDROID: vendor hooks: add vendor hooks for do_new_mount
    Add a vendor hook for do_new_mount_fc is aimed at categorizing the
    properties according to the path. And making these properties more
    easily accessible.This will help us to customize readahead operations, improving the speed of App startup and game loading.
    
    Bug: 345590666
    
    Change-Id: I913bbb9bd4c3efd5f4c7d101056d301855881cc2
    Signed-off-by: Wang Jianzheng <11134417@vivo.corp-partner.google.com>

--- include/trace/hooks/blk.h
+++ include/trace/hooks/blk.h
@@ -17,6 +17,13 @@ DECLARE_HOOK(android_vh_bd_link_disk_holder,
 	TP_PROTO(struct block_device *bdev, struct gendisk *disk),
 	TP_ARGS(bdev, disk));
 
+struct path;
+struct vfsmount;
+
+DECLARE_HOOK(android_vh_do_new_mount_fc,
+	TP_PROTO(struct path *mountpoint, struct vfsmount *mnt),
+	TP_ARGS(mountpoint, mnt));
+
 #endif /* _TRACE_HOOK_BLK_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
