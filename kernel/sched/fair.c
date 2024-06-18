be69823b1bf0ed08a873cd01c38b7a7b9169b877
Vineeth Pillai <vineethrp@google.com>
2024-06-18 19:40:49 +0000

    CHROMIUM: sched: load balance tunables related to lowres timers
    Finch experiment was going on for the last few months with highres
    timers disabled and results are generally positive:
    
    124 beta 100%:
    https://uma.googleplex.com/p/chrome/variations?sid=a16be03f03c14b440edd235ef6554d93
    
    123 stable 1%:
    https://uma.googleplex.com/p/chrome/variations?sid=11f97e0da078a4af95ae7b465c913ee3
    
    Beta has more data as experiment is enabled 100%.
    
    This CL applies the load balancing related tunables for fixes related to low resolution timers. The fixes were gated behind the tunables for the
    finch experiments.
    
    This is enabled only for x86 as of now. More verification is needed on arm boards before we rollout this feature on non-x86 boards.
    
    BUG=b:263289152
    TEST=boot
    Cq-Depend: chromium:5730074
    Disallow-Recycled-Builds: all
    Change-Id: Iae9923b9af6ae068bfcbd8b66fc9a28c894b7f49
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5640456
    Reviewed-by: Joel Fernandes <joelaf@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5813730
    Tested-by: Joel Fernandes <joelaf@google.com>
    Commit-Queue: Joel Fernandes <joelaf@google.com>
    Signed-off-by: Hubert Mazur <hmazur@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -169,9 +169,16 @@ static unsigned int sysctl_numa_balancing_promote_rate_limit = 65536;
  * The minimum load balance interval in jiffies that must pass before a
  * a periodic or nohz-idle balance happens.
  */
+#ifdef CONFIG_X86
+static unsigned long __read_mostly sysctl_sched_min_load_balance_interval = 16UL;
+#else
 static unsigned long __read_mostly sysctl_sched_min_load_balance_interval = 1UL;
-
+#endif
+#ifdef CONFIG_X86
+DEFINE_STATIC_KEY_FALSE(sched_aggressive_next_balance);
+#else
 DEFINE_STATIC_KEY_TRUE(sched_aggressive_next_balance);
+#endif
 #endif
 
 #ifdef CONFIG_SYSCTL
