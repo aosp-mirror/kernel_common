75ffdaf12abec1e9aaa74bab2acf7e1e0b132bb0
Xiaofeng Yuan <yuanxiaofeng@vivo.com>
2024-06-25 19:56:31 +0800

    ANDROID: vendor_hooks: add hooks to modify pageflags
    These hooks are designed to set or clear OEM reserved pageflags when the
    memory state may change.
    
    Bug: 336964184
    Bug: 366116397
    
    Change-Id: I9cb288ef6eef7a719d4f4748d6b71010645b7d50
    Signed-off-by: Xiaofeng Yuan <yuanxiaofeng@vivo.com>
    Signed-off-by: renchenghan <renchenghan@vivo.corp-partner.google.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -30,6 +30,9 @@
 
 #include <linux/uprobes.h>
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/mm.h>
+
 #define UINSNS_PER_PAGE			(PAGE_SIZE/UPROBE_XOL_SLOT_BYTES)
 #define MAX_UPROBE_XOL_SLOTS		UINSNS_PER_PAGE
 
@@ -185,6 +188,7 @@ static int __replace_page(struct vm_area_struct *vma, unsigned long addr,
 		get_page(new_page);
 		page_add_new_anon_rmap(new_page, vma, addr, false);
 		lru_cache_add_inactive_or_unevictable(new_page, vma);
+		trace_android_vh_uprobes_replace_page(new_page, old_page);
 	} else
 		/* no new page, just dec_mm_counter for old_page */
 		dec_mm_counter(mm, MM_ANONPAGES);
