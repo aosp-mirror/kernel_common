1cefa59a204013112a496a1226dc88fc29b9d844
Greg Kroah-Hartman <gregkh@google.com>
2024-07-01 11:36:38 +0000

    Revert "ANDROID: vendor_hooks: Add hook for mmc queue"
    This reverts commit a3b3ea8791d5c9d13334c488ec5188caf8d3fa7d.
    
    The hook added was not used by any vendor, so remove it to help with
    merge issues with future LTS releases.
    
    If this is needed by any real user, it can easily be reverted to add it
    back and then the symbol should be added to the abi list at the same
    time to prevent it from being removed again later.
    
    Bug: 203756332
    Bug: 306334321
    Bug: 322951001
    Cc: Qilin Tan <qilin.tan@mediatek.com>
    Cc: Browse Zhang <browse.zhang@mediatek.com>
    Cc: Sanjeev Yadav <sanjeev.y@mediatek.com>
    Change-Id: I4f9dae83300d36be303fe0901e1ce83bd942b4ad
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/trace/hooks/mmc.h
+++ include/trace/hooks/mmc.h
@@ -6,19 +6,12 @@
 #if !defined(_TRACE_HOOK_MMC_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _TRACE_HOOK_MMC_H
 #include <trace/hooks/vendor_hooks.h>
-struct mmc_card;
-struct mmc_queue;
-struct blk_mq_queue_data;
 struct mmc_host;
 
 /*
  * Following tracepoints are not exported in tracefs and provide a
  * mechanism for vendor modules to hook and extend functionality
  */
-DECLARE_HOOK(android_vh_mmc_update_mmc_queue,
-	TP_PROTO(struct mmc_card *card, struct mmc_queue *mq),
-	TP_ARGS(card, mq));
-
 DECLARE_HOOK(android_vh_mmc_sdio_pm_flag_set,
 	TP_PROTO(struct mmc_host *host),
 	TP_ARGS(host));
