c4db168998fa4b38e7857f435be9e7e8ba937bc1
Jyu Jiang <jyu.jiang@vivo.corp-partner.google.com>
2024-07-10 11:13:49 +0800

    ANDROID: GKI: net: add vendor hooks net qos for gki purpose
    Add vendor hooks to support net qos policy feature:
    
    1.android_rvh_tcp_rcv_spurious_retrans
    With this hook we could trace the ACK path through the segments which
    are spuriously retransmitted by sender.
    
    Bug: 351960112
    
    Change-Id: I739cef428f4a43999d99393a205b3d53a1eb52da
    Signed-off-by: Jyu Jiang <jyu.jiang@vivo.corp-partner.google.com>

--- include/trace/hooks/net.h
+++ include/trace/hooks/net.h
@@ -46,6 +46,8 @@ DECLARE_RESTRICTED_HOOK(android_rvh_bpf_skb_load_bytes,
 	TP_PROTO(const struct sk_buff *skb, u32 offset, void *to, u32 len,
 		int *handled, int *err),
 	TP_ARGS(skb, offset, to, len, handled, err), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_tcp_rcv_spurious_retrans,
+	TP_PROTO(struct sock *sk), TP_ARGS(sk), 1);
 DECLARE_HOOK(android_vh_dc_send_copy,
 	TP_PROTO(struct sk_buff *skb, struct net_device *dev), TP_ARGS(skb, dev));
 DECLARE_HOOK(android_vh_dc_receive,
