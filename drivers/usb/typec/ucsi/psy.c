68202351a3c7f52f4cc69fc90f69c10edd18b5e9
Jameson Thies <jthies@google.com>
2024-07-17 00:49:47 +0000

    FROMLIST: usb: typec: ucsi: Add USB PD DRP to USB type
    Add POWER_SUPPLY_USB_TYPE_PD_DRP as a USB type in the UCSI power supply
    driver. The DRP type is set when the partner supports USB PD and offers
    both source and sink PDOs.
    
    Signed-off-by: Jameson Thies <jthies@google.com>
    (am from https://patchwork.kernel.org/patch/13741318/)
    (also found at https://lore.kernel.org/r/20240724201116.2094059-3-jthies@google.com)
    
    BUG=b:351778077
    UPSTREAM-TASK=b:353605100
    TEST=emerge-brox chromeos-kernel-6_6, usb_type is PD_DRP for partners
    with source and sink PDOs.
    
    Change-Id: I06e7d0cb45059b99550396867e62895598198429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5713891
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Jameson Thies <jthies@google.com>
    Reviewed-by: Benson Leung <bleung@google.com>
    Tested-by: Jameson Thies <jthies@google.com>
    Signed-off-by: Hubert Mazur <hmazur@google.com>

--- drivers/usb/typec/ucsi/psy.c
+++ drivers/usb/typec/ucsi/psy.c
@@ -203,8 +203,17 @@ static int ucsi_psy_get_usb_type(struct ucsi_connector *con,
 
 	val->intval = POWER_SUPPLY_USB_TYPE_C;
 	if (flags & UCSI_CONSTAT_CONNECTED &&
-	    UCSI_CONSTAT_PWR_OPMODE(flags) == UCSI_CONSTAT_PWR_OPMODE_PD)
+	    UCSI_CONSTAT_PWR_OPMODE(flags) == UCSI_CONSTAT_PWR_OPMODE_PD) {
+		for (int i = 0; i < con->num_pdos; i++) {
+			if (pdo_type(con->src_pdos[i]) == PDO_TYPE_FIXED &&
+			    con->src_pdos[i] & PDO_FIXED_DUAL_ROLE) {
+				val->intval = POWER_SUPPLY_USB_TYPE_PD_DRP;
+				return 0;
+			}
+		}
+
 		val->intval = POWER_SUPPLY_USB_TYPE_PD;
+	}
 
 	return 0;
 }
@@ -243,6 +252,7 @@ static enum power_supply_usb_type ucsi_psy_usb_types[] = {
 	POWER_SUPPLY_USB_TYPE_C,
 	POWER_SUPPLY_USB_TYPE_PD,
 	POWER_SUPPLY_USB_TYPE_PD_PPS,
+	POWER_SUPPLY_USB_TYPE_PD_DRP,
 };
 
 int ucsi_register_port_psy(struct ucsi_connector *con)
