BayLibre/android_kernel_common

ANDROID: rust_binder: add non-oneway transactions
Make it possible to send transactions that are not oneway transactions,
that is, transactions that you need to reply to.

Generally, binder will try to look like a normal function call, where
the call blocks until the function returns. This is implemented by
allowing you to reply to incoming transactions, and having the sender
sleep until a reply arrives.

For each thread, binder will keep track of the current transaction.
Furthermore, if you send a transaction from a thread that already has a
current transaction, then binder will make that transaction into a
"sub-transaction". This mimicks a call stack with normal functions. If
you use subtransactions to send calls A->B->A with A and B being two
different processes, then binder will ensure that the incoming
sub-transaction is executed on the thread in A that sent the original
message to B (and that thread in A is not used for any other incoming
transactions). This feature is often referred to as "deadlock avoidance"
because it avoids the case where A's threadpool has run out of threads,
preventing the incoming subtransaction from being processed.

Link: https://lore.kernel.org/rust-for-linux/20231101-rust-binder-v1-8-08ba9197f637@google.com/
Change-Id: I9042150ca671a3c15c1087dfd5b76510912e7438
Co-developed-by: Alice Ryhl <aliceryhl@google.com>
Signed-off-by: Wedson Almeida Filho <wedsonaf@gmail.com>
Signed-off-by: Alice Ryhl <aliceryhl@google.com>
Bug: 278052745

drivers/android/binder/transaction.rs
@@ -5,23 +5,25 @@ use kernel::{
     prelude::*,
     sync::{Arc, SpinLock},
     task::Kuid,
+    types::ScopeGuard,
     user_ptr::UserSlicePtrWriter,
 };
 
 use crate::{
     allocation::Allocation,
     defs::*,
-    error::BinderResult,
+    error::{BinderError, BinderResult},
     node::{Node, NodeRef},
     process::Process,
     ptr_align,
-    thread::Thread,
+    thread::{PushWorkRes, Thread},
     DArc, DLArc, DTRWrap, DeliverToRead,
 };
 
 #[pin_data]
 pub(crate) struct Transaction {
     target_node: Option<DArc<Node>>,
+    from_parent: Option<DArc<Transaction>>,
     pub(crate) from: Arc<Thread>,
     to: Arc<Process>,
     #[pin]
@@ -41,6 +43,7 @@ kernel::list::impl_list_arc_safe! {
 impl Transaction {
     pub(crate) fn new(
         node_ref: NodeRef,
+        from_parent: Option<DArc<Transaction>>,
         from: &Arc<Thread>,
         tr: &BinderTransactionDataSg,
     ) -> BinderResult<DLArc<Self>> {
@@ -58,8 +61,8 @@ impl Transaction {
                     return Err(err);
                 }
             };
-        if trd.flags & TF_ONE_WAY == 0 {
-            pr_warn!("Non-oneway transactions are not yet supported.");
+        if trd.flags & TF_ONE_WAY != 0 && from_parent.is_some() {
+            pr_warn!("Oneway transaction should not be in a transaction stack.");
             return Err(EINVAL.into());
         }
         if trd.flags & TF_CLEAR_BUF != 0 {
@@ -71,6 +74,7 @@ impl Transaction {
 
         Ok(DTRWrap::arc_pin_init(pin_init!(Transaction {
             target_node: Some(target_node),
+            from_parent,
             sender_euid: from.process.cred.euid(),
             from: from.clone(),
             to,
@@ -83,15 +87,100 @@ impl Transaction {
         }))?)
     }
 
-    /// Submits the transaction to a work queue.
+    pub(crate) fn new_reply(
+        from: &Arc<Thread>,
+        to: Arc<Process>,
+        tr: &BinderTransactionDataSg,
+    ) -> BinderResult<DLArc<Self>> {
+        let trd = &tr.transaction_data;
+        let mut alloc = match from.copy_transaction_data(to.clone(), tr, None) {
+            Ok(alloc) => alloc,
+            Err(err) => {
+                pr_warn!("Failure in copy_transaction_data: {:?}", err);
+                return Err(err);
+            }
+        };
+        if trd.flags & TF_CLEAR_BUF != 0 {
+            alloc.set_info_clear_on_drop();
+        }
+        Ok(DTRWrap::arc_pin_init(pin_init!(Transaction {
+            target_node: None,
+            from_parent: None,
+            sender_euid: from.process.task.euid(),
+            from: from.clone(),
+            to,
+            code: trd.code,
+            flags: trd.flags,
+            data_size: trd.data_size as _,
+            data_address: alloc.ptr,
+            allocation <- kernel::new_spinlock!(Some(alloc), "Transaction::new"),
+            txn_security_ctx_off: None,
+        }))?)
+    }
+
+    /// Determines if the transaction is stacked on top of the given transaction.
+    pub(crate) fn is_stacked_on(&self, onext: &Option<DArc<Self>>) -> bool {
+        match (&self.from_parent, onext) {
+            (None, None) => true,
+            (Some(from_parent), Some(next)) => Arc::ptr_eq(from_parent, next),
+            _ => false,
+        }
+    }
+
+    /// Returns a pointer to the next transaction on the transaction stack, if there is one.
+    pub(crate) fn clone_next(&self) -> Option<DArc<Self>> {
+        Some(self.from_parent.as_ref()?.clone())
+    }
+
+    /// Searches in the transaction stack for a thread that belongs to the target process. This is
+    /// useful when finding a target for a new transaction: if the node belongs to a process that
+    /// is already part of the transaction stack, we reuse the thread.
+    fn find_target_thread(&self) -> Option<Arc<Thread>> {
+        let mut it = &self.from_parent;
+        while let Some(transaction) = it {
+            if Arc::ptr_eq(&transaction.from.process, &self.to) {
+                return Some(transaction.from.clone());
+            }
+            it = &transaction.from_parent;
+        }
+        None
+    }
+
+    /// Searches in the transaction stack for a transaction originating at the given thread.
+    pub(crate) fn find_from(&self, thread: &Thread) -> Option<DArc<Transaction>> {
+        let mut it = &self.from_parent;
+        while let Some(transaction) = it {
+            if core::ptr::eq(thread, transaction.from.as_ref()) {
+                return Some(transaction.clone());
+            }
+
+            it = &transaction.from_parent;
+        }
+        None
+    }
+
+    /// Submits the transaction to a work queue. Uses a thread if there is one in the transaction
+    /// stack, otherwise uses the destination process.
+    ///
+    /// Not used for replies.
     pub(crate) fn submit(self: DLArc<Self>) -> BinderResult {
         let process = self.to.clone();
         let mut process_inner = process.inner.lock();
-        match process_inner.push_work(self) {
+
+        let res = if let Some(thread) = self.find_target_thread() {
+            match thread.push_work(self) {
+                PushWorkRes::Ok => Ok(()),
+                PushWorkRes::FailedDead(me) => Err((BinderError::new_dead(), me)),
+            }
+        } else {
+            process_inner.push_work(self)
+        };
+        drop(process_inner);
+
+        match res {
             Ok(()) => Ok(()),
             Err((err, work)) => {
                 // Drop work after releasing process lock.
-                drop(process_inner);
                 drop(work);
                 Err(err)
             }
@@ -100,11 +189,14 @@ impl Transaction {
 }
 
 impl DeliverToRead for Transaction {
-    fn do_work(
-        self: DArc<Self>,
-        _thread: &Thread,
-        writer: &mut UserSlicePtrWriter,
-    ) -> Result<bool> {
+    fn do_work(self: DArc<Self>, thread: &Thread, writer: &mut UserSlicePtrWriter) -> Result<bool> {
+        let send_failed_reply = ScopeGuard::new(|| {
+            if self.target_node.is_some() && self.flags & TF_ONE_WAY == 0 {
+                let reply = Err(BR_FAILED_REPLY);
+                self.from.deliver_reply(reply, &self);
+            }
+        });
+
         let mut tr_sec = BinderTransactionDataSecctx::default();
         let tr = tr_sec.tr_data();
         if let Some(target_node) = &self.target_node {
@@ -143,10 +235,20 @@ impl DeliverToRead for Transaction {
             writer.write(&*tr)?;
         }
 
+        // Dismiss the completion of transaction with a failure. No failure paths are allowed from
+        // here on out.
+        send_failed_reply.dismiss();
+
         // It is now the user's responsibility to clear the allocation.
         let alloc = self.allocation.lock().take();
         if let Some(alloc) = alloc {
             alloc.keep_alive();
+        }
+
+        // When this is not a reply and not a oneway transaction, update `current_transaction`. If
+        // it's a reply, `current_transaction` has already been updated appropriately.
+        if self.target_node.is_some() && tr_sec.transaction_data.flags & TF_ONE_WAY == 0 {
+            thread.set_current_transaction(self);
         }
 
         Ok(false)
@@ -154,6 +256,12 @@ impl DeliverToRead for Transaction {
 
     fn cancel(self: DArc<Self>) {
         drop(self.allocation.lock().take());
+
+        // If this is not a reply or oneway transaction, then send a dead reply.
+        if self.target_node.is_some() && self.flags & TF_ONE_WAY == 0 {
+            let reply = Err(BR_DEAD_REPLY);
+            self.from.deliver_reply(reply, &self);
+        }
     }
 
     fn should_sync_wakeup(&self) -> bool {

aosp-mirror/kernel_common

Change-Id: I84da6da68443f07d645ea0d55539dd1e1bc9c9e5

drivers/android/binder/transaction.rs
@@ -6,22 +6,24 @@ use kernel::{
     prelude::*,
     sync::{Arc, SpinLock},
     task::Kuid,
+    types::ScopeGuard,
 };
 
 use crate::{
     allocation::Allocation,
     defs::*,
-    error::BinderResult,
+    error::{BinderError, BinderResult},
     node::{Node, NodeRef},
     process::Process,
     ptr_align,
-    thread::Thread,
+    thread::{PushWorkRes, Thread},
     BinderReturnWriter, DArc, DLArc, DTRWrap, DeliverToRead,
 };
 
 #[pin_data]
 pub(crate) struct Transaction {
     target_node: Option<DArc<Node>>,
+    from_parent: Option<DArc<Transaction>>,
     pub(crate) from: Arc<Thread>,
     to: Arc<Process>,
     #[pin]
@@ -41,6 +43,7 @@ kernel::list::impl_list_arc_safe! {
 impl Transaction {
     pub(crate) fn new(
         node_ref: NodeRef,
+        from_parent: Option<DArc<Transaction>>,
         from: &Arc<Thread>,
         tr: &BinderTransactionDataSg,
     ) -> BinderResult<DLArc<Self>> {
@@ -58,8 +61,8 @@ impl Transaction {
                     return Err(err);
                 }
             };
-        if trd.flags & TF_ONE_WAY == 0 {
-            pr_warn!("Non-oneway transactions are not yet supported.");
+        if trd.flags & TF_ONE_WAY != 0 && from_parent.is_some() {
+            pr_warn!("Oneway transaction should not be in a transaction stack.");
             return Err(EINVAL.into());
         }
         if trd.flags & TF_CLEAR_BUF != 0 {
@@ -71,6 +74,7 @@ impl Transaction {
 
         Ok(DTRWrap::arc_pin_init(pin_init!(Transaction {
             target_node: Some(target_node),
+            from_parent,
             sender_euid: from.process.cred.euid(),
             from: from.clone(),
             to,
@@ -83,15 +87,100 @@ impl Transaction {
         }))?)
     }
 
-    /// Submits the transaction to a work queue.
+    pub(crate) fn new_reply(
+        from: &Arc<Thread>,
+        to: Arc<Process>,
+        tr: &BinderTransactionDataSg,
+    ) -> BinderResult<DLArc<Self>> {
+        let trd = &tr.transaction_data;
+        let mut alloc = match from.copy_transaction_data(to.clone(), tr, None) {
+            Ok(alloc) => alloc,
+            Err(err) => {
+                pr_warn!("Failure in copy_transaction_data: {:?}", err);
+                return Err(err);
+            }
+        };
+        if trd.flags & TF_CLEAR_BUF != 0 {
+            alloc.set_info_clear_on_drop();
+        }
+        Ok(DTRWrap::arc_pin_init(pin_init!(Transaction {
+            target_node: None,
+            from_parent: None,
+            sender_euid: from.process.task.euid(),
+            from: from.clone(),
+            to,
+            code: trd.code,
+            flags: trd.flags,
+            data_size: trd.data_size as _,
+            data_address: alloc.ptr,
+            allocation <- kernel::new_spinlock!(Some(alloc.success()), "Transaction::new"),
+            txn_security_ctx_off: None,
+        }))?)
+    }
+
+    /// Determines if the transaction is stacked on top of the given transaction.
+    pub(crate) fn is_stacked_on(&self, onext: &Option<DArc<Self>>) -> bool {
+        match (&self.from_parent, onext) {
+            (None, None) => true,
+            (Some(from_parent), Some(next)) => Arc::ptr_eq(from_parent, next),
+            _ => false,
+        }
+    }
+
+    /// Returns a pointer to the next transaction on the transaction stack, if there is one.
+    pub(crate) fn clone_next(&self) -> Option<DArc<Self>> {
+        Some(self.from_parent.as_ref()?.clone())
+    }
+
+    /// Searches in the transaction stack for a thread that belongs to the target process. This is
+    /// useful when finding a target for a new transaction: if the node belongs to a process that
+    /// is already part of the transaction stack, we reuse the thread.
+    fn find_target_thread(&self) -> Option<Arc<Thread>> {
+        let mut it = &self.from_parent;
+        while let Some(transaction) = it {
+            if Arc::ptr_eq(&transaction.from.process, &self.to) {
+                return Some(transaction.from.clone());
+            }
+            it = &transaction.from_parent;
+        }
+        None
+    }
+
+    /// Searches in the transaction stack for a transaction originating at the given thread.
+    pub(crate) fn find_from(&self, thread: &Thread) -> Option<DArc<Transaction>> {
+        let mut it = &self.from_parent;
+        while let Some(transaction) = it {
+            if core::ptr::eq(thread, transaction.from.as_ref()) {
+                return Some(transaction.clone());
+            }
+
+            it = &transaction.from_parent;
+        }
+        None
+    }
+
+    /// Submits the transaction to a work queue. Uses a thread if there is one in the transaction
+    /// stack, otherwise uses the destination process.
+    ///
+    /// Not used for replies.
     pub(crate) fn submit(self: DLArc<Self>) -> BinderResult {
         let process = self.to.clone();
         let mut process_inner = process.inner.lock();
-        match process_inner.push_work(self) {
+
+        let res = if let Some(thread) = self.find_target_thread() {
+            match thread.push_work(self) {
+                PushWorkRes::Ok => Ok(()),
+                PushWorkRes::FailedDead(me) => Err((BinderError::new_dead(), me)),
+            }
+        } else {
+            process_inner.push_work(self)
+        };
+        drop(process_inner);
+
+        match res {
             Ok(()) => Ok(()),
             Err((err, work)) => {
                 // Drop work after releasing process lock.
-                drop(process_inner);
                 drop(work);
                 Err(err)
             }
@@ -100,11 +189,14 @@ impl Transaction {
 }
 
 impl DeliverToRead for Transaction {
-    fn do_work(
-        self: DArc<Self>,
-        _thread: &Thread,
-        writer: &mut BinderReturnWriter,
-    ) -> Result<bool> {
+    fn do_work(self: DArc<Self>, thread: &Thread, writer: &mut BinderReturnWriter) -> Result<bool> {
+        let send_failed_reply = ScopeGuard::new(|| {
+            if self.target_node.is_some() && self.flags & TF_ONE_WAY == 0 {
+                let reply = Err(BR_FAILED_REPLY);
+                self.from.deliver_reply(reply, &self);
+            }
+        });
+
         let mut tr_sec = BinderTransactionDataSecctx::default();
         let tr = tr_sec.tr_data();
         if let Some(target_node) = &self.target_node {
@@ -143,10 +235,20 @@ impl DeliverToRead for Transaction {
             writer.write_payload(&*tr)?;
         }
 
+        // Dismiss the completion of transaction with a failure. No failure paths are allowed from
+        // here on out.
+        send_failed_reply.dismiss();
+
         // It is now the user's responsibility to clear the allocation.
         let alloc = self.allocation.lock().take();
         if let Some(alloc) = alloc {
             alloc.keep_alive();
+        }
+
+        // When this is not a reply and not a oneway transaction, update `current_transaction`. If
+        // it's a reply, `current_transaction` has already been updated appropriately.
+        if self.target_node.is_some() && tr_sec.transaction_data.flags & TF_ONE_WAY == 0 {
+            thread.set_current_transaction(self);
         }
 
         Ok(false)
@@ -154,6 +256,12 @@ impl DeliverToRead for Transaction {
 
     fn cancel(self: DArc<Self>) {
         drop(self.allocation.lock().take());
+
+        // If this is not a reply or oneway transaction, then send a dead reply.
+        if self.target_node.is_some() && self.flags & TF_ONE_WAY == 0 {
+            let reply = Err(BR_DEAD_REPLY);
+            self.from.deliver_reply(reply, &self);
+        }
     }
 
     fn should_sync_wakeup(&self) -> bool {