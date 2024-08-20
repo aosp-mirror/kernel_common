3d97ed3d77d32a2f4baf8c69c826a2d8778f1940
Tzung-Bi Shih <tzungbi@chromium.org>
2024-08-20 04:45:29 +0000

    Revert "CHROMIUM: net: Increase net.unix.max_dgram_qlen."
    This reverts commit bfd762abbc6d81ff43e2fadd1ddfd1b36df4f7a3.
    
    Reason: the patch is for ARC container which is no longer needed in 6.6.
    
    BUG=b:356770963
    BUG=b:285134532
    TEST=CQ
    
    Change-Id: Iace5348ca6cb7d6a0329cd1174430c08231c92a6
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5796710
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Signed-off-by: Mateusz Urbańczyk <urbanczyk@google.com>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -3554,12 +3554,7 @@ static int __net_init unix_net_init(struct net *net)
 {
 	int i;
 
-	/* The value was 10 in the original kernel. It is modified directly
-	 * here to give the larger value for processes inside containers, in
-	 * which the kernel does not provide a way to dynamically customize.
-	 * TODO(crbug/758081): Implement and upstream a safe way to customize.
-	 */
-	net->unx.sysctl_max_dgram_qlen = 60;
+	net->unx.sysctl_max_dgram_qlen = 10;
 	if (unix_sysctl_register(net))
 		goto out;
 
