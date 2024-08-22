1e539e8035a61f914968f534986a02cd3f2c6a8a
chunpeng li <lichunpeng@xiaomi.com>
2024-08-22 15:46:27 +0800

    ANDROID: vendor_hook: customize order for thp pcp list
    This hook is used to modify the order of the thp pcp list,
    without KMI changed.
    
    Bug: 359422098
    Change-Id: Id46e99c3290a6866f38dbe3e154ee7048a3238a4
    Signed-off-by: chunpeng li <lichunpeng@xiaomi.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -381,6 +381,9 @@ DECLARE_HOOK(android_vh_zs_shrinker_adjust,
 DECLARE_HOOK(android_vh_zs_shrinker_bypass,
 	TP_PROTO(bool *bypass),
 	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_customize_thp_pcp_order,
+	TP_PROTO(unsigned int *order),
+	TP_ARGS(order));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
