f32dba3b34c87c83ff48a41094d74cbc4eca54b8
jiangxinpei <huangdezhi@hihonor.com>
2024-08-22 20:31:21 +0800

    ANDROID: GKI: modify vendor hook name for Honor ogki
    1. Modify vendor hook name for Honor ogki to solve vh/rvh
       symbol name too long issue. This issue only happen when
       we use our own ogki vh/rvh in our oem modules.
    
    2. Add parameters declarations that ogki vh/rvh needed.
       These parameters declarations have already been declared
       in other vh/rvh declare header files, when our vendor modules
       only include ogki_honor.h, we need to make declarations for
       these parameters. This issue could only be found when we
       built vendor modules. So we move these to our ogki vh/rvh
       declarations header files:
       ufs_hba has already in include/trace/hooks/ufshcd.h
       task_struct has already in include/trace/hooks/sched.h
       sock and sk_buff have already in include/trace/hooks/net.h
    
    3. task_struct is used for android_vh_ogki_check_vip_status
       sock is used for android_vh_ogki_tcp_srtt_estimator
       sk_buff is used for android_vh_ogki_udp_unicast_rcv_skb
       ufs_hba is used for android_vh_ogki_ufs_dsm
       remove unused struct scsi_device
    
    Bug: 361105138
    Bug: 361519657
    Change-Id: Ie89f3ee402e405bf6d1b6d67c7b9ca1bd094b94c
    Fixes: eba98a6e2a70 ("ANDROID: GKI: Add vendor hook define for Honor ogki")
    Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>
    (cherry picked from commit b4a27402dc4ed0a9d97c11c161152df2ee813975)

--- include/trace/hooks/ogki_honor.h
+++ include/trace/hooks/ogki_honor.h
@@ -11,7 +11,10 @@
 
 #include <trace/hooks/vendor_hooks.h>
 
-struct scsi_device;
+struct task_struct;
+struct sock;
+struct sk_buff;
+struct ufs_hba;
 struct tcp_sock;
 struct net_device;
 struct cfg80211_registered_device;
@@ -24,16 +27,16 @@ DECLARE_HOOK(android_vh_ogki_ufs_clock_scaling,
 DECLARE_HOOK(android_vh_ogki_ufs_dsm,
 	TP_PROTO(struct ufs_hba *hba, unsigned long code, char *err_msg),
 	TP_ARGS(hba, code, err_msg));
-DECLARE_HOOK(android_vh_ogki_security_audit_log_setid,
+DECLARE_HOOK(android_vh_ogki_audit_log_setid,
 	TP_PROTO(u32 type, u32 old_id, u32 new_id),
 	TP_ARGS(type, old_id, new_id));
-DECLARE_HOOK(android_vh_ogki_security_audit_log_cfi,
+DECLARE_HOOK(android_vh_ogki_audit_log_cfi,
 	TP_PROTO(unsigned long addr, unsigned long* target),
 	TP_ARGS(addr, target));
-DECLARE_RESTRICTED_HOOK(android_rvh_ogki_security_audit_log_usercopy,
+DECLARE_RESTRICTED_HOOK(android_rvh_ogki_audit_log_usercopy,
 	TP_PROTO(bool to_user, const char* name, unsigned long len),
 	TP_ARGS(to_user, name, len), 1);
-DECLARE_RESTRICTED_HOOK(android_rvh_ogki_security_audit_log_module_sign,
+DECLARE_RESTRICTED_HOOK(android_rvh_ogki_audit_log_module_sign,
 	TP_PROTO(int err),
 	TP_ARGS(err), 1);
 DECLARE_HOOK(android_vh_ogki_check_vip_status,
@@ -56,9 +59,9 @@ DECLARE_HOOK(android_rvh_ogki_check_task_tags,
 	TP_ARGS(p, ret));
 DECLARE_HOOK(android_vh_ogki_tcp_srtt_estimator,
 	TP_PROTO(struct sock *sk), TP_ARGS(sk));
-DECLARE_HOOK(android_vh_ogki_tcp_rcv_established_fast_path,
+DECLARE_HOOK(android_vh_ogki_tcp_rcv_estab_fastpath,
 	TP_PROTO(struct sock *sk), TP_ARGS(sk));
-DECLARE_HOOK(android_vh_ogki_tcp_rcv_established_slow_path,
+DECLARE_HOOK(android_vh_ogki_tcp_rcv_estab_slowpath,
 	TP_PROTO(struct sock *sk), TP_ARGS(sk));
 DECLARE_HOOK(android_vh_ogki_set_wifi_state_connect,
 	TP_PROTO(const char *name, struct cfg80211_registered_device *rdev, struct net_device *dev, u8 *mac_addr),
@@ -81,19 +84,19 @@ DECLARE_HOOK(android_vh_ogki_get_log_usertype,
 DECLARE_HOOK(android_vh_ogki_hievent_to_jank,
 	TP_PROTO(int tag, int prio, const char *buf, int *ret),
 	TP_ARGS(tag, prio, buf, ret));
-DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hiview_hievent_create,
+DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hievent_create,
 	TP_PROTO(unsigned int event_id, void **event),
 	TP_ARGS(event_id, event), 1);
-DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hiview_hievent_put_string,
+DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hievent_put_string,
 	TP_PROTO(void *event, const char *key, const char *value, int *ret),
 	TP_ARGS(event, key, value, ret), 1);
-DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hiview_hievent_put_integral,
+DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hievent_put_integral,
 	TP_PROTO(void *event, const char *key, long long value, int *ret),
 	TP_ARGS(event, key, value, ret), 1);
-DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hiview_hievent_report,
+DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hievent_report,
 	TP_PROTO(void *event, int *ret),
 	TP_ARGS(event, ret), 1);
-DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hiview_hievent_destroy,
+DECLARE_RESTRICTED_HOOK(android_rvh_ogki_hievent_destroy,
 	TP_PROTO(void *event),
 	TP_ARGS(event), 1);
 DECLARE_HOOK(android_vh_ogki_f2fs_dsm,
