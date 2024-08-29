ef208407268a2dc8b04ed0c91495e4b2f40e9860
Tzung-Bi Shih <tzungbi@google.com>
2024-08-29 01:10:32 +0000

    Revert "CHROMIUM: fs/namespace.c: Copy nosymfollow to flags"
    This reverts commit 730f06b02b30a92b4cfda31ba175ae52807eae57.
    
    Reason: no longer needed.
    
    BUG=b:285134532
    BUG=b:356550631
    TEST=none
    
    Change-Id: I9b33b5b965cfb4361dfd242adaed5352c977ec29
    Signed-off-by: Tzung-Bi Shih <tzungbi@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5825889
    Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>
    Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Signed-off-by: Hubert Mazur <hmazur@google.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -3633,17 +3633,6 @@ int path_mount(const char *dev_name, struct path *path,
 	if (!(flags & MS_NOATIME))
 		mnt_flags |= MNT_RELATIME;
 
-	/*
-	 * The nosymfollow option used to be extracted from data_page by an LSM.
-	 * It is now passed in as MS_NOSYMFOLLOW.  We need to also check in
-	 * the old place until all callers have been updated to use the flag.
-	 */
-	if (data_page) {
-		if (!strncmp((char *)data_page, "nosymfollow", 11) ||
-		    strstr((char *)data_page, ",nosymfollow"))
-			flags |= MS_NOSYMFOLLOW;
-	}
-
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
 		mnt_flags |= MNT_NOSUID;
