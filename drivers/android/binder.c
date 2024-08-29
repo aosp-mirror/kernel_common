b9a259a716080f95d5e903f0f402a2cfa18397a5
Carlos Llamas <cmllamas@google.com>
2024-08-29 18:18:15 +0000

    ANDROID: binder: fix KMI issues due to frozen notification
    The patches to support binder's frozen notification feature break the
    KMI. This change fixes such issues by (1) moving proc->delivered_freeze
    into the existing proc_wrapper struction, (2) dropping the frozen stats
    support and (3) amending the STG due to a harmless enum binder_work_type
    addition.
    
    These are the reported KMI issues fixed by this patch:
    
      function symbol 'int __traceiter_binder_transaction_received(void*, struct binder_transaction*)' changed
        CRC changed from 0x74e9c98b to 0xfe0f8640
    
      type 'struct binder_proc' changed
        byte size changed from 584 to 632
        member 'struct list_head delivered_death' changed
          offset changed by 256
        member 'struct list_head delivered_freeze' was added
        13 members ('u32 max_threads' .. 'u64 android_oem_data1') changed
          offset changed by 384
    
      type 'struct binder_thread' changed
        byte size changed from 464 to 496
        2 members ('atomic_t tmp_ref' .. 'bool is_dead') changed
          offset changed by 224
        4 members ('struct task_struct* task' .. 'enum binder_prio_state prio_state') changed
          offset changed by 256
    
      type 'struct binder_stats' changed
        byte size changed from 216 to 244
        member changed from 'atomic_t br[21]' to 'atomic_t br[23]'
          type changed from 'atomic_t[21]' to 'atomic_t[23]'
            number of elements changed from 21 to 23
        member changed from 'atomic_t bc[19]' to 'atomic_t bc[22]'
          offset changed from 672 to 736
          type changed from 'atomic_t[19]' to 'atomic_t[22]'
            number of elements changed from 19 to 22
        member changed from 'atomic_t obj_created[7]' to 'atomic_t obj_created[8]'
          offset changed from 1280 to 1440
          type changed from 'atomic_t[7]' to 'atomic_t[8]'
            number of elements changed from 7 to 8
        member changed from 'atomic_t obj_deleted[7]' to 'atomic_t obj_deleted[8]'
          offset changed from 1504 to 1696
          type changed from 'atomic_t[7]' to 'atomic_t[8]'
            number of elements changed from 7 to 8
    
      type 'enum binder_work_type' changed
        enumerator 'BINDER_WORK_FROZEN_BINDER' (10) was added
        enumerator 'BINDER_WORK_CLEAR_FREEZE_NOTIFICATION' (11) was added
    
    Bug: 363013421
    Change-Id: If9f1f14a2eda215a4c9cb0823c50c8e0e8079ef1
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3894,7 +3894,6 @@ binder_request_freeze_notification(struct binder_proc *proc,
 	is_frozen = ref->node->proc->is_frozen;
 	binder_inner_proc_unlock(ref->node->proc);
 
-	binder_stats_created(BINDER_STAT_FREEZE);
 	INIT_LIST_HEAD(&freeze->work.entry);
 	freeze->cookie = handle_cookie->cookie;
 	freeze->work.type = BINDER_WORK_FROZEN_BINDER;
@@ -3980,7 +3979,7 @@ binder_freeze_notification_done(struct binder_proc *proc,
 	struct binder_work *w;
 
 	binder_inner_proc_lock(proc);
-	list_for_each_entry(w, &proc->delivered_freeze, entry) {
+	list_for_each_entry(w, &proc_wrapper(proc)->delivered_freeze, entry) {
 		struct binder_ref_freeze *tmp_freeze =
 			container_of(w, struct binder_ref_freeze, work);
 
@@ -4937,7 +4936,7 @@ retry:
 			info.is_frozen = freeze->is_frozen;
 			info.cookie = freeze->cookie;
 			freeze->sent = true;
-			binder_enqueue_work_ilocked(w, &proc->delivered_freeze);
+			binder_enqueue_work_ilocked(w, &proc_wrapper(proc)->delivered_freeze);
 			binder_inner_proc_unlock(proc);
 
 			if (put_user(BR_FROZEN_BINDER, (uint32_t __user *)ptr))
@@ -4957,7 +4956,6 @@ retry:
 
 			binder_inner_proc_unlock(proc);
 			kfree(freeze);
-			binder_stats_deleted(BINDER_STAT_FREEZE);
 			if (put_user(BR_CLEAR_FREEZE_NOTIFICATION_DONE, (uint32_t __user *)ptr))
 				return -EFAULT;
 			ptr += sizeof(uint32_t);
@@ -5244,7 +5242,6 @@ static struct binder_thread *binder_get_thread(struct binder_proc *proc)
 
 static void binder_free_proc(struct binder_proc *proc)
 {
-	struct binder_proc_wrap *proc_wrap;
 	struct binder_device *device;
 
 	BUG_ON(!list_empty(&proc->todo));
@@ -5261,8 +5258,7 @@ static void binder_free_proc(struct binder_proc *proc)
 	put_task_struct(proc->tsk);
 	put_cred(proc->cred);
 	binder_stats_deleted(BINDER_STAT_PROC);
-	proc_wrap = binder_proc_wrap_entry(proc);
-	kfree(proc_wrap);
+	kfree(proc_wrapper(proc));
 }
 
 static void binder_free_thread(struct binder_thread *thread)
@@ -6036,7 +6032,7 @@ static int binder_open(struct inode *nodp, struct file *filp)
 	binder_stats_created(BINDER_STAT_PROC);
 	proc->pid = current->group_leader->pid;
 	INIT_LIST_HEAD(&proc->delivered_death);
-	INIT_LIST_HEAD(&proc->delivered_freeze);
+	INIT_LIST_HEAD(&proc_wrapper(proc)->delivered_freeze);
 	INIT_LIST_HEAD(&proc->waiting_threads);
 	filp->private_data = proc;
 
@@ -6591,9 +6587,6 @@ static const char * const binder_return_strings[] = {
 	"BR_FAILED_REPLY",
 	"BR_FROZEN_REPLY",
 	"BR_ONEWAY_SPAM_SUSPECT",
-	"UNSUPPORTED",
-	"BR_FROZEN_BINDER",
-	"BR_CLEAR_FREEZE_NOTIFICATION_DONE",
 };
 
 static const char * const binder_command_strings[] = {
@@ -6616,9 +6609,6 @@ static const char * const binder_command_strings[] = {
 	"BC_DEAD_BINDER_DONE",
 	"BC_TRANSACTION_SG",
 	"BC_REPLY_SG",
-	"BC_REQUEST_FREEZE_NOTIFICATION",
-	"BC_CLEAR_FREEZE_NOTIFICATION",
-	"BC_FREEZE_NOTIFICATION_DONE",
 };
 
 static const char * const binder_objstat_strings[] = {
@@ -6629,7 +6619,6 @@ static const char * const binder_objstat_strings[] = {
 	"death",
 	"transaction",
 	"transaction_complete",
-	"freeze",
 };
 
 static void print_binder_stats(struct seq_file *m, const char *prefix,
