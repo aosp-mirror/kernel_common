99ada58989e66c4607eae1d241b97f5bc76ca40d
Greg Kroah-Hartman <gregkh@google.com>
2024-09-09 15:34:54 +0000

    Revert "bpf, sockmap: Convert schedule_work into delayed_work"
    This reverts commit 657cfb194cdbe9b13ab2693cbf68d783b6de6318 which is
    commit 29173d07f79883ac94f5570294f98af3d4287382 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I0ba69fdcb3ad9ecccf1bfb1070d3862b8cc2d3b4
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- net/core/skmsg.c
+++ net/core/skmsg.c
@@ -482,7 +482,7 @@ int sk_msg_recvmsg(struct sock *sk, struct sk_psock *psock, struct msghdr *msg,
 	}
 out:
 	if (psock->work_state.skb && copied > 0)
-		schedule_delayed_work(&psock->work, 0);
+		schedule_work(&psock->work);
 	return copied;
 }
 EXPORT_SYMBOL_GPL(sk_msg_recvmsg);
@@ -633,8 +633,7 @@ static void sk_psock_skb_state(struct sk_psock *psock,
 
 static void sk_psock_backlog(struct work_struct *work)
 {
-	struct delayed_work *dwork = to_delayed_work(work);
-	struct sk_psock *psock = container_of(dwork, struct sk_psock, work);
+	struct sk_psock *psock = container_of(work, struct sk_psock, work);
 	struct sk_psock_work_state *state = &psock->work_state;
 	struct sk_buff *skb = NULL;
 	bool ingress;
@@ -674,12 +673,6 @@ start:
 				if (ret == -EAGAIN) {
 					sk_psock_skb_state(psock, state, skb,
 							   len, off);
-
-					/* Delay slightly to prioritize any
-					 * other work that might be here.
-					 */
-					if (sk_psock_test_state(psock, SK_PSOCK_TX_ENABLED))
-						schedule_delayed_work(&psock->work, 1);
 					goto end;
 				}
 				/* Hard errors break pipe and stop xmit. */
@@ -733,7 +726,7 @@ struct sk_psock *sk_psock_init(struct sock *sk, int node)
 	INIT_LIST_HEAD(&psock->link);
 	spin_lock_init(&psock->link_lock);
 
-	INIT_DELAYED_WORK(&psock->work, sk_psock_backlog);
+	INIT_WORK(&psock->work, sk_psock_backlog);
 	mutex_init(&psock->work_mutex);
 	INIT_LIST_HEAD(&psock->ingress_msg);
 	spin_lock_init(&psock->ingress_lock);
@@ -822,7 +815,7 @@ static void sk_psock_destroy(struct work_struct *work)
 
 	sk_psock_done_strp(psock);
 
-	cancel_delayed_work_sync(&psock->work);
+	cancel_work_sync(&psock->work);
 	mutex_destroy(&psock->work_mutex);
 
 	psock_progs_drop(&psock->progs);
@@ -934,7 +927,7 @@ static int sk_psock_skb_redirect(struct sk_psock *from, struct sk_buff *skb)
 	}
 
 	skb_queue_tail(&psock_other->ingress_skb, skb);
-	schedule_delayed_work(&psock_other->work, 0);
+	schedule_work(&psock_other->work);
 	spin_unlock_bh(&psock_other->ingress_lock);
 	return 0;
 }
@@ -1014,7 +1007,7 @@ static int sk_psock_verdict_apply(struct sk_psock *psock, struct sk_buff *skb,
 			spin_lock_bh(&psock->ingress_lock);
 			if (sk_psock_test_state(psock, SK_PSOCK_TX_ENABLED)) {
 				skb_queue_tail(&psock->ingress_skb, skb);
-				schedule_delayed_work(&psock->work, 0);
+				schedule_work(&psock->work);
 				err = 0;
 			}
 			spin_unlock_bh(&psock->ingress_lock);
@@ -1045,7 +1038,7 @@ static void sk_psock_write_space(struct sock *sk)
 	psock = sk_psock(sk);
 	if (likely(psock)) {
 		if (sk_psock_test_state(psock, SK_PSOCK_TX_ENABLED))
-			schedule_delayed_work(&psock->work, 0);
+			schedule_work(&psock->work);
 		write_space = psock->saved_write_space;
 	}
 	rcu_read_unlock();
