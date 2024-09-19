eedda5f0420580317c11d2522517bda2be8248e8
Esther Shimanovich <eshimanovich@google.com>
2024-09-19 20:54:56 +0000

    CHROMIUM: HID: multitouch: skip driver reset for elantech touchpad
    There is another elantech touchpad which fails on resume due to the
    mt_reset function. Skip the driver reset for this particular touchpad.
    
    BUG=b:362576581
    TEST=`emerge-reven chromeos-kernel-6_6`
    TEST=`update_kernel.sh --remote=$dut_ip`
    TEST=verify the touchpad works after resuming from sleep
    UPSTREAM-TASK=b:298170707
    
    Change-Id: I9873dec2af12c00e94a7c12545fbc3d742e10a11
    Signed-off-by: Esther Shimanovich <eshimanovich@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5876781
    Reviewed-by: Jonathan Denose <jdenose@google.com>
    Signed-off-by: Mateusz Urbańczyk <urbanczyk@google.com>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -2326,6 +2326,10 @@ static const struct hid_device_id mt_devices[] = {
 		HID_DEVICE(HID_BUS_ANY, HID_GROUP_MULTITOUCH_WIN_8,
 			USB_VENDOR_ID_ELAN, 0x30c1) },
 
+	{ .driver_data = MT_CLS_WIN_8_SKIP_RESET,
+		HID_DEVICE(HID_BUS_ANY, HID_GROUP_MULTITOUCH_WIN_8,
+			USB_VENDOR_ID_ELAN, 0x3082) },
+
 	/* Zinitix devices */
 	{ .driver_data = MT_CLS_WIN_8_SKIP_RESET,
 		HID_DEVICE(HID_BUS_ANY, HID_GROUP_MULTITOUCH_WIN_8,
