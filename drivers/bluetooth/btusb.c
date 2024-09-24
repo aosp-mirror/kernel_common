a656c402f1c79b641b2ca716a992ea401ddd67d3
Chris Lu <chris.lu@mediatek.corp-partner.google.com>
2024-09-24 19:02:41 +0800

    UPSTREAM: Bluetooth: btusb: add callback function in btusb suspend/resume
    Add suspend/resum callback function in btusb_data which are reserved
    for vendor specific usage during suspend/resume. hdev->suspend will be
    added before stop traffic in btusb_suspend and hdev-> resume will be
    added after resubmit urb in btusb_resume.
    
    Signed-off-by: Chris Lu <chris.lu@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    (cherry picked from commit 95f92928ad2215b5f524903e67eebd8e14f99564
     https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git master)
    
    BUG=b:308395932
    TEST=build pass bluetooth basic function pass on Brya
    
    Conflicts:
            drivers/bluetooth/btusb.c
    
    Backport Notes:
            MediaTek BT reset flow refactor in the latest Kerenl.
    
    Change-Id: Ibfb05dddad6f9b146e8e1bfbd9b88ebb20614281
    Signed-off-by: Chris Lu <chris.lu@mediatek.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5890051
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Yun-hao Chung <howardchung@chromium.org>
    Tested-by: Yun-hao Chung <howardchung@chromium.org>
    Commit-Queue: Yun-hao Chung <howardchung@chromium.org>
    Signed-off-by: Mateusz Urbańczyk <urbanczyk@google.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -854,6 +854,9 @@ struct btusb_data {
 
 	int (*setup_on_usb)(struct hci_dev *hdev);
 
+	int (*suspend)(struct hci_dev *hdev);
+	int (*resume)(struct hci_dev *hdev);
+
 	int oob_wake_irq;   /* irq for out-of-band wake-on-bt */
 
 	struct qca_dump_info qca_dump;
@@ -4716,6 +4719,9 @@ static int btusb_suspend(struct usb_interface *intf, pm_message_t message)
 
 	cancel_work_sync(&data->work);
 
+	if (data->suspend)
+		data->suspend(data->hdev);
+
 	btusb_stop_traffic(data);
 	usb_kill_anchored_urbs(&data->tx_anchor);
 
@@ -4825,6 +4831,9 @@ static int btusb_resume(struct usb_interface *intf)
 		else
 			btusb_submit_isoc_urb(hdev, GFP_NOIO);
 	}
+
+	if (data->resume)
+		data->resume(hdev);
 
 	spin_lock_irq(&data->txlock);
 	play_deferred(data);
