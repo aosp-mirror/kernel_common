390ea33206dcafb5f309d18de17ad5598711f5dd
Eric Biggers <ebiggers@google.com>
2024-10-01 08:37:15 -0700

    FROMLIST: crypto: arm64/sha256-ce - add support for finup_mb
    Add an implementation of finup_mb to sha256-ce, using an interleaving
    factor of 2.  It interleaves a finup operation for two equal-length
    messages that share a common prefix.  dm-verity and fs-verity will take
    advantage of this for greatly improved performance on capable CPUs.
    
    This increases the throughput of SHA-256 hashing 4096-byte messages by
    the following amounts on the following CPUs:
    
        ARM Cortex-X1: 70%
        ARM Cortex-X3: 68%
        ARM Cortex-A76: 65%
        ARM Cortex-A715: 43%
        ARM Cortex-A510: 25%
        ARM Cortex-A55: 8%
    
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Reviewed-by: Sami Tolvanen <samitolvanen@google.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    
    Bug: 351884559
    Link: https://lore.kernel.org/linux-crypto/20241001153718.111665-5-ebiggers@kernel.org/
    Change-Id: Iadf56e5ce048c03ce97feae56c405634ef554cfa
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- arch/arm64/crypto/sha2-ce-glue.c
+++ arch/arm64/crypto/sha2-ce-glue.c
@@ -33,6 +33,11 @@ extern const u32 sha256_ce_offsetof_finalize;
 asmlinkage int __sha256_ce_transform(struct sha256_ce_state *sst, u8 const *src,
 				     int blocks);
 
+asmlinkage void __sha256_ce_finup2x(const struct sha256_state *sctx,
+				    const u8 *data1, const u8 *data2, int len,
+				    u8 out1[SHA256_DIGEST_SIZE],
+				    u8 out2[SHA256_DIGEST_SIZE]);
+
 static void sha256_ce_transform(struct sha256_state *sst, u8 const *src,
 				int blocks)
 {
@@ -124,6 +129,39 @@ static int sha256_ce_digest(struct shash_desc *desc, const u8 *data,
 	return sha256_ce_finup(desc, data, len, out);
 }
 
+static int sha256_ce_finup_mb(struct shash_desc *desc,
+			      const u8 * const data[], unsigned int len,
+			      u8 * const outs[], unsigned int num_msgs)
+{
+	struct sha256_ce_state *sctx = shash_desc_ctx(desc);
+
+	/*
+	 * num_msgs != 2 should not happen here, since this algorithm sets
+	 * mb_max_msgs=2, and the crypto API handles num_msgs <= 1 before
+	 * calling into the algorithm's finup_mb method.
+	 */
+	if (WARN_ON_ONCE(num_msgs != 2))
+		return -EOPNOTSUPP;
+
+	if (unlikely(!crypto_simd_usable()))
+		return -EOPNOTSUPP;
+
+	/* __sha256_ce_finup2x() assumes SHA256_BLOCK_SIZE <= len <= INT_MAX. */
+	if (unlikely(len < SHA256_BLOCK_SIZE || len > INT_MAX))
+		return -EOPNOTSUPP;
+
+	/* __sha256_ce_finup2x() assumes the following offsets. */
+	BUILD_BUG_ON(offsetof(struct sha256_state, state) != 0);
+	BUILD_BUG_ON(offsetof(struct sha256_state, count) != 32);
+	BUILD_BUG_ON(offsetof(struct sha256_state, buf) != 40);
+
+	kernel_neon_begin();
+	__sha256_ce_finup2x(&sctx->sst, data[0], data[1], len, outs[0],
+			    outs[1]);
+	kernel_neon_end();
+	return 0;
+}
+
 static int sha256_ce_export(struct shash_desc *desc, void *out)
 {
 	struct sha256_ce_state *sctx = shash_desc_ctx(desc);
@@ -164,9 +202,11 @@ static struct shash_alg algs[] = { {
 	.final			= sha256_ce_final,
 	.finup			= sha256_ce_finup,
 	.digest			= sha256_ce_digest,
+	.finup_mb		= sha256_ce_finup_mb,
 	.export			= sha256_ce_export,
 	.import			= sha256_ce_import,
 	.descsize		= sizeof(struct sha256_ce_state),
+	.mb_max_msgs		= 2,
 	.statesize		= sizeof(struct sha256_state),
 	.digestsize		= SHA256_DIGEST_SIZE,
 	.base			= {
